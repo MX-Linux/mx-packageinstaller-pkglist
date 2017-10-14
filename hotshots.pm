f015d0cdf81f723f8215b920257476650bf56d35
Dolphin Oracle <dolphinoracle@gmail.com>
2017-10-13 21:11:03 -0400

    added screenshooters

---
+++ hotshots.pm
@@ -0,0 +1,41 @@
+<?xml version="1.0"?>
+<app>
+
+
+<category>
+Screenshot Utilities
+</category>
+
+<name>  
+Hotshots
+</name>
+
+<description>  
+a Qt-4 based advanced screencapture app
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>http://ubuntuhandbook.org/wp-content/uploads/2013/07/HotShots-300x248.jpg</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+hotshots
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+hotshots
+</uninstall_package_names>
+
+</app>
---
+++ shutter.pm
@@ -0,0 +1,41 @@
+<?xml version="1.0"?>
+<app>
+
+
+<category>
+Screenshot Utilities
+</category>
+
+<name>  
+Shutter
+</name>
+
+<description>  
+a feature rich screenshot app
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>http://shutter-project.org/wp-content/uploads/key_feature_030.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+shutter
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+shutter
+</uninstall_package_names>
+
+</app>
