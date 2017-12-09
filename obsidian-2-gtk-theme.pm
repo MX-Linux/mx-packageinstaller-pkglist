4afa79302a4f03dba2914640bdfd939aa7dee901
pcallahan80 <pauldcallahan80@gmail.com>
2017-12-09 17:22:08 -0500

    added category Themes, Obsidian-2, Adwaita Dark WM

---
+++ obsidian-2-gtk-theme.pm
@@ -0,0 +1,39 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Themes
+</category>
+
+<name>  
+Obsidian-2 gtk Theme
+</name>
+
+<description>  
+a dark gtk Theme
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot></screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+obsidian-2-gtk-theme
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+obsidian-2-gtk-theme
+</uninstall_package_names>
+</app>
