5ce1fb4518030c158fba7605539da9f93076157c
Adrian <adrian@mxlinux.org>
2018-02-12 12:25:30 -0500

    add dropbox-thunar-addin.pm

---
+++ dropbox-thunar-addin.pm
@@ -0,0 +1,39 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Network
+</category>
+
+<name>  
+Dropbox Thunar Addin
+</name>
+
+<description>  
+Thunar Addin for Dropbox file sync utility
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+thunar-dropbox-plugin
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+thunar-dropbox-plugin
+</uninstall_package_names>
+</app>
