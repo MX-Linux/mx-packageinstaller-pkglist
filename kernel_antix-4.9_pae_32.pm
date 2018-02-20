2b30dda4abfced18ceff92b016ec67ac13b2f7c3
pcallahan80 <pauldcallahan80@gmail.com>
2018-02-20 09:26:25 -0500

    update antiX 4.9, add antix 4.15 kernels

--- kernel_antix-4.9_pae_32.pm
+++ kernel_antix-4.9_pae_32.pm
@@ -6,11 +6,11 @@ Kernels
 </category>
 
 <name>  
-antix 32-bit pae
+antix 4.9 32 bit pae
 </name>
 
 <description>  
-antiX 4.9.79 kernel, 32 bit pae
+antiX 4.9.81 kernel, 32 bit pae
 </description>
 
 <installable>
@@ -24,8 +24,8 @@ antiX 4.9.79 kernel, 32 bit pae
 </preinstall>
 
 <install_package_names>
-linux-image-4.9.79-antix.1-686-smp-pae
-linux-headers-4.9.79-antix.1-686-smp-pae
+linux-image-4.9.81-antix.1-686-smp-pae
+linux-headers-4.9.81-antix.1-686-smp-pae
 </install_package_names>
 
 
@@ -35,7 +35,7 @@ linux-headers-4.9.79-antix.1-686-smp-pae
 
 
 <uninstall_package_names>
-linux-image-4.9.79-antix.1-686-smp-pae
-linux-headers-4.9.79-antix.1-686-smp-pae
+linux-image-4.9.81-antix.1-686-smp-pae
+linux-headers-4.9.81-antix.1-686-smp-pae
 </uninstall_package_names>
 </app>
