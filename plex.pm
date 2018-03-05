da8491572d056834ed2b14b8cd23b3614f5f27a5
Dolphin Oracle <dolphinoracle@gmail.com>
2018-03-04 23:24:30 -0500

    plex changes

--- plex.pm
+++ plex.pm
@@ -34,10 +34,11 @@ plexmediaserver
 <postinstall>
 rm -f /etc/apt/sources.list.d/plex.list
 cp /usr/share/mx-packageinstaller-pkglist/plex_stuff/default/plexmediaserver /etc/default/plexmediaserver
-rm /etc/init.d/plexmediaserver
 chown -R plex:plex /var/lib/plexmediaserver/
-cp /usr/share/mx-packageinstaller-pkglist/plex_stuff/init/plexmediaserver /etc/init.d/plexmediaserver
-update-rc.d plexmediaserver defaults
+cp /usr/share/mx-packageinstaller-pkglist/plex_stuff/init/plexserver /etc/init.d/plexserver
+chmod a+x /etc/init.d/plexserver
+ln -fs /etc/init.d/plexserver /etc/init.d/plexmediaserver
+update-rc.d plexserver defaults
 </postinstall>
 
 
