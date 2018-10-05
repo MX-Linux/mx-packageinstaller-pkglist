f0da6081f11101fdb7dc0c813b9d20e5c780e832
Dolphin Oracle <dolphinoracle@gmail.com>
2018-10-04 20:07:30 -0400

    revised antiX kernels

---
+++ kernel_antix-4.18_pae_32.pm
@@ -0,0 +1,67 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Kernels
+</category>
+
+<name>
+antiX 4.18 32 bit pae
+</name>
+
+<description>
+   <am>antiX 4.18.7 kernel pae 32 bit</am>
+   <ca>antiX 4.18.7 kernel pae 32 bit</ca>
+   <cs>antiX 4.18.7 kernel pae 32 bit</cs>
+   <de>antiX 4.18.7 Kernel Pae 32 Bit</de>
+   <el>antiX 4.18.7 kernel pae 32 bit</el>
+   <en>antiX 4.18.7 kernel pae 32 bit</en>
+   <es>antiX 4.18.7 kernel pae 32 bit</es>
+   <fi>antiX 4.18.7 kernel pae 32 bit</fi>
+   <fr>Noyau antiX 4.18.7 pae 32 bit</fr>
+   <hi>antiX 4.18.7 kernel pae 32 bit</hi>
+   <hr>antiX 4.18.7 kernel pae 32 bit</hr>
+   <hu>antiX 4.18.7 kernel pae 32 bit</hu>
+   <it>antiX 4.18.7 kernel pae 32 bit</it>
+   <ja>antiX 4.18.7 kernel pae 32 bit</ja>
+   <kk>antiX 4.18.7 kernel pae 32 bit</kk>
+   <lt>antiX 4.18.7 kernel pae 32 bit</lt>
+   <nl>antiX 4.18.7 kernel pae 32 bit</nl>
+   <pl>antiX 4.18.7 kernel pae 32 bit</pl>
+   <pt_BR>antiX 4.18.7 kernel pae 32 bit</pt_BR>
+   <pt>antiX 4.18.7 kernel pae 32 bit</pt>
+   <ro>antiX 4.18.7 kernel pae 32 bit</ro>
+   <ru>antiX 4.18.7 kernel pae 32 bit</ru>
+   <sk>antiX 4.18.7 kernel pae 32 bit</sk>
+   <sv>antiX 4.18.7 kärna pae 32 bit</sv>
+   <tr>antiX 4.18.7 kernel pae 32 bit</tr>
+   <uk>antiX 4.18.7 kernel pae 32 bit</uk>
+   <zh_TW>antiX 4.18.7 kernel pae 32 bit</zh_TW>
+</description>
+
+<installable>
+32
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+linux-image-4.18.7-antix.1-686-smp-pae
+linux-headers-4.18.7-antix.1-686-smp-pae
+</install_package_names>
+
+
+<postinstall>
+/usr/share/mx-packageinstaller-pkglist/rebuild_dkms_packages.sh linux-image-4.18.7-antix.1-686-smp-pae
+</postinstall>
+
+
+<uninstall_package_names>
+linux-image-4.18.7-antix.1-686-smp-pae
+linux-headers-4.18.7-antix.1-686-smp-pae
+</uninstall_package_names>
+</app>
