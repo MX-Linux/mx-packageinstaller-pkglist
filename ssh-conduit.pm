6c1bda74ea6eb4ea6cd5f8c24f0d0d2b0cbbce44
Dolphin Oracle <dolphinoracle@gmail.com>
2018-11-18 21:14:43 -0500

    added clonezilla and ssh-conduit

---
+++ ssh-conduit.pm
@@ -0,0 +1,65 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Remote Access
+</category>
+
+<name>
+ssh Conduit
+</name>
+
+<description>
+   <am>A simple way to use remote resouces via an ssh encypted connection</am>
+   <ca>A simple way to use remote resouces via an ssh encypted connection</ca>
+   <cs>A simple way to use remote resouces via an ssh encypted connection</cs>
+   <de>A simple way to use remote resouces via an ssh encypted connection</de>
+   <el>A simple way to use remote resouces via an ssh encypted connection</el>
+   <en>A simple way to use remote resouces via an ssh encypted connection</en>
+   <es>A simple way to use remote resouces via an ssh encypted connection</es>
+   <fi>A simple way to use remote resouces via an ssh encypted connection</fi>
+   <fr>A simple way to use remote resouces via an ssh encypted connection</fr>
+   <hi>A simple way to use remote resouces via an ssh encypted connection</hi>
+   <hr>A simple way to use remote resouces via an ssh encypted connection</hr>
+   <hu>A simple way to use remote resouces via an ssh encypted connection</hu>
+   <it>A simple way to use remote resouces via an ssh encypted connection</it>
+   <ja>A simple way to use remote resouces via an ssh encypted connection</ja>
+   <kk>A simple way to use remote resouces via an ssh encypted connection</kk>
+   <lt>A simple way to use remote resouces via an ssh encypted connection</lt>
+   <nl>A simple way to use remote resouces via an ssh encypted connection</nl>
+   <pl>A simple way to use remote resouces via an ssh encypted connection</pl>
+   <pt_BR>A simple way to use remote resouces via an ssh encypted connection</pt_BR>
+   <pt>A simple way to use remote resouces via an ssh encypted connection</pt>
+   <ro>A simple way to use remote resouces via an ssh encypted connection</ro>
+   <ru>A simple way to use remote resouces via an ssh encypted connection</ru>
+   <sk>A simple way to use remote resouces via an ssh encypted connection</sk>
+   <sv>A simple way to use remote resouces via an ssh encypted connection</sv>
+   <tr>A simple way to use remote resouces via an ssh encypted connection</tr>
+   <uk>A simple way to use remote resouces via an ssh encypted connection</uk>
+   <zh_TW>A simple way to use remote resouces via an ssh encypted connection</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+ssh-conduit-antix
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+ssh-conduit-antix
+</uninstall_package_names>
+</app>
