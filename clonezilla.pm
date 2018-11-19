808ed3e21fff99194ace4be41d7f46fd0ec8ce80
Dolphin Oracle <dolphinoracle@gmail.com>
2018-11-18 21:14:43 -0500

    added clonezilla and ssh-conduit

---
+++ clonezilla.pm
@@ -0,0 +1,65 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Utility
+</category>
+
+<name>
+clonezilla
+</name>
+
+<description>
+   <am>clone and restore disks and partitions</am>
+   <ca>clone and restore disks and partitions</ca>
+   <cs>clone and restore disks and partitions</cs>
+   <de>clone and restore disks and partitions</de>
+   <el>clone and restore disks and partitions</el>
+   <en>clone and restore disks and partitions</en>
+   <es>clone and restore disks and partitions</es>
+   <fi>clone and restore disks and partitions</fi>
+   <fr>clone and restore disks and partitions</fr>
+   <hi>clone and restore disks and partitions</hi>
+   <hr>clone and restore disks and partitions</hr>
+   <hu>clone and restore disks and partitions</hu>
+   <it>clone and restore disks and partitions</it>
+   <ja>clone and restore disks and partitions</ja>
+   <kk>clone and restore disks and partitions</kk>
+   <lt>clone and restore disks and partitions</lt>
+   <nl>clone and restore disks and partitions</nl>
+   <pl>clone and restore disks and partitions</pl>
+   <pt_BR>clone and restore disks and partitions</pt_BR>
+   <pt>clone and restore disks and partitions</pt>
+   <ro>clone and restore disks and partitions</ro>
+   <ru>clone and restore disks and partitions</ru>
+   <sk>clone and restore disks and partitions</sk>
+   <sv>clone and restore disks and partitions</sv>
+   <tr>clone and restore disks and partitions</tr>
+   <uk>clone and restore disks and partitions</uk>
+   <zh_TW>clone and restore disks and partitions</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+clonezilla
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+clonezilla
+</uninstall_package_names>
+</app>
