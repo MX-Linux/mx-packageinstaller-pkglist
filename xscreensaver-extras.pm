537a844c45816fa39274c9e32e0078764fef9e7a
Dolphin Oracle <dolphinoracle@gmail.com>
2018-12-07 21:12:35 -0500

    more mx18 revisions

---
+++ xscreensaver-extras.pm
@@ -0,0 +1,70 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Screensaver
+</category>
+
+<name>
+additional screensavers
+</name>
+
+<description>
+   <am>extra screensavers (requires xscreensaver)</am>
+   <ca>extra screensavers (requires xscreensaver)</ca>
+   <cs>extra screensavers (requires xscreensaver)</cs>
+   <da>extra screensavers (requires xscreensaver)</da>
+   <de>extra screensavers (requires xscreensaver)</de>
+   <el>extra screensavers (requires xscreensaver)</el>
+   <en>extra screensavers (requires xscreensaver)</en>
+   <es>extra screensavers (requires xscreensaver)</es>
+   <fi>extra screensavers (requires xscreensaver)</fi>
+   <fr>extra screensavers (requires xscreensaver)</fr>
+   <hi>extra screensavers (requires xscreensaver)</hi>
+   <hr>extra screensavers (requires xscreensaver)</hr>
+   <hu>extra screensavers (requires xscreensaver)</hu>
+   <is>extra screensavers (requires xscreensaver)</is>
+   <it>extra screensavers (requires xscreensaver)</it>
+   <ja>extra screensavers (requires xscreensaver)</ja>
+   <kk>extra screensavers (requires xscreensaver)</kk>
+   <lt>extra screensavers (requires xscreensaver)</lt>
+   <nl>extra screensavers (requires xscreensaver)</nl>
+   <pl>extra screensavers (requires xscreensaver)</pl>
+   <pt_BR>extra screensavers (requires xscreensaver)</pt_BR>
+   <pt>extra screensavers (requires xscreensaver)</pt>
+   <ro>extra screensavers (requires xscreensaver)</ro>
+   <ru>extra screensavers (requires xscreensaver)</ru>
+   <sk>extra screensavers (requires xscreensaver)</sk>
+   <sq>extra screensavers (requires xscreensaver)</sq>
+   <sv>extra screensavers (requires xscreensaver)</sv>
+   <tr>extra screensavers (requires xscreensaver)</tr>
+   <uk>extra screensavers (requires xscreensaver)</uk>
+   <zh_TW>extra screensavers (requires xscreensaver)</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+xscreensavers-data-extra
+xscreensaver-screensaver-bsod
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+xscreensavers-data-extra
+xscreensaver-screensaver-bsod
+</uninstall_package_names>
+</app>
---
+++ xscreensaver-opengl.pm
@@ -0,0 +1,70 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Screensaver
+</category>
+
+<name>
+openGL screensavers 
+</name>
+
+<description>
+   <am>extra opengl screensavers (requires xscreensaver)</am>
+   <ca>extra opengl screensavers (requires xscreensaver)</ca>
+   <cs>extra opengl screensavers (requires xscreensaver)</cs>
+   <da>extra opengl screensavers (requires xscreensaver)</da>
+   <de>extra opengl screensavers (requires xscreensaver)</de>
+   <el>extra opengl screensavers (requires xscreensaver)</el>
+   <en>extra opengl screensavers (requires xscreensaver)</en>
+   <es>extra opengl screensavers (requires xscreensaver)</es>
+   <fi>extra opengl screensavers (requires xscreensaver)</fi>
+   <fr>extra opengl screensavers (requires xscreensaver)</fr>
+   <hi>extra opengl screensavers (requires xscreensaver)</hi>
+   <hr>extra opengl screensavers (requires xscreensaver)</hr>
+   <hu>extra opengl screensavers (requires xscreensaver)</hu>
+   <is>extra opengl screensavers (requires xscreensaver)</is>
+   <it>extra opengl screensavers (requires xscreensaver)</it>
+   <ja>extra opengl screensavers (requires xscreensaver)</ja>
+   <kk>extra opengl screensavers (requires xscreensaver)</kk>
+   <lt>extra opengl screensavers (requires xscreensaver)</lt>
+   <nl>extra opengl screensavers (requires xscreensaver)</nl>
+   <pl>extra opengl screensavers (requires xscreensaver)</pl>
+   <pt_BR>extra opengl screensavers (requires xscreensaver)</pt_BR>
+   <pt>extra opengl screensavers (requires xscreensaver)</pt>
+   <ro>extra opengl screensavers (requires xscreensaver)</ro>
+   <ru>extra opengl screensavers (requires xscreensaver)</ru>
+   <sk>extra opengl screensavers (requires xscreensaver)</sk>
+   <sq>extra opengl screensavers (requires xscreensaver)</sq>
+   <sv>extra opengl screensavers (requires xscreensaver)</sv>
+   <tr>extra opengl screensavers (requires xscreensaver)</tr>
+   <uk>extra opengl screensavers (requires xscreensaver)</uk>
+   <zh_TW>extra opengl screensavers (requires xscreensaver)</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+xscreensavers-gl
+xscreensaver-gl-extra
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+xscreensavers-gl
+xscreensaver-gl-extra
+</uninstall_package_names>
+</app>
