6762bb030731397cd6541ec0377716874e34674e
Dolphin Oracle <dolphinoracle@gmail.com>
2018-12-08 11:11:44 -0500

    added grub-themes-mx

---
+++ grub_themes_mx.pm
@@ -0,0 +1,68 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Themes
+</category>
+
+<name>
+grub-themes-mx
+</name>
+
+<description>
+   <am>some themes for grub boot menu</am>
+   <ca>some themes for grub boot menu</ca>
+   <cs>some themes for grub boot menu</cs>
+   <da>some themes for grub boot menu</da>
+   <de>some themes for grub boot menu</de>
+   <el>some themes for grub boot menu</el>
+   <en>some themes for grub boot menu</en>
+   <es>some themes for grub boot menu</es>
+   <fi>some themes for grub boot menu</fi>
+   <fr>some themes for grub boot menu</fr>
+   <hi>some themes for grub boot menu</hi>
+   <hr>some themes for grub boot menu</hr>
+   <hu>some themes for grub boot menu</hu>
+   <is>some themes for grub boot menu</is>
+   <it>some themes for grub boot menu</it>
+   <ja>some themes for grub boot menu</ja>
+   <kk>some themes for grub boot menu</kk>
+   <lt>some themes for grub boot menu</lt>
+   <nl>some themes for grub boot menu</nl>
+   <pl>some themes for grub boot menu</pl>
+   <pt_BR>some themes for grub boot menu</pt_BR>
+   <pt>some themes for grub boot menu</pt>
+   <ro>some themes for grub boot menu</ro>
+   <ru>some themes for grub boot menu</ru>
+   <sk>some themes for grub boot menu</sk>
+   <sq>some themes for grub boot menu</sq>
+   <sv>some themes for grub boot menu</sv>
+   <tr>some themes for grub boot menu</tr>
+   <uk>some themes for grub boot menu</uk>
+   <zh_TW>some themes for grub boot menu</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot></screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+grub-themes-mx
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+grub-themes-mx
+</uninstall_package_names>
+</app>
