0e37b38e6cfcfc7c5bc8d39e9ee0dd8f4bbd81f9
Dolphin Oracle <dolphinoracle@gmail.com>
2018-12-08 11:08:29 -0500

    added mx18 artwork updated antiX kernel entries

--- kernel_antix-4.9_nopae_32.pm
+++ kernel_antix-4.9_nopae_32.pm
@@ -10,36 +10,36 @@ antiX 4.9 32 bit no-pae
 </name>
 
 <description>
-   <am>antiX 4.9.140 kernel, 32 bit no-pae</am>
-   <ca>antiX 4.9.140 kernel, 32 bit no-pae</ca>
-   <cs>antiX 4.9.140 kernel, 32 bit no-pae</cs>
-   <da>antiX 4.9.140 kernel, 32 bit no-pae</da>
-   <de>antiX 4.9.140 Kernel, 32 bit no-pae</de>
-   <el>πυρήνα antiX 4.9.140, 32 bit no-pae</el>
-   <en>antiX 4.9.140 kernel, 32 bit no-pae</en>
-   <es>antiX 4.9.140 kernel, 32 bit no-pae</es>
-   <fi>antiX 4.9.140 kernel, 32 bit no-pae</fi>
-   <fr>Noyau antiX 4.9.140 no-pae, 32 bit</fr>
-   <hi>antiX 4.9.140 kernel, 32 bit no-pae</hi>
-   <hr>antiX 4.9.140 kernel, 32 bit no-pae</hr>
-   <hu>antiX 4.9.140 kernel, 32 bit no-pae</hu>
-   <is>antiX 4.9.140 kernel, 32 bit no-pae</is>
-   <it>antiX 4.9.140 kernel, 32 bit no-pae</it>
-   <ja>antiX 4.9.140 kernel, 32 bit no-pae</ja>
-   <kk>antiX 4.9.140 kernel, 32 bit no-pae</kk>
-   <lt>antiX 4.9.140 kernel, 32 bit no-pae</lt>
-   <nl>antiX 4.9.140 kernel, 32 bit no-pae</nl>
-   <pl>antiX 4.9.140 kernel, 32 bit no-pae</pl>
-   <pt_BR>antiX 4.9.140 kernel, 32 bit no-pae</pt_BR>
-   <pt>Núcleo antiX 4.9.140, 32 bit no-pae</pt>
-   <ro>antiX 4.9.140 kernel, 32 bit no-pae</ro>
-   <ru>antiX ядро 4.9.140, 32 bit no-PAE</ru>
-   <sk>antiX 4.9.140 kernel, 32 bit no-pae</sk>
-   <sq>antiX 4.9.140 kernel, 32 bit no-pae</sq>
-   <sv>antiX 4.9.140 kärna, 32 bit no-pae</sv>
-   <tr>antiX 4.9.140 kernel, 32 bit no-pae</tr>
-   <uk>antiX 4.9.140 kernel, 32 bit no-pae</uk>
-   <zh_TW>antiX 4.9.140 kernel, 32 bit no-pae</zh_TW>
+   <am>antiX 4.9.143 kernel, 32 bit no-pae</am>
+   <ca>antiX 4.9.143 kernel, 32 bit no-pae</ca>
+   <cs>antiX 4.9.143 kernel, 32 bit no-pae</cs>
+   <da>antiX 4.9.143 kernel, 32 bit no-pae</da>
+   <de>antiX 4.9.143 Kernel, 32 bit no-pae</de>
+   <el>πυρήνα antiX 4.9.143, 32 bit no-pae</el>
+   <en>antiX 4.9.143 kernel, 32 bit no-pae</en>
+   <es>antiX 4.9.143 kernel, 32 bit no-pae</es>
+   <fi>antiX 4.9.143 kernel, 32 bit no-pae</fi>
+   <fr>Noyau antiX 4.9.143 no-pae, 32 bit</fr>
+   <hi>antiX 4.9.143 kernel, 32 bit no-pae</hi>
+   <hr>antiX 4.9.143 kernel, 32 bit no-pae</hr>
+   <hu>antiX 4.9.143 kernel, 32 bit no-pae</hu>
+   <is>antiX 4.9.143 kernel, 32 bit no-pae</is>
+   <it>antiX 4.9.143 kernel, 32 bit no-pae</it>
+   <ja>antiX 4.9.143 kernel, 32 bit no-pae</ja>
+   <kk>antiX 4.9.143 kernel, 32 bit no-pae</kk>
+   <lt>antiX 4.9.143 kernel, 32 bit no-pae</lt>
+   <nl>antiX 4.9.143 kernel, 32 bit no-pae</nl>
+   <pl>antiX 4.9.143 kernel, 32 bit no-pae</pl>
+   <pt_BR>antiX 4.9.143 kernel, 32 bit no-pae</pt_BR>
+   <pt>Núcleo antiX 4.9.143, 32 bit no-pae</pt>
+   <ro>antiX 4.9.143 kernel, 32 bit no-pae</ro>
+   <ru>antiX ядро 4.9.143, 32 bit no-PAE</ru>
+   <sk>antiX 4.9.143 kernel, 32 bit no-pae</sk>
+   <sq>antiX 4.9.143 kernel, 32 bit no-pae</sq>
+   <sv>antiX 4.9.143 kärna, 32 bit no-pae</sv>
+   <tr>antiX 4.9.143 kernel, 32 bit no-pae</tr>
+   <uk>antiX 4.9.143 kernel, 32 bit no-pae</uk>
+   <zh_TW>antiX 4.9.143 kernel, 32 bit no-pae</zh_TW>
 </description>
 
 <installable>
@@ -53,18 +53,18 @@ antiX 4.9 32 bit no-pae
 </preinstall>
 
 <install_package_names>
-linux-image-4.9.140-antix.1-486-smp
-linux-headers-4.9.140-antix.1-486-smp
+linux-image-4.9.143-antix.1-486-smp
+linux-headers-4.9.143-antix.1-486-smp
 </install_package_names>
 
 
 <postinstall>
-/usr/share/mx-packageinstaller-pkglist/rebuild_dkms_packages.sh linux-image-4.9.140-antix.1-486-smp
+/usr/share/mx-packageinstaller-pkglist/rebuild_dkms_packages.sh linux-image-4.9.143-antix.1-486-smp
 </postinstall>
 
 
 <uninstall_package_names>
-linux-image-4.9.140-antix.1-486-smp
-linux-headers-4.9.140-antix.1-486-smp
+linux-image-4.9.143-antix.1-486-smp
+linux-headers-4.9.143-antix.1-486-smp
 </uninstall_package_names>
 </app>
