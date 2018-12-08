e6b0de6ca5099e8de07a4f76015d35e1bed7da17
Dolphin Oracle <dolphinoracle@gmail.com>
2018-12-07 21:12:35 -0500

    more mx18 revisions

---
+++ grub-customizer.pm
@@ -0,0 +1,68 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Utility
+</category>
+
+<name>
+grub customizer
+</name>
+
+<description>
+   <am>customize grub boot menu</am>
+   <ca>customize grub boot menu</ca>
+   <cs>customize grub boot menu</cs>
+   <da>customize grub boot menu</da>
+   <de>customize grub boot menu</de>
+   <el>customize grub boot menu</el>
+   <en>customize grub boot menu</en>
+   <es>customize grub boot menu</es>
+   <fi>customize grub boot menu</fi>
+   <fr>customize grub boot menu</fr>
+   <hi>customize grub boot menu</hi>
+   <hr>customize grub boot menu</hr>
+   <hu>customize grub boot menu</hu>
+   <is>customize grub boot menu</is>
+   <it>customize grub boot menu</it>
+   <ja>customize grub boot menu</ja>
+   <kk>customize grub boot menu</kk>
+   <lt>customize grub boot menu</lt>
+   <nl>customize grub boot menu</nl>
+   <pl>customize grub boot menu</pl>
+   <pt_BR>customize grub boot menu</pt_BR>
+   <pt>customize grub boot menu</pt>
+   <ro>customize grub boot menu</ro>
+   <ru>customize grub boot menu</ru>
+   <sk>customize grub boot menu</sk>
+   <sq>customize grub boot menu</sq>
+   <sv>customize grub boot menu</sv>
+   <tr>customize grub boot menu</tr>
+   <uk>customize grub boot menu</uk>
+   <zh_TW>customize grub boot menu</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+grub-customizer
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+grub-customizer
+</uninstall_package_names>
+</app>
---
+++ xscreensaver.pm
@@ -0,0 +1,70 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Screensaver
+</category>
+
+<name>
+xscreensaver
+</name>
+
+<description>
+   <am>the classic screensaver for X servers</am>
+   <ca>the classic screensaver for X servers</ca>
+   <cs>the classic screensaver for X servers</cs>
+   <da>the classic screensaver for X servers</da>
+   <de>the classic screensaver for X servers</de>
+   <el>the classic screensaver for X servers</el>
+   <en>the classic screensaver for X servers</en>
+   <es>the classic screensaver for X servers</es>
+   <fi>the classic screensaver for X servers</fi>
+   <fr>the classic screensaver for X servers</fr>
+   <hi>the classic screensaver for X servers</hi>
+   <hr>the classic screensaver for X servers</hr>
+   <hu>the classic screensaver for X servers</hu>
+   <is>the classic screensaver for X servers</is>
+   <it>the classic screensaver for X servers</it>
+   <ja>the classic screensaver for X servers</ja>
+   <kk>the classic screensaver for X servers</kk>
+   <lt>the classic screensaver for X servers</lt>
+   <nl>the classic screensaver for X servers</nl>
+   <pl>the classic screensaver for X servers</pl>
+   <pt_BR>the classic screensaver for X servers</pt_BR>
+   <pt>the classic screensaver for X servers</pt>
+   <ro>the classic screensaver for X servers</ro>
+   <ru>the classic screensaver for X servers</ru>
+   <sk>the classic screensaver for X servers</sk>
+   <sq>the classic screensaver for X servers</sq>
+   <sv>the classic screensaver for X servers</sv>
+   <tr>the classic screensaver for X servers</tr>
+   <uk>the classic screensaver for X servers</uk>
+   <zh_TW>the classic screensaver for X servers</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+xscreensaver
+xscreensaver-data
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+xscreensaver
+xscreensaver-data
+</uninstall_package_names>
+</app>
