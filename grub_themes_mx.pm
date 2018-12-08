0ea4ce8ff1142498ae6cef0f6c3f32fc416ca4f7
Dolphin Oracle <dolphinoracle@gmail.com>
2018-12-08 14:12:05 -0500

    added LO-pt langauge area

--- grub_themes_mx.pm
+++ grub_themes_mx.pm
@@ -10,36 +10,36 @@ grub-themes-mx
 </name>
 
 <description>
-   <am>some themes for grub boot menu</am>
-   <ca>some themes for grub boot menu</ca>
-   <cs>some themes for grub boot menu</cs>
-   <da>some themes for grub boot menu</da>
-   <de>some themes for grub boot menu</de>
-   <el>some themes for grub boot menu</el>
-   <en>some themes for grub boot menu</en>
-   <es>some themes for grub boot menu</es>
-   <fi>some themes for grub boot menu</fi>
-   <fr>some themes for grub boot menu</fr>
-   <hi>some themes for grub boot menu</hi>
-   <hr>some themes for grub boot menu</hr>
-   <hu>some themes for grub boot menu</hu>
-   <is>some themes for grub boot menu</is>
-   <it>some themes for grub boot menu</it>
-   <ja>some themes for grub boot menu</ja>
-   <kk>some themes for grub boot menu</kk>
-   <lt>some themes for grub boot menu</lt>
-   <nl>some themes for grub boot menu</nl>
-   <pl>some themes for grub boot menu</pl>
-   <pt_BR>some themes for grub boot menu</pt_BR>
-   <pt>some themes for grub boot menu</pt>
-   <ro>some themes for grub boot menu</ro>
-   <ru>some themes for grub boot menu</ru>
-   <sk>some themes for grub boot menu</sk>
-   <sq>some themes for grub boot menu</sq>
-   <sv>some themes for grub boot menu</sv>
-   <tr>some themes for grub boot menu</tr>
-   <uk>some themes for grub boot menu</uk>
-   <zh_TW>some themes for grub boot menu</zh_TW>
+   <am>themes for grub boot menu</am>
+   <ca>themes for grub boot menu</ca>
+   <cs>themes for grub boot menu</cs>
+   <da>themes for grub boot menu</da>
+   <de>themes for grub boot menu</de>
+   <el>themes for grub boot menu</el>
+   <en>themes for grub boot menu</en>
+   <es>themes for grub boot menu</es>
+   <fi>themes for grub boot menu</fi>
+   <fr>themes for grub boot menu</fr>
+   <hi>themes for grub boot menu</hi>
+   <hr>themes for grub boot menu</hr>
+   <hu>themes for grub boot menu</hu>
+   <is>themes for grub boot menu</is>
+   <it>themes for grub boot menu</it>
+   <ja>themes for grub boot menu</ja>
+   <kk>themes for grub boot menu</kk>
+   <lt>themes for grub boot menu</lt>
+   <nl>themes for grub boot menu</nl>
+   <pl>themes for grub boot menu</pl>
+   <pt_BR>themes for grub boot menu</pt_BR>
+   <pt>themes for grub boot menu</pt>
+   <ro>themes for grub boot menu</ro>
+   <ru>themes for grub boot menu</ru>
+   <sk>themes for grub boot menu</sk>
+   <sq>themes for grub boot menu</sq>
+   <sv>themes for grub boot menu</sv>
+   <tr>themes for grub boot menu</tr>
+   <uk>themes for grub boot menu</uk>
+   <zh_TW>themes for grub boot menu</zh_TW>
 </description>
 
 <installable>
---
+++ portugese_pt_LO.pm
@@ -0,0 +1,80 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Language
+</category>
+
+<name>
+Portugese(PT) Libreoffice
+</name>
+
+<description>
+   <am>Portugese(PT) Language Meta-Package for LibreOffice</am>
+   <ca>Meta-paquet de llengua per LibreOffice en Portuguès (PT)</ca>
+   <cs>Portugese(PT) Language Meta-Package for LibreOffice</cs>
+   <da>Portugisisk (brasilien) sprog-metapakke til LibreOffice</da>
+   <de>Portugiesisches (PT) Meta-Paket für LibreOffice</de>
+   <el>LibreOffice στα πορτογαλικά (Βραζιλίας)</el>
+   <en>Portugese(PT) Language Meta-Package for LibreOffice</en>
+   <es>Portugese(PT) Language Meta-Package for LibreOffice</es>
+   <fi>Portugese(PT) Language Meta-Package for LibreOffice</fi>
+   <fr>Méta-paquet langue portugaise (PT) pour LibreOffice</fr>
+   <hi>Portugese(PT) Language Meta-Package for LibreOffice</hi>
+   <hr>Portugese(PT) Language Meta-Package for LibreOffice</hr>
+   <hu>Portugese(PT) Language Meta-Package for LibreOffice</hu>
+   <is>Portugese(PT) Language Meta-Package for LibreOffice</is>
+   <it>Meta-pacchetto della lingua portoghese(PT) per LibreOffice</it>
+   <ja>Portugese(PT) Language Meta-Package for LibreOffice</ja>
+   <kk>Portugese(PT) Language Meta-Package for LibreOffice</kk>
+   <lt>Portugese(PT) Language Meta-Package for LibreOffice</lt>
+   <nl>Portugese(PT) Taal Meta-Pakket voor LibreOffice</nl>
+   <pl>Portugese(PT) Language Meta-Package for LibreOffice</pl>
+   <pt_PT>Português(PT) Meta-Pacote de Idioma para LibreOffice</pt_BR>
+   <pt>Português(PT) Meta-Pacote de Idioma para LibreOffice</pt>
+   <ro>Portugese(PT) Language Meta-Package for LibreOffice</ro>
+   <ru>Portugese(PT) Language Meta-Package for LibreOffice</ru>
+   <sk>Portugese(PT) Language Meta-Package for LibreOffice</sk>
+   <sq>Portugese(PT) Language Meta-Package for LibreOffice</sq>
+   <sv>Portugisiskt(PT) Språk Meta-Paket för LibreOffice</sv>
+   <tr>Portugese(PT) Language Meta-Package for LibreOffice</tr>
+   <uk>Portugese(PT) Language Meta-Package for LibreOffice</uk>
+   <zh_TW>Portugese(PT) Language Meta-Package for LibreOffice</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+libreoffice-calc
+libreoffice-draw
+libreoffice-impress
+libreoffice-math
+libreoffice-writer
+libreoffice-l10n-pt
+libreoffice-help-pt
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+libreoffice-calc
+libreoffice-draw
+libreoffice-impress
+libreoffice-math
+libreoffice-writer
+libreoffice-l10n-pt
+libreoffice-help-pt
+</uninstall_package_names>
+</app>
