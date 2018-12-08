b8330adca4cc9b626b0b402bde56d0835ffaab26
Dolphin Oracle <dolphinoracle@gmail.com>
2018-12-08 11:08:29 -0500

    added mx18 artwork updated antiX kernel entries

---
+++ mx18-artwork.pm
@@ -0,0 +1,68 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Wallpapers
+</category>
+
+<name>
+MX 18 Wallpapers
+</name>
+
+<description>
+   <am>backgrounds originally supplied with MX 18</am>
+   <ca>backgrounds originally supplied with MX 18</ca>
+   <cs>backgrounds originally supplied with MX 18</cs>
+   <da>backgrounds originally supplied with MX 18</da>
+   <de>Bildschirmhintergründe, die ursprünglich mit MX 18 geliefert wurden</de>
+   <el>επιφάνεια εργασίας που παρέχεται αρχικά με MX 18</el>
+   <en>backgrounds originally supplied with MX 18</en>
+   <es>backgrounds originally supplied with MX 18</es>
+   <fi>backgrounds originally supplied with MX 18</fi>
+   <fr>Fonds d'écran MX 18</fr>
+   <hi>backgrounds originally supplied with MX 18</hi>
+   <hr>backgrounds originally supplied with MX 18</hr>
+   <hu>backgrounds originally supplied with MX 18</hu>
+   <is>backgrounds originally supplied with MX 18</is>
+   <it>sfondi originariamente forniti con MX 18</it>
+   <ja>backgrounds originally supplied with MX 18</ja>
+   <kk>backgrounds originally supplied with MX 18</kk>
+   <lt>backgrounds originally supplied with MX 18</lt>
+   <nl>achtergronden oorspronkelijk geleverd met MX 18</nl>
+   <pl>backgrounds originally supplied with MX 18</pl>
+   <pt_BR>backgrounds originally supplied with MX 18</pt_BR>
+   <pt>Imagens de fundo originalmente incluídas no MX 18</pt>
+   <ro>backgrounds originally supplied with MX 18</ro>
+   <ru>Обои использованные для дистрибутива MX 18</ru>
+   <sk>backgrounds originally supplied with MX 18</sk>
+   <sq>backgrounds originally supplied with MX 18</sq>
+   <sv>wallpapers ursprungligen medföljande MX 18</sv>
+   <tr>backgrounds originally supplied with MX 18</tr>
+   <uk>backgrounds originally supplied with MX 18</uk>
+   <zh_TW>backgrounds originally supplied with MX 18</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+mx18-artwork
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+mx18-artwork
+</uninstall_package_names>
+</app>
