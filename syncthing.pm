b2459740cd5bcfd3a7ae2e186f0a5b5bfdb4acf7
Dolphin Oracle <dolphinoracle@gmail.com>
2019-04-14 17:25:08 -0400

    modify syncthing pm

--- syncthing.pm
+++ syncthing.pm
@@ -6,54 +6,54 @@ Network
 </category>
 
 <name>
-Syncthing.net
+Syncthing
 </name>
 
 <description>
-   <am>syncthing.net file sync utility</am>
-   <ar>syncthing.net file sync utility</ar>
-   <bg>syncthing.net file sync utility</bg>
-   <ca>Utilitat de sincronització de fitxers amb syncthing.net</ca>
-   <cs>syncthing.net file sync utility</cs>
-   <da>syncthing.net-filsynkroniseringsredskab</da>
-   <de>syncthing.net Dateisynchronisationsprogramm</de>
-   <el>Βοηθητικό πρόγραμμα συγχρονισμού αρχείων syncthing.net</el>
-   <en>syncthing.net file sync utility</en>
-   <es>Sincronizar archivos con syncthing.net</es>
-   <et>syncthing.net file sync utility</et>
-   <eu>syncthing.net file sync utility</eu>
-   <fa>syncthing.net file sync utility</fa>
-   <fi>syncthing.net file sync utility</fi>
-   <fr>Utilitaire de synchronisation de fichier syncthing.net</fr>
-   <he_IL>syncthing.net file sync utility</he_IL>
-   <hi>syncthing.net file sync utility</hi>
-   <hr>syncthing.net file sync utility</hr>
-   <hu>syncthing.net file sync utility</hu>
-   <id>syncthing.net file sync utility</id>
-   <is>syncthing.net file sync utility</is>
+   <am>Syncthing file sync utility</am>
+   <ar>Syncthing file sync utility</ar>
+   <bg>Syncthing file sync utility</bg>
+   <ca>Utilitat de sincronització de fitxers amb Syncthing</ca>
+   <cs>Syncthing file sync utility</cs>
+   <da>Syncthing-filsynkroniseringsredskab</da>
+   <de>Syncthing Dateisynchronisationsprogramm</de>
+   <el>Βοηθητικό πρόγραμμα συγχρονισμού αρχείων Syncthing</el>
+   <en>Syncthing file sync utility</en>
+   <es>Sincronizar archivos con Syncthing</es>
+   <et>Syncthing file sync utility</et>
+   <eu>Syncthing file sync utility</eu>
+   <fa>Syncthing file sync utility</fa>
+   <fi>Syncthing file sync utility</fi>
+   <fr>Utilitaire de synchronisation de fichier Syncthing</fr>
+   <he_IL>Syncthing file sync utility</he_IL>
+   <hi>Syncthing file sync utility</hi>
+   <hr>Syncthing file sync utility</hr>
+   <hu>Syncthing file sync utility</hu>
+   <id>Syncthing file sync utility</id>
+   <is>Syncthing file sync utility</is>
    <it>Utility di sincronizzazione dei file</it>
-   <ja_JP>syncthing.net file sync utility</ja_JP>
-   <ja>syncthing.net file sync utility</ja>
-   <kk>syncthing.net file sync utility</kk>
-   <ko>syncthing.net file sync utility</ko>
-   <lt>syncthing.net failų sinchronizavimo paslaugų programa</lt>
-   <mk>syncthing.net file sync utility</mk>
-   <nb>syncthing.net file sync utility</nb>
-   <nl>syncthing.net bestand synchronisatie gereedschap</nl>
-   <pl>narzędzie synchronizacji plików syncthing.net</pl>
-   <pt_BR>syncthing.net file sync utility</pt_BR>
+   <ja_JP>Syncthing file sync utility</ja_JP>
+   <ja>Syncthing file sync utility</ja>
+   <kk>Syncthing file sync utility</kk>
+   <ko>Syncthing file sync utility</ko>
+   <lt>Syncthing failų sinchronizavimo paslaugų programa</lt>
+   <mk>Syncthing file sync utility</mk>
+   <nb>Syncthing file sync utility</nb>
+   <nl>Syncthing bestand synchronisatie gereedschap</nl>
+   <pl>narzędzie synchronizacji plików Syncthing</pl>
+   <pt_BR>Syncthing file sync utility</pt_BR>
    <pt>Utilitário de armazenagem e sincronização de ficheiros</pt>
-   <ro>syncthing.net file sync utility</ro>
-   <ru>Утилита синхронизации файлов с syncthing.net</ru>
-   <sk>syncthing.net file sync utility</sk>
-   <sl>syncthing.net file sync utility</sl>
-   <sq>syncthing.net file sync utility</sq>
-   <sr>syncthing.net file sync utility</sr>
-   <sv>syncthing.net filsynkroniserings-redskap</sv>
-   <tr>syncthing.net file sync utility</tr>
-   <uk>утиліта синхронізації файлів syncthing.net</uk>
-   <zh_CN>syncthing.net file sync utility</zh_CN>
-   <zh_TW>syncthing.net file sync utility</zh_TW>
+   <ro>Syncthing file sync utility</ro>
+   <ru>Утилита синхронизации файлов с Syncthing</ru>
+   <sk>Syncthing file sync utility</sk>
+   <sl>Syncthing file sync utility</sl>
+   <sq>Syncthing file sync utility</sq>
+   <sr>Syncthing file sync utility</sr>
+   <sv>Syncthing filsynkroniserings-redskap</sv>
+   <tr>Syncthing file sync utility</tr>
+   <uk>утиліта синхронізації файлів Syncthing</uk>
+   <zh_CN>Syncthing file sync utility</zh_CN>
+   <zh_TW>Syncthing file sync utility</zh_TW>
 </description>
 
 <installable>
