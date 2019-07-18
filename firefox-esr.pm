37d86be9c8197ca193795454262af825acbdcbf7
Dolphin Oracle <dolphinoracle@gmail.com>
2019-07-17 23:11:18 -0400

    add firefox-esr

---
+++ firefox-esr.pm
@@ -0,0 +1,81 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Browser
+</category>
+
+<name>
+Firefox-ESR
+</name>
+
+<description>
+   <am>Latest Firefox-ESR</am>
+   <ar>Latest Firefox-ESR</ar>
+   <bg>Latest Firefox-ESR</bg>
+   <ca>Darrer Firefox-ESR</ca>
+   <cs>Latest Firefox-ESR</cs>
+   <da>Seneste Firefox-ESR</da>
+   <de>Neuester Firefox-ESR</de>
+   <el>Τελευταία Firefox-ESR</el>
+   <en>Latest Firefox-ESR</en>
+   <es>Firefox-ESR actual</es>
+   <et>Latest Firefox-ESR</et>
+   <eu>Latest Firefox-ESR</eu>
+   <fa>Latest Firefox-ESR</fa>
+   <fi>Viimeisin Firefox-ESR-versio</fi>
+   <fr>La dernière version de Firefox-ESR</fr>
+   <he_IL>Latest Firefox-ESR</he_IL>
+   <hi>Latest Firefox-ESR</hi>
+   <hr>Latest Firefox-ESR</hr>
+   <hu>Latest Firefox-ESR</hu>
+   <id>Latest Firefox-ESR</id>
+   <is>Latest Firefox-ESR</is>
+   <it>Ultima versione di Firefox-ESR</it>
+   <ja_JP>Latest Firefox-ESR</ja_JP>
+   <ja>Latest Firefox-ESR</ja>
+   <kk>Latest Firefox-ESR</kk>
+   <ko>Latest Firefox-ESR</ko>
+   <lt>Naujausia Firefox-ESR</lt>
+   <mk>Latest Firefox-ESR</mk>
+   <nb>Latest Firefox-ESR</nb>
+   <nl>Meest recente Firefox-ESR</nl>
+   <pl>najnowszy Firefox-ESR</pl>
+   <pt_BR>Latest Firefox-ESR</pt_BR>
+   <pt>Latest Firefox-ESR</pt>
+   <ro>Latest Firefox-ESR</ro>
+   <ru>Браузер Firefox-ESR последней версии</ru>
+   <sk>Latest Firefox-ESR</sk>
+   <sl>Latest Firefox-ESR</sl>
+   <sq>Latest Firefox-ESR</sq>
+   <sr>Latest Firefox-ESR</sr>
+   <sv>Senaste Firefox-ESR</sv>
+   <tr>Latest Firefox-ESR</tr>
+   <uk>Крайній Firefox-ESR</uk>
+   <zh_CN>Latest Firefox-ESR</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/screenshots/000/014/721/large.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+firefox-esr
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+firefox-esr
+</uninstall_package_names>
+</app>
