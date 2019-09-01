777cfea1ffbdae77a02dc2a02a4f34ddcbedbaf6
Dolphin Oracle <dolphinoracle@gmail.com>
2019-08-31 21:33:29 -0400

    added redshift

---
+++ redshift.pm
@@ -0,0 +1,84 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Utility
+</category>
+
+<name>
+Redshift
+</name>
+
+<description>
+   <am>Transition monitor color temperature by time of day</am>
+   <ar>Transition monitor color temperature by time of day</ar>
+   <bg>Transition monitor color temperature by time of day</bg>
+   <ca>Transition monitor color temperature by time of day</ca>
+   <cs>Transition monitor color temperature by time of day</cs>
+   <da>Transition monitor color temperature by time of day</da>
+   <de>Transition monitor color temperature by time of day</de>
+   <el>Transition monitor color temperature by time of day</el>
+   <en>Transition monitor color temperature by time of day</en>
+   <es>Transition monitor color temperature by time of day</es>
+   <et>Transition monitor color temperature by time of day</et>
+   <eu>Transition monitor color temperature by time of day</eu>
+   <fa>Transition monitor color temperature by time of day</fa>
+   <fi>Transition monitor color temperature by time of day</fi>
+   <fr>Transition monitor color temperature by time of day</fr>
+   <he_IL>Transition monitor color temperature by time of day</he_IL>
+   <hi>Transition monitor color temperature by time of day</hi>
+   <hr>Transition monitor color temperature by time of day</hr>
+   <hu>Transition monitor color temperature by time of day</hu>
+   <id>Transition monitor color temperature by time of day</id>
+   <is>Transition monitor color temperature by time of day</is>
+   <it>Transition monitor color temperature by time of day</it>
+   <ja_JP>Transition monitor color temperature by time of day</ja_JP>
+   <ja>Transition monitor color temperature by time of day</ja>
+   <kk>Transition monitor color temperature by time of day</kk>
+   <ko>Transition monitor color temperature by time of day</ko>
+   <lt>Transition monitor color temperature by time of day</lt>
+   <mk>Transition monitor color temperature by time of day</mk>
+   <nb>Transition monitor color temperature by time of day</nb>
+   <nl>Transition monitor color temperature by time of day</nl>
+   <pl>Transition monitor color temperature by time of day</pl>
+   <pt_BR>Transition monitor color temperature by time of day</pt_BR>
+   <pt>Transition monitor color temperature by time of day</pt>
+   <ro>Transition monitor color temperature by time of day</ro>
+   <ru>Transition monitor color temperature by time of day</ru>
+   <sk>Transition monitor color temperature by time of day</sk>
+   <sl>Transition monitor color temperature by time of day</sl>
+   <sq>Transition monitor color temperature by time of day</sq>
+   <sr>Transition monitor color temperature by time of day</sr>
+   <sv>Transition monitor color temperature by time of day</sv>
+   <tr>Transition monitor color temperature by time of day</tr>
+   <uk>Transition monitor color temperature by time of day</uk>
+   <zh_CN>Transition monitor color temperature by time of day</zh_CN>
+   <zh_TW>Transition monitor color temperature by time of day</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+redshift
+redshift-gtk
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+redshift
+redshift-gtk
+</uninstall_package_names>
+</app>
