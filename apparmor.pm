b90928ef4e2ef861dd4d58ae8705cdb2497b21bf
Dolphin Oracle <dolphinoracle@gmail.com>
2019-10-03 19:52:30 -0400

    update kernel entries add apparmor

---
+++ apparmor.pm
@@ -0,0 +1,86 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Utility
+</category>
+
+<name>
+apparmor
+</name>
+
+<description>
+   <am>Userspace components of AppArmor</am>
+   <ar>Userspace components of AppArmor</ar>
+   <bg>Userspace components of AppArmor</bg>
+   <ca>Userspace components of AppArmor</ca>
+   <cs>Userspace components of AppArmor</cs>
+   <da>Userspace components of AppArmor</da>
+   <de>Userspace components of AppArmor</de>
+   <el>Userspace components of AppArmor</el>
+   <en>Userspace components of AppArmor </en>
+   <es>Userspace components of AppArmor</es>
+   <et>Userspace components of AppArmor</et>
+   <eu>Userspace components of AppArmor</eu>
+   <fa>Userspace components of AppArmor</fa>
+   <fi>Userspace components of AppArmor</fi>
+   <fr>Userspace components of AppArmor</fr>
+   <he_IL>Userspace components of AppArmor</he_IL>
+   <hi>Userspace components of AppArmor</hi>
+   <hr>Userspace components of AppArmor</hr>
+   <hu>Userspace components of AppArmor</hu>
+   <id>Userspace components of AppArmor</id>
+   <is>Userspace components of AppArmor</is>
+   <it>Userspace components of AppArmor</it>
+   <ja_JP>Userspace components of AppArmor</ja_JP>
+   <ja>Userspace components of AppArmor</ja>
+   <kk>Userspace components of AppArmor</kk>
+   <ko>Userspace components of AppArmor</ko>
+   <lt>Userspace components of AppArmor</lt>
+   <mk>Userspace components of AppArmor</mk>
+   <mr>Userspace components of AppArmor</mr>
+   <nb>Userspace components of AppArmor</nb>
+   <nl>Userspace components of AppArmor</nl>
+   <pl>Userspace components of AppArmor</pl>
+   <pt_BR>Userspace components of AppArmor</pt_BR>
+   <pt>Userspace components of AppArmor</pt>
+   <ro>Userspace components of AppArmor</ro>
+   <ru>Userspace components of AppArmor</ru>
+   <sk>Userspace components of AppArmor</sk>
+   <sl>Userspace components of AppArmor</sl>
+   <sq>Userspace components of AppArmor</sq>
+   <sr>Userspace components of AppArmor</sr>
+   <sv>Userspace components of AppArmor</sv>
+   <tr>Userspace components of AppArmor</tr>
+   <uk>Userspace components of AppArmor</uk>
+   <zh_CN>Userspace components of AppArmor</zh_CN>
+   <zh_TW>Userspace components of AppArmor</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+apparmor
+apparmor-profiles
+apparmor-utils
+</install_package_names>
+
+
+<postinstall>
+</postinstall>
+
+
+<uninstall_package_names>
+apparmor
+apparmor-profiles
+apparmor-utils
+</uninstall_package_names>
+</app>
--- kernel_liquorix_32_pae.pm
+++ kernel_liquorix_32_pae.pm
@@ -10,51 +10,51 @@ Liquorix 32 bit pae
 </name>
 
 <description>
-   <am>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</am>
-   <ar>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</ar>
-   <bg>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</bg>
-   <ca>Kernel Liquorix (PAE, darrer 4.x del dipòsit MX TEST)</ca>
-   <cs>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</cs>
-   <da>Liquorix-kerne (PAE, seneste 4.x fra MX TEST-softwarekilde)</da>
-   <de>Liquorix Kernel (PAE, neueste Version 4.x von MX TEST repo)</de>
-   <el>Liquorix Kernel (PAE, τελευταία 4.x από το MX TEST repo)</el>
-   <en>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</en>
-   <es>Kernel Liquorix (PAE, último 4.x del repositorio MX TEST)</es>
-   <et>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</et>
-   <eu>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</eu>
-   <fa>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</fa>
-   <fi>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</fi>
-   <fr>Noyau Liquorix (PAE, dernier 4.x du dépôt de test MX)</fr>
-   <he_IL>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</he_IL>
-   <hi>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</hi>
-   <hr>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</hr>
-   <hu>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</hu>
-   <id>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</id>
-   <is>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</is>
-   <it>Liquorix Kernel (PAE, ultimo 4.x da MX TEST repo)</it>
-   <ja_JP>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</ja_JP>
-   <ja>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</ja>
-   <kk>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</kk>
-   <ko>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</ko>
-   <lt>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</lt>
-   <mk>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</mk>
-   <mr>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</mr>
-   <nb>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</nb>
-   <nl>Liquorix Kernel (PAE, laatste 4.x vanuit MX TEST pakketbron)</nl>
-   <pl>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</pl>
-   <pt_BR>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</pt_BR>
-   <pt>Núcleo Liquorix (PAE, 4.x mais recente no MX TEST repo)</pt>
-   <ro>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</ro>
-   <ru>Liquorix ядро (PAE, последнее 4.x из репозитория MX TEST)</ru>
-   <sk>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</sk>
-   <sl>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</sl>
-   <sq>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</sq>
-   <sr>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</sr>
-   <sv>Liquorix Kärna (PAE, senaste 4.x från MX TEST repo)</sv>
-   <tr>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</tr>
-   <uk>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</uk>
-   <zh_CN>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</zh_CN>
-   <zh_TW>Liquorix Kernel (PAE, latest 4.x from MX TEST repo)</zh_TW>
+   <am>Liquorix Kernel (PAE, latest from MX TEST repo)</am>
+   <ar>Liquorix Kernel (PAE, latest from MX TEST repo)</ar>
+   <bg>Liquorix Kernel (PAE, latest from MX TEST repo)</bg>
+   <ca>Kernel Liquorix (PAE, darrer del dipòsit MX TEST)</ca>
+   <cs>Liquorix Kernel (PAE, latest from MX TEST repo)</cs>
+   <da>Liquorix-kerne (PAE, seneste fra MX TEST-softwarekilde)</da>
+   <de>Liquorix Kernel (PAE, neueste Version von MX TEST repo)</de>
+   <el>Liquorix Kernel (PAE, τελευταία από το MX TEST repo)</el>
+   <en>Liquorix Kernel (PAE, latest from MX TEST repo)</en>
+   <es>Kernel Liquorix (PAE, último del repositorio MX TEST)</es>
+   <et>Liquorix Kernel (PAE, latest from MX TEST repo)</et>
+   <eu>Liquorix Kernel (PAE, latest from MX TEST repo)</eu>
+   <fa>Liquorix Kernel (PAE, latest from MX TEST repo)</fa>
+   <fi>Liquorix Kernel (PAE, latest from MX TEST repo)</fi>
+   <fr>Noyau Liquorix (PAE, dernier du dépôt de test MX)</fr>
+   <he_IL>Liquorix Kernel (PAE, latest from MX TEST repo)</he_IL>
+   <hi>Liquorix Kernel (PAE, latest from MX TEST repo)</hi>
+   <hr>Liquorix Kernel (PAE, latest from MX TEST repo)</hr>
+   <hu>Liquorix Kernel (PAE, latest from MX TEST repo)</hu>
+   <id>Liquorix Kernel (PAE, latest from MX TEST repo)</id>
+   <is>Liquorix Kernel (PAE, latest from MX TEST repo)</is>
+   <it>Liquorix Kernel (PAE, ultimo da MX TEST repo)</it>
+   <ja_JP>Liquorix Kernel (PAE, latest from MX TEST repo)</ja_JP>
+   <ja>Liquorix Kernel (PAE, latest from MX TEST repo)</ja>
+   <kk>Liquorix Kernel (PAE, latest from MX TEST repo)</kk>
+   <ko>Liquorix Kernel (PAE, latest from MX TEST repo)</ko>
+   <lt>Liquorix Kernel (PAE, latest from MX TEST repo)</lt>
+   <mk>Liquorix Kernel (PAE, latest from MX TEST repo)</mk>
+   <mr>Liquorix Kernel (PAE, latest from MX TEST repo)</mr>
+   <nb>Liquorix Kernel (PAE, latest from MX TEST repo)</nb>
+   <nl>Liquorix Kernel (PAE, laatste vanuit MX TEST pakketbron)</nl>
+   <pl>Liquorix Kernel (PAE, latest from MX TEST repo)</pl>
+   <pt_BR>Liquorix Kernel (PAE, latest from MX TEST repo)</pt_BR>
+   <pt>Núcleo Liquorix (PAE, mais recente no MX TEST repo)</pt>
+   <ro>Liquorix Kernel (PAE, latest from MX TEST repo)</ro>
+   <ru>Liquorix ядро (PAE, последнее из репозитория MX TEST)</ru>
+   <sk>Liquorix Kernel (PAE, latest from MX TEST repo)</sk>
+   <sl>Liquorix Kernel (PAE, latest from MX TEST repo)</sl>
+   <sq>Liquorix Kernel (PAE, latest from MX TEST repo)</sq>
+   <sr>Liquorix Kernel (PAE, latest from MX TEST repo)</sr>
+   <sv>Liquorix Kärna (PAE, senaste från MX TEST repo)</sv>
+   <tr>Liquorix Kernel (PAE, latest from MX TEST repo)</tr>
+   <uk>Liquorix Kernel (PAE, latest from MX TEST repo)</uk>
+   <zh_CN>Liquorix Kernel (PAE, latest from MX TEST repo)</zh_CN>
+   <zh_TW>Liquorix Kernel (PAE, latest from MX TEST repo)</zh_TW>
 </description>
 
 <installable>
