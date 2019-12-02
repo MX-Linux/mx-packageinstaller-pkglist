5ef55830435272f357505015cd0a384e2e1bfa8d
Dolphin Oracle <dolphinoracle@gmail.com>
2019-12-01 19:57:13 -0500

    add mxfluxbox entry in window managers

---
+++ mxfluxbox.pm
@@ -0,0 +1,87 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Window Managers
+</category>
+
+<name>
+Fluxbox (with MX-flux settings)
+</name>
+
+<description>
+   <am>flubox - after installation use mx-flux in menu to finish</am>
+   <ar>flubox - after installation use mx-flux in menu to finish</ar>
+   <bg>flubox - after installation use mx-flux in menu to finish</bg>
+   <ca>flubox - after installation use mx-flux in menu to finish</ca>
+   <cs>flubox - after installation use mx-flux in menu to finish</cs>
+   <da>flubox - after installation use mx-flux in menu to finish</da>
+   <de>flubox - after installation use mx-flux in menu to finish</de>
+   <el>flubox - after installation use mx-flux in menu to finish</el>
+   <en>flubox - after installation use mx-flux in menu to finish</en>
+   <es>Entorno de escritorio ligero</es>
+   <et>flubox - after installation use mx-flux in menu to finish</et>
+   <eu>flubox - after installation use mx-flux in menu to finish</eu>
+   <fa>flubox - after installation use mx-flux in menu to finish</fa>
+   <fi>flubox - after installation use mx-flux in menu to finish</fi>
+   <fr>flubox - after installation use mx-flux in menu to finish</fr>
+   <he_IL>flubox - after installation use mx-flux in menu to finish</he_IL>
+   <hi>flubox - after installation use mx-flux in menu to finish</hi>
+   <hr>flubox - after installation use mx-flux in menu to finish</hr>
+   <hu>flubox - after installation use mx-flux in menu to finish</hu>
+   <id>flubox - after installation use mx-flux in menu to finish</id>
+   <is>flubox - after installation use mx-flux in menu to finish</is>
+   <it>flubox - after installation use mx-flux in menu to finish</it>
+   <ja_JP>flubox - after installation use mx-flux in menu to finish</ja_JP>
+   <ja>flubox - after installation use mx-flux in menu to finish</ja>
+   <kk>flubox - after installation use mx-flux in menu to finish</kk>
+   <ko>flubox - after installation use mx-flux in menu to finish</ko>
+   <lt>flubox - after installation use mx-flux in menu to finish</lt>
+   <mk>flubox - after installation use mx-flux in menu to finish</mk>
+   <mr>flubox - after installation use mx-flux in menu to finish</mr>
+   <nb>flubox - after installation use mx-flux in menu to finish</nb>
+   <nl>flubox - after installation use mx-flux in menu to finish</nl>
+   <pl>flubox - after installation use mx-flux in menu to finish</pl>
+   <pt_BR>flubox - after installation use mx-flux in menu to finish</pt_BR>
+   <pt>flubox - after installation use mx-flux in menu to finish</pt>
+   <ro>flubox - after installation use mx-flux in menu to finish</ro>
+   <ru>flubox - after installation use mx-flux in menu to finish</ru>
+   <sk>flubox - after installation use mx-flux in menu to finish</sk>
+   <sl>flubox - after installation use mx-flux in menu to finish</sl>
+   <sq>flubox - after installation use mx-flux in menu to finish</sq>
+   <sr>flubox - after installation use mx-flux in menu to finish</sr>
+   <sv>flubox - after installation use mx-flux in menu to finish</sv>
+   <tr>flubox - after installation use mx-flux in menu to finish</tr>
+   <uk>flubox - after installation use mx-flux in menu to finish</uk>
+   <zh_CN>flubox - after installation use mx-flux in menu to finish</zh_CN>
+   <zh_TW>flubox - after installation use mx-flux in menu to finish</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot></screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+fluxbox
+idesk
+mx-fluxbox
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+fluxbox
+idesk
+mx-fluxbox
+</uninstall_package_names>
+</app>
