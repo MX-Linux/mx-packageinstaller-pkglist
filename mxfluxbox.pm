37a33d346b333f0369773045374eeb6d161cc37d
Dolphin Oracle <dolphinoracle@gmail.com>
2019-12-22 19:05:56 -0500

    update mxfluxbox.pm

--- mxfluxbox.pm
+++ mxfluxbox.pm
@@ -10,54 +10,51 @@ Fluxbox (with MX-flux settings)
 </name>
 
 <description>
-   <am>fluxbox - after installation use mx-flux in menu to finish</am>
-   <ar>fluxbox - after installation use mx-flux in menu to finish</ar>
-   <bg>fluxbox - after installation use mx-flux in menu to finish</bg>
-   <bn>fluxbox - after installation use mx-flux in menu to finish</bn>
-   <ca>Fluxbox - després de la instal·lació useu mx-flux al menú per acabar</ca>
-   <cs>fluxbox - after installation use mx-flux in menu to finish</cs>
-   <da>fluxbox - after installation use mx-flux in menu to finish</da>
-   <de>fluxbox - nach Installation mx-flux im Menü zum Fertigstellen benutzen</de>
-   <el>fluxbox - μετά την εγκατάσταση, χρησιμοποιήστε το mx-flux στο μενού για να ολοκληρώσετε</el>
-   <en>fluxbox - after installation use mx-flux in menu to finish</en>
-   <es>fluxbox - después de la instalación use mx-flux en el menú para terminar</es>
-   <et>fluxbox - after installation use mx-flux in menu to finish</et>
-   <eu>fluxbox - after installation use mx-flux in menu to finish</eu>
-   <fa>fluxbox - after installation use mx-flux in menu to finish</fa>
-   <fil_PH>fluxbox - after installation use mx-flux in menu to finish</fil_PH>
-   <fi>fluxbox - after installation use mx-flux in menu to finish</fi>
-   <fr>fluxbox - after installation use mx-flux in menu to finish</fr>
-   <he_IL>fluxbox - after installation use mx-flux in menu to finish</he_IL>
-   <hi>fluxbox - after installation use mx-flux in menu to finish</hi>
-   <hr>fluxbox - after installation use mx-flux in menu to finish</hr>
-   <hu>fluxbox - after installation use mx-flux in menu to finish</hu>
-   <id>fluxbox - after installation use mx-flux in menu to finish</id>
-   <is>fluxbox - after installation use mx-flux in menu to finish</is>
-   <it>fluxbox - dopo l'installazione, per terminare, usare mx-flux presente nel menu</it>
-   <ja_JP>fluxbox - after installation use mx-flux in menu to finish</ja_JP>
-   <ja>fluxbox - after installation use mx-flux in menu to finish</ja>
-   <kk>fluxbox - after installation use mx-flux in menu to finish</kk>
-   <ko>fluxbox - after installation use mx-flux in menu to finish</ko>
-   <lt>fluxbox - after installation use mx-flux in menu to finish</lt>
-   <mk>fluxbox - after installation use mx-flux in menu to finish</mk>
-   <mr>fluxbox - after installation use mx-flux in menu to finish</mr>
-   <nb>fluxbox - after installation use mx-flux in menu to finish</nb>
-   <nl>fluxbox - after installation use mx-flux in menu to finish</nl>
-   <pl>fluxbox - po instalacji użyj mx-flux w menu, aby zakończyć</pl>
-   <pt_BR>fluxbox - after installation use mx-flux in menu to finish</pt_BR>
-   <pt>fluxbox - after installation use mx-flux in menu to finish</pt>
-   <ro>fluxbox - after installation use mx-flux in menu to finish</ro>
-   <ru>fluxbox - после установки используйте для завершения mx-flux в меню</ru>
-   <sk>fluxbox - after installation use mx-flux in menu to finish</sk>
-   <sl>fluxbox - po namestitvi za dokončanje uporabite mx-flux v meniju</sl>
-   <sq>fluxbox - after installation use mx-flux in menu to finish</sq>
-   <sr>fluxbox - after installation use mx-flux in menu to finish</sr>
-   <sv>fluxbox - efter installation använd mx-flux i menyn för att avsluta</sv>
-   <tr>fluxbox - after installation use mx-flux in menu to finish</tr>
-   <uk>fluxbox - after installation use mx-flux in menu to finish</uk>
-   <vi>fluxbox - after installation use mx-flux in menu to finish</vi>
-   <zh_CN>fluxbox - after installation use mx-flux in menu to finish</zh_CN>
-   <zh_TW>fluxbox - after installation use mx-flux in menu to finish</zh_TW>
+   <am>fluxbox</am>
+   <ar>fluxbox</ar>
+   <bg>fluxbox</bg>
+   <ca>fluxbox</ca>
+   <cs>fluxbox</cs>
+   <da>fluxbox</da>
+   <de>fluxbox</de>
+   <el>fluxbox</el>
+   <en>fluxbox</en>
+   <es>fluxbox</es>
+   <et>fluxbox</et>
+   <eu>fluxbox</eu>
+   <fa>fluxbox</fa>
+   <fi>fluxbox</fi>
+   <fr>fluxbox</fr>
+   <he_IL>fluxbox</he_IL>
+   <hi>fluxbox</hi>
+   <hr>fluxbox</hr>
+   <hu>fluxbox</hu>
+   <id>fluxbox</id>
+   <is>fluxbox</is>
+   <it>fluxbox</it>
+   <ja_JP>fluxbox</ja_JP>
+   <ja>fluxbox</ja>
+   <kk>fluxbox</kk>
+   <ko>fluxbox</ko>
+   <lt>fluxbox</lt>
+   <mk>fluxbox</mk>
+   <mr>fluxbox</mr>
+   <nb>fluxbox</nb>
+   <nl>fluxbox</nl>
+   <pl>fluxbox</pl>
+   <pt_BR>fluxbox</pt_BR>
+   <pt>fluxbox</pt>
+   <ro>fluxbox</ro>
+   <ru>fluxbox</ru>
+   <sk>fluxbox</sk>
+   <sl>fluxbox</sl>
+   <sq>fluxbox</sq>
+   <sr>fluxbox</sr>
+   <sv>fluxbox</sv>
+   <tr>fluxbox</tr>
+   <uk>fluxbox</uk>
+   <zh_CN>fluxbox</zh_CN>
+   <zh_TW>fluxbox</zh_TW>
 </description>
 
 <installable>
@@ -79,7 +76,7 @@ gkrellm
 
 
 <postinstall>
-
+sudo -H -u $(logname) /usr/bin/mxflux_install.sh
 </postinstall>
 
 
