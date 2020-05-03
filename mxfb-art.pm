4af18a9c93e9304aa04c71a630affd02f7060017
Dolphin Oracle <dolphinoracle@gmail.com>
2020-05-03 15:26:58 -0400

    added mxfb-art fixed lxde

---
+++ mxfb-art.pm
@@ -0,0 +1,85 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Wallpapers
+</category>
+
+<name>
+MX flubox Wallpapers
+</name>
+
+<description>
+   <am>wallpapers with mx fluxbox logo</am>
+   <ar>wallpapers with mx fluxbox logo</ar>
+   <bg>wallpapers with mx fluxbox logo</bg>
+   <bn>wallpapers with mx fluxbox logo</bn>
+   <ca>wallpapers with mx fluxbox logo</ca>
+   <cs>wallpapers with mx fluxbox logo</cs>
+   <da>wallpapers with mx fluxbox logo</da>
+   <de>wallpapers with mx fluxbox logo</de>
+   <el>wallpapers with mx fluxbox logo</el>
+   <en>wallpapers with mx fluxbox logo</en>
+   <es>wallpapers with mx fluxbox logo</es>
+   <et>wallpapers with mx fluxbox logo</et>
+   <eu>wallpapers with mx fluxbox logo</eu>
+   <fa>wallpapers with mx fluxbox logo</fa>
+   <fil_PH>wallpapers with mx fluxbox logo</fil_PH>
+   <fi>wallpapers with mx fluxbox logo</fi>
+   <fr>wallpapers with mx fluxbox logo</fr>
+   <he_IL>wallpapers with mx fluxbox logo</he_IL>
+   <hi>wallpapers with mx fluxbox logo</hi>
+   <hr>wallpapers with mx fluxbox logo</hr>
+   <hu>wallpapers with mx fluxbox logo</hu>
+   <id>wallpapers with mx fluxbox logo</id>
+   <is>wallpapers with mx fluxbox logo</is>
+   <it>wallpapers with mx fluxbox logo</it>
+   <ja>wallpapers with mx fluxbox logo</ja>
+   <kk>wallpapers with mx fluxbox logo</kk>
+   <ko>wallpapers with mx fluxbox logo</ko>
+   <lt>wallpapers with mx fluxbox logo</lt>
+   <mk>wallpapers with mx fluxbox logo</mk>
+   <mr>wallpapers with mx fluxbox logo</mr>
+   <nb>wallpapers with mx fluxbox logo</nb>
+   <nl>achtergronden oorspronkelijk geleverd met MX 18</nl>
+   <pl>wallpapers with mx fluxbox logo</pl>
+   <pt_BR>wallpapers with mx fluxbox logo</pt_BR>
+   <pt>wallpapers with mx fluxbox logo</pt>
+   <ro>wallpapers with mx fluxbox logo</ro>
+   <ru>wallpapers with mx fluxbox logo</ru>
+   <sk>wallpapers with mx fluxbox logo</sk>
+   <sl>wallpapers with mx fluxbox logo</sl>
+   <sq>wallpapers with mx fluxbox logo</sq>
+   <sr>wallpapers with mx fluxbox logo</sr>
+   <sv>wallpapers with mx fluxbox logo</sv>
+   <tr>wallpapers with mx fluxbox logo</tr>
+   <uk>wallpapers with mx fluxbox logo</uk>
+   <vi>wallpapers with mx fluxbox logo</vi>
+   <zh_CN>wallpapers with mx fluxbox logo</zh_CN>
+   <zh_TW>wallpapers with mx fluxbox logo</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+mxfb-art
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+mxfb-art
+</uninstall_package_names>
+</app>
