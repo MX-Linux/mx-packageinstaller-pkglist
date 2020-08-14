9f64268d7d7f7afaaba09076f2738f99466a3597
Dolphin Oracle <dolphinoracle@gmail.com>
2020-08-13 20:10:05 -0400

    add krita, kmail

---
+++ krita.pm
@@ -0,0 +1,139 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Graphics
+</category>
+
+<name>
+Krita
+</name>
+
+<description>
+   <am>Krita is a creative application for raster images</am>
+   <ar>Krita is a creative application for raster images</ar>
+   <bg>Krita is a creative application for raster images</bg>
+   <bn>Krita is a creative application for raster images</bn>
+   <ca>Krita is a creative application for raster images</ca>
+   <cs>Krita is a creative application for raster images</cs>
+   <da>Krita is a creative application for raster images</da>
+   <de>Krita is a creative application for raster images</de>
+   <el>Krita is a creative application for raster images</el>
+   <en>Krita is a creative application for raster images</en>
+   <es>Krita is a creative application for raster images</es>
+   <et>Krita is a creative application for raster images</et>
+   <eu>Krita is a creative application for raster images</eu>
+   <fa>Krita is a creative application for raster images</fa>
+   <fil_PH>Krita is a creative application for raster images</fil_PH>
+   <fi>Krita is a creative application for raster images</fi>
+   <fr>Krita is a creative application for raster images</fr>
+   <he_IL>Krita is a creative application for raster images</he_IL>
+   <hi>Krita is a creative application for raster images</hi>
+   <hr>Krita is a creative application for raster images</hr>
+   <hu>Krita is a creative application for raster images</hu>
+   <id>Krita is a creative application for raster images</id>
+   <is>Krita is a creative application for raster images</is>
+   <it>Krita is a creative application for raster images</it>
+   <ja_JP>Krita is a creative application for raster images</ja_JP>
+   <ja>Krita is a creative application for raster images</ja>
+   <kk>Krita is a creative application for raster images</kk>
+   <ko>Krita is a creative application for raster images</ko>
+   <lt>Krita is a creative application for raster images</lt>
+   <mk>Krita is a creative application for raster images</mk>
+   <mr>Krita is a creative application for raster images</mr>
+   <nb>Krita is a creative application for raster images</nb>
+   <nl>Krita is a creative application for raster images</nl>
+   <pl>Krita is a creative application for raster images</pl>
+   <pt_BR>Krita is a creative application for raster images</pt_BR>
+   <pt>Krita is a creative application for raster images</pt>
+   <ro>Krita is a creative application for raster images</ro>
+   <ru>Krita is a creative application for raster images</ru>
+   <sk>Krita is a creative application for raster images</sk>
+   <sl>Krita is a creative application for raster images</sl>
+   <sq>Krita is a creative application for raster images</sq>
+   <sr>Krita is a creative application for raster images</sr>
+   <sv>Krita is a creative application for raster images</sv>
+   <tr>Krita is a creative application for raster images</tr>
+   <uk>Krita is a creative application for raster images</uk>
+   <vi>Krita is a creative application for raster images</vi>
+   <zh_CN>Krita is a creative application for raster images</zh_CN>
+   <zh_TW>Krita is a creative application for raster images</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/screenshots/000/017/088/large.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+krita
+krita-gmic
+krita-data
+krita-l10n
+gdal-data 
+libaec0 
+libarmadillo9 
+libarpack2 
+libcharls2
+libdap25
+libdapclient6v5 
+libdapserver7v5 
+libepsilon1 
+libfreexl1 
+libfyba0 
+libgdal20 
+libgdcm2.8
+libgeos-3.7.1 
+libgeos-c1v5 
+libgeotiff2 
+libgmic1 
+libgraphicsmagick++-q16-12
+libgraphicsmagick-q16-3 
+libhdf4-0-alt 
+libhdf5-103 
+libkmlbase1 
+libkmlconvenience1 
+libkmldom1
+libkmlengine1 
+libkmlregionator1 
+libkmlxsd1 
+libnetcdf13 
+libodbc1 
+libogdi3.2 
+libopencolorio1v5
+libopencv-imgcodecs3.2 
+libopencv-videoio3.2 
+libpq5 
+libproj13 
+libqhull7 
+libsocket++1
+libspatialite7 
+libsuperlu5 
+libsz2 
+libtinyxml2.6.2v5 
+liburiparser1 
+libyaml-cpp0.6 
+odbcinst
+odbcinst1debian2 
+proj-bin 
+proj-data
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+krita
+krita-l10n
+krita-gmic
+krita-data
+</uninstall_package_names>
+</app>
