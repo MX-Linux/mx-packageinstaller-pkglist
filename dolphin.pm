e53b5de31fb87006bcc8363c18694f640e8df3d1
Dolphin Oracle <dolphinoracle@gmail.com>
2020-09-22 07:04:30 -0400

    add some file managers

---
+++ dolphin.pm
@@ -0,0 +1,88 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+FileManagers
+</category>
+
+<name>
+Dolphin
+</name>
+
+<description>
+   <am> default file manager from the KDE plasma desktop</am>
+   <ar> default file manager from the KDE plasma desktop</ar>
+   <bg> default file manager from the KDE plasma desktop</bg>
+   <bn> default file manager from the KDE plasma desktop</bn>
+   <ca> default file manager from the KDE plasma desktop</ca>
+   <cs> default file manager from the KDE plasma desktop</cs>
+   <da> default file manager from the KDE plasma desktop</da>
+   <de> default file manager from the KDE plasma desktop</de>
+   <el> default file manager from the KDE plasma desktop</el>
+   <en> default file manager from the KDE plasma desktop</en>
+   <es> default file manager from the KDE plasma desktop</es>
+   <et> default file manager from the KDE plasma desktop</et>
+   <eu> default file manager from the KDE plasma desktop</eu>
+   <fa> default file manager from the KDE plasma desktop</fa>
+   <fil_PH> default file manager from the KDE plasma desktop</fil_PH>
+   <fi> default file manager from the KDE plasma desktop</fi>
+   <fr> default file manager from the KDE plasma desktop</fr>
+   <he_IL> default file manager from the KDE plasma desktop</he_IL>
+   <hi> default file manager from the KDE plasma desktop</hi>
+   <hr> default file manager from the KDE plasma desktop</hr>
+   <hu> default file manager from the KDE plasma desktop</hu>
+   <id> default file manager from the KDE plasma desktop</id>
+   <is> default file manager from the KDE plasma desktop</is>
+   <it> default file manager from the KDE plasma desktop</it>
+   <ja_JP> default file manager from the KDE plasma desktop</ja_JP>
+   <ja> default file manager from the KDE plasma desktop</ja>
+   <kk> default file manager from the KDE plasma desktop</kk>
+   <ko> default file manager from the KDE plasma desktop</ko>
+   <lt> default file manager from the KDE plasma desktop</lt>
+   <mk> default file manager from the KDE plasma desktop</mk>
+   <mr> default file manager from the KDE plasma desktop</mr>
+   <nb> default file manager from the KDE plasma desktop</nb>
+   <nl> default file manager from the KDE plasma desktop</nl>
+   <pl> default file manager from the KDE plasma desktop</pl>
+   <pt_BR> default file manager from the KDE plasma desktop</pt_BR>
+   <pt> default file manager from the KDE plasma desktop</pt>
+   <ro> default file manager from the KDE plasma desktop</ro>
+   <ru> default file manager from the KDE plasma desktop</ru>
+   <sk> default file manager from the KDE plasma desktop</sk>
+   <sl> default file manager from the KDE plasma desktop</sl>
+   <sq> default file manager from the KDE plasma desktop</sq>
+   <sr> default file manager from the KDE plasma desktop</sr>
+   <sv> default file manager from the KDE plasma desktop</sv>
+   <tr> default file manager from the KDE plasma desktop</tr>
+   <uk> default file manager from the KDE plasma desktop</uk>
+   <vi> default file manager from the KDE plasma desktop</vi>
+   <zh_CN> default file manager from the KDE plasma desktop</zh_CN>
+   <zh_TW> default file manager from the KDE plasma desktop</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/screenshots/000/016/631/large.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+dolphin
+kde-servicemenu-rootactions
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+dolphin
+kde-servicemenu-rootactions
+</uninstall_package_names>
+</app>
---
+++ sunflower.pm
@@ -0,0 +1,86 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+FileManagers
+</category>
+
+<name>
+Sunflower
+</name>
+
+<description>
+   <am> Small and highly customizable twin-panel file manager</am>
+   <ar> Small and highly customizable twin-panel file manager</ar>
+   <bg> Small and highly customizable twin-panel file manager</bg>
+   <bn> Small and highly customizable twin-panel file manager</bn>
+   <ca> Small and highly customizable twin-panel file manager</ca>
+   <cs> Small and highly customizable twin-panel file manager</cs>
+   <da> Small and highly customizable twin-panel file manager</da>
+   <de> Small and highly customizable twin-panel file manager</de>
+   <el> Small and highly customizable twin-panel file manager</el>
+   <en> Small and highly customizable twin-panel file manager</en>
+   <es> Small and highly customizable twin-panel file manager</es>
+   <et> Small and highly customizable twin-panel file manager</et>
+   <eu> Small and highly customizable twin-panel file manager</eu>
+   <fa> Small and highly customizable twin-panel file manager</fa>
+   <fil_PH> Small and highly customizable twin-panel file manager</fil_PH>
+   <fi> Small and highly customizable twin-panel file manager</fi>
+   <fr> Small and highly customizable twin-panel file manager</fr>
+   <he_IL> Small and highly customizable twin-panel file manager</he_IL>
+   <hi> Small and highly customizable twin-panel file manager</hi>
+   <hr> Small and highly customizable twin-panel file manager</hr>
+   <hu> Small and highly customizable twin-panel file manager</hu>
+   <id> Small and highly customizable twin-panel file manager</id>
+   <is> Small and highly customizable twin-panel file manager</is>
+   <it> Small and highly customizable twin-panel file manager</it>
+   <ja_JP> Small and highly customizable twin-panel file manager</ja_JP>
+   <ja> Small and highly customizable twin-panel file manager</ja>
+   <kk> Small and highly customizable twin-panel file manager</kk>
+   <ko> Small and highly customizable twin-panel file manager</ko>
+   <lt> Small and highly customizable twin-panel file manager</lt>
+   <mk> Small and highly customizable twin-panel file manager</mk>
+   <mr> Small and highly customizable twin-panel file manager</mr>
+   <nb> Small and highly customizable twin-panel file manager</nb>
+   <nl> Small and highly customizable twin-panel file manager</nl>
+   <pl> Small and highly customizable twin-panel file manager</pl>
+   <pt_BR> Small and highly customizable twin-panel file manager</pt_BR>
+   <pt> Small and highly customizable twin-panel file manager</pt>
+   <ro> Small and highly customizable twin-panel file manager</ro>
+   <ru> Small and highly customizable twin-panel file manager</ru>
+   <sk> Small and highly customizable twin-panel file manager</sk>
+   <sl> Small and highly customizable twin-panel file manager</sl>
+   <sq> Small and highly customizable twin-panel file manager</sq>
+   <sr> Small and highly customizable twin-panel file manager</sr>
+   <sv> Small and highly customizable twin-panel file manager</sv>
+   <tr> Small and highly customizable twin-panel file manager</tr>
+   <uk> Small and highly customizable twin-panel file manager</uk>
+   <vi> Small and highly customizable twin-panel file manager</vi>
+   <zh_CN> Small and highly customizable twin-panel file manager</zh_CN>
+   <zh_TW> Small and highly customizable twin-panel file manager</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://sunflower-fm.org/site/gallery/images/5f9b38108c926a6730000f57c81372a8.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+sunflower
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+sunflower
+</uninstall_package_names>
+</app>
