f1dda310f5cfcccf915a2d3b7e885a126520b056
Dolphin Oracle <dolphinoracle@gmail.com>
2020-11-08 16:02:14 -0500

    revise rebuild_dkms script

--- bin/rebuild_dkms_packages.sh
+++ bin/rebuild_dkms_packages.sh
@@ -3,12 +3,21 @@
 #force rebuild of dkms packages (virtualbox, nvidia, fglrx, broadcom, ndiswrapper)
 #part of mx-packageinstaller
 
-
 [[ $(dpkg-query --show $1 2>/dev/null && echo $?) ]] || exit 0
 
+kernel="$1"
 
-for i in $(dpkg-query -l |grep "\-dkms" |awk '{print $2'}); do
- dpkg-reconfigure $i |tee -a /var/log/rebuild-dkms.log
-done
+#if no kernel parameter given, assume current kernel
+if [ -z "$kernel" ]; then
+	kernel="$(uname -r)"
+fi
+
+kernel=${kernel#"linux-image-"}
+kernel=${kernel%-unsigned}
+
+echo "update dkms modules for kernel: " $kernel |tee /var/log/rebuild-dkms.log
+echo "see log at /var/log/rebuild-dkms.log"
+
+/usr/lib/dkms/dkms_autoinstaller start $kernel |tee -a /var/log/rebuild-dkms.log
 
 exit 0
