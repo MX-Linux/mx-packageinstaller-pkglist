48bf088039c3202dcd269ca328318f8e151520e6
Dolphin Oracle <dolphinoracle@gmail.com>
2020-11-09 09:22:58 -0500

    Update kernel_liquorix_32_nopae.pm

--- kernel_liquorix_32_nopae.pm
+++ kernel_liquorix_32_nopae.pm
@@ -82,7 +82,6 @@ file=$(apt-cache show linux-image-liquorix-686 |grep -m1 Depends)
 file=${file#"Depends:"}
 rebuild_dkms_packages.sh $file
 rm -f /etc/apt/sources.list.d/mxpitemp.list
-rebuild_dkms_packages.sh linux-image-liquorix-686
 apt-get -y remove linux-image-liquorix-686 linux-headers-liquorix-686
 apt-get update
 
