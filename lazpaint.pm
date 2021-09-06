b00b822267f9c04f1642f39b65df419c1cb323cd
Dolphin Oracle <dolphinoracle@gmail.com>
2021-09-06 11:45:08 -0400

    add lazpaint, archive non-working kernel entries

---
+++ lazpaint.pm
@@ -0,0 +1,86 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Graphics
+</category>
+
+<name>
+Lazpaint
+</name>
+
+<description>
+   <am>Պատկերի խմբագիր `ռաստերային և վեկտորային շերտերով</am>
+   <ar>محرر الصور مع الطبقات النقطية والمتجهة</ar>
+   <bg>Редактор на изображения с растерни и векторни слоеве</bg>
+   <bn>Uređivač slika sa rasterskim i vektorskim slojevima</bn>
+   <ca>Editor d'imatges amb capes ràster i vectorials</ca>
+   <cs>Obrazový editor s rastrovými a vektorovými vrstvami</cs>
+   <da>Billededitor med raster- og vektorlag</da>
+   <de>Bildeditor mit Raster- und Vektorebenen</de>
+   <el>Επεξεργαστής εικόνας με ράστερ και διανυσματικά επίπεδα</el>
+   <en>Image editor with raster and  vector layers</en>
+   <es>Editor de imágenes con capas raster y vectoriales</es>
+   <et>Pildiredaktor raster- ja vektorkihtidega</et>
+   <eu>Irudi editorea raster eta bektore geruzekin</eu>
+   <fa>ویرایشگر تصویر با لایه های شطرنجی و بردار</fa>
+   <fil_PH>Editor ng imahe na may mga layer ng raster at vector</fil_PH>
+   <fi>Kuvankäsittelyohjelma, jossa on rasteri- ja vektorikerroksia</fi>
+   <fr>Éditeur d'images avec calques raster et vectoriels</fr>
+   <he_IL>עורך תמונות עם שכבות סריקה וקטור</he_IL>
+   <hi>रेखापुंज और वेक्टर परतों के साथ छवि संपादक</hi>
+   <hr>Uređivač slika s rasterskim i vektorskim slojevima</hr>
+   <hu>Képszerkesztő raszteres és vektoros rétegekkel</hu>
+   <id>Editor gambar dengan lapisan raster dan vektor</id>
+   <is>Myndritstjóri með raster- og vektorlagi</is>
+   <it>Editor di immagini con livelli raster e vettoriali</it>
+   <ja_JP>ラスターレイヤーとベクターレイヤーを備えた画像エディター</ja_JP>
+   <ja>ラスターレイヤーとベクターレイヤーを備えた画像エディター</ja>
+   <kk>Растрлық және векторлық қабаттары бар кескін редакторы</kk>
+   <ko>래스터 및 벡터 레이어가 있는 이미지 편집기</ko>
+   <lt>Vaizdo redaktorius su rastriniais ir vektoriniais sluoksniais</lt>
+   <mk>Уредувач на слики со растерски и векторски слоеви</mk>
+   <mr>रास्टर आणि वेक्टर स्तरांसह प्रतिमा संपादक</mr>
+   <nb>Bilderedigerer med raster- og vektorlag</nb>
+   <nl>Afbeeldingseditor met raster- en vectorlagen</nl>
+   <pl>Edytor obrazów z warstwami rastrowymi i wektorowymi</pl>
+   <pt_BR>Editor de imagens com camadas raster e vetoriais</pt_BR>
+   <pt>Editor de imagens com camadas raster e vetoriais</pt>
+   <ro>Editor de imagini cu straturi raster și vectoriale</ro>
+   <ru>Редактор изображений с растровыми и векторными слоями</ru>
+   <sk>Obrazový editor s rastrovými a vektorovými vrstvami</sk>
+   <sl>Urejevalnik slik z rastrskimi in vektorskimi plastmi</sl>
+   <sq>Redaktori i imazhit me shtresa rasterike dhe vektoriale</sq>
+   <sr>Уређивач слика са растерским и векторским слојевима</sr>
+   <sv>Bildredigerare med raster- och vektorlager</sv>
+   <tr>Raster ve vektör katmanlarına sahip görüntü düzenleyici</tr>
+   <uk>Редактор зображень з растровими та векторними шарами</uk>
+   <vi>Trình chỉnh sửa hình ảnh với các lớp raster và vector</vi>
+   <zh_CN>帶有光柵和矢量圖層的圖像編輯器</zh_CN>
+   <zh_TW>带有光栅和矢量图层的图像编辑器</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/19519/simage/large-b6770670a808c18e497ab222ed10a13c.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+lazpaint-qt5
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+lazpaint-qt5
+</uninstall_package_names>
+</app>
