c7daef982d3932b809751aed620c89e2d5a972dd
Dolphin Oracle <dolphinoracle@gmail.com>
2021-09-09 20:32:59 -0400

    remove muon, add xfce4-screenshooter

---
+++ xfce4-screenshooter.pm
@@ -0,0 +1,88 @@
+<?xml version="1.0"?>
+<app>
+
+
+<category>
+Screenshot Utilities
+</category>
+
+<name>
+xfce4-screenshot
+</name>
+
+<description>
+   <am>սքրինշոթ կոմունալ</am>
+   <ar>فائدة لقطة الشاشة</ar>
+   <bg>помощна програма за скрийншот</bg>
+   <bn>uslužni program za snimanje ekrana</bn>
+   <ca>utilitat de captura de pantalla</ca>
+   <cs>nástroj pro snímky obrazovky</cs>
+   <da>værktøj til skærmbillede</da>
+   <de>Screenshot-Dienstprogramm</de>
+   <el>βοηθητικό πρόγραμμα screenshot</el>
+   <en>xfce4 screenshot utility</en>
+   <es>ekraanipiltide utiliit</es>
+   <et>Qt (not kde) -based screenshot tool inspired by the Windows Snipping Tools</et>
+   <eu>pantaila-argazkia</eu>
+   <fa>ابزار اسکرین شات</fa>
+   <fil_PH>utility sa screenshot</fil_PH>
+   <fi>kuvakaappausapuohjelma</fi>
+   <fr>utilitaire de capture d'écran</fr>
+   <he_IL>כלי צילום מסך</he_IL>
+   <hi>स्क्रीनशॉट उपयोगिता</hi>
+   <hr>uslužni program za snimanje zaslona</hr>
+   <hu>screenshot segédprogram</hu>
+   <id>utilitas tangkapan layar</id>
+   <is>gagnsemi skjámynda</is>
+   <it>utilità di screenshot</it>
+   <ja_JP>スクリーンショットユーティリティ</ja_JP>
+   <ja>スクリーンショットユーティリティ</ja>
+   <kk>скриншоттың қызметтік бағдарламасы</kk>
+   <ko>스크린샷 유틸리티</ko>
+   <lt>ekrano kopijos programa</lt>
+   <mk>алатка за слики од екранот</mk>
+   <mr>स्क्रीनशॉट उपयुक्तता</mr>
+   <nb>skjermbildeverktøy</nb>
+   <nl>screenshot-hulpprogramma</nl>
+   <pl>narzędzie do zrzutów ekranu</pl>
+   <pt_BR>utilitário de captura de tela</pt_BR>
+   <pt>utilitário de captura de tela</pt>
+   <ro>utilitar de captură de ecran</ro>
+   <ru>утилита для создания скриншотов</ru>
+   <sk>nástroj na vytváranie snímok obrazovky</sk>
+   <sl>pripomoček za posnetek zaslona</sl>
+   <sq>dobi e screenshot -it</sq>
+   <sr>услужни програм за снимање екрана</sr>
+   <sv>skärmdumpverktyg</sv>
+   <tr>ekran görüntüsü yardımcı programı</tr>
+   <uk>утиліта скріншота</uk>
+   <vi>tiện ích ảnh chụp màn hình</vi>
+   <zh_CN>截圖實用程序</zh_CN>
+   <zh_TW>截图实用程序</zh_TW>
+</description>
+
+<installable>
+32,64
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/18224/simage/large-79439b41c283123c786813c0a56e7450.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+xfce4-screenshooter
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+xfce4-screenshooter
+</uninstall_package_names>
+
+</app>
