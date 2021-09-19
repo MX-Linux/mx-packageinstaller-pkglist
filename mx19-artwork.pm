fc4e46c542ff140394234e54c03008f03e47696d
Dolphin Oracle <dolphinoracle@gmail.com>
2021-09-18 21:34:16 -0400

    add mx19-artwork

---
+++ mx19-artwork.pm
@@ -0,0 +1,86 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Wallpapers
+</category>
+
+<name>
+MX 19 Wallpapers
+</name>
+
+<description>
+   <am>backgrounds originally supplied with MX 19</am>
+   <ar>backgrounds originally supplied with MX 19</ar>
+   <bg>backgrounds originally supplied with MX 19</bg>
+   <bn>backgrounds originally supplied with MX 19</bn>
+   <ca>Fons de pantalla subministrats amb MX 19</ca>
+   <cs>backgrounds originally supplied with MX 19</cs>
+   <da>baggrunde fra MX 19</da>
+   <de>Bildschirmhintergründe, die ursprünglich mit MX 19 geliefert wurden</de>
+   <el>επιφάνεια εργασίας που παρέχεται αρχικά με MX 19</el>
+   <en>backgrounds originally supplied with MX 19</en>
+   <es>Fondos de escritorio suministrados originalmente con MX 19</es>
+   <et>backgrounds originally supplied with MX 19</et>
+   <eu>backgrounds originally supplied with MX 19</eu>
+   <fa>backgrounds originally supplied with MX 19</fa>
+   <fil_PH>backgrounds originally supplied with MX 19</fil_PH>
+   <fi>backgrounds originally supplied with MX 19</fi>
+   <fr>Fonds d'écran initialement fournis avec MX 19</fr>
+   <he_IL>backgrounds originally supplied with MX 19</he_IL>
+   <hi>backgrounds originally supplied with MX 19</hi>
+   <hr>backgrounds originally supplied with MX 19</hr>
+   <hu>backgrounds originally supplied with MX 19</hu>
+   <id>backgrounds originally supplied with MX 19</id>
+   <is>backgrounds originally supplied with MX 19</is>
+   <it>sfondi originariamente forniti con MX 19</it>
+   <ja_JP>backgrounds originally supplied with MX 19</ja_JP>
+   <ja>backgrounds originally supplied with MX 19</ja>
+   <kk>backgrounds originally supplied with MX 19</kk>
+   <ko>backgrounds originally supplied with MX 19</ko>
+   <lt>backgrounds originally supplied with MX 19</lt>
+   <mk>backgrounds originally supplied with MX 19</mk>
+   <mr>backgrounds originally supplied with MX 19</mr>
+   <nb>backgrounds originally supplied with MX 19</nb>
+   <nl>achtergronden oorspronkelijk geleverd met MX 19</nl>
+   <pl>tła oryginalnie dostarczone z MX 19</pl>
+   <pt_BR>Imagens de fundo originalmente incluídas no MX 19</pt_BR>
+   <pt>Imagens de fundo originalmente incluídas no MX 19</pt>
+   <ro>backgrounds originally supplied with MX 19</ro>
+   <ru>Обои первоначально входившие в MX 19</ru>
+   <sk>backgrounds originally supplied with MX 19</sk>
+   <sl>Izvirna ozadja za MX 19</sl>
+   <sq>backgrounds originally supplied with MX 19</sq>
+   <sr>backgrounds originally supplied with MX 19</sr>
+   <sv>wallpapers ursprungligen medföljande MX 19</sv>
+   <tr>backgrounds originally supplied with MX 19</tr>
+   <uk>backgrounds originally supplied with MX 19</uk>
+   <vi>backgrounds originally supplied with MX 19</vi>
+   <zh_CN>backgrounds originally supplied with MX 19</zh_CN>
+   <zh_TW>backgrounds originally supplied with MX 19</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+mx19-artwork
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+mx19-artwork
+</uninstall_package_names>
+</app>
