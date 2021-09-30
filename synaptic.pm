6d1725cd045941e45df0619548a8d8a59af66383
Dolphin Oracle <dolphinoracle@gmail.com>
2021-09-30 19:47:15 -0400

    add synaptic to popular apps

---
+++ synaptic.pm
@@ -0,0 +1,86 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Utility
+</category>
+
+<name>
+Synaptic Package Manager
+</name>
+
+<description>
+   <am>የግራፊክ ጥቅል አስተዳዳሪ</am>
+   <ar>مدير الحزم الرسومية</ar>
+   <bg>графичен мениджър на пакети</bg>
+   <bn>grafički menadžer paketa</bn>
+   <ca>gestor de paquets gràfics</ca>
+   <cs>grafický správce balíčků</cs>
+   <da>grafisk pakkeleder</da>
+   <de>grafischer Paketmanager</de>
+   <el>γραφικός διαχειριστής πακέτων</el>
+   <en>graphical package manager</en>
+   <es>administrador de paquetes gráficos</es>
+   <et>graafiline paketihaldur</et>
+   <eu>pakete kudeatzaile grafikoa</eu>
+   <fa>مدیر بسته های گرافیکی</fa>
+   <fil_PH>graphic manager ng package</fil_PH>
+   <fi>graafinen paketinhallinta</fi>
+   <fr>gestionnaire de paquets graphique</fr>
+   <he_IL>מנהל חבילות גרפי</he_IL>
+   <hi>ग्राफिकल पैकेज मैनेजर</hi>
+   <hr>grafički upravitelj paketa</hr>
+   <hu>grafikus csomagkezelő</hu>
+   <id>manajer paket grafis</id>
+   <is>grafískur pakkastjóri</is>
+   <it>gestore di pacchetti grafico</it>
+   <ja_JP>グラフィカルパッケージマネージャー</ja_JP>
+   <ja>グラフィカルパッケージマネージャー</ja>
+   <kk>графикалық пакет менеджері</kk>
+   <ko>그래픽 패키지 관리자</ko>
+   <lt>grafinis paketų tvarkytojas</lt>
+   <mk>менаџер на графички пакети</mk>
+   <mr>ग्राफिकल पॅकेज व्यवस्थापक</mr>
+   <nb>grafisk pakkeleder</nb>
+   <nl>grafische pakketbeheerder</nl>
+   <pl>graficzny menedżer pakietów</pl>
+   <pt_BR>gerenciador de pacote gráfico</pt_BR>
+   <pt>gerenciador de pacote gráfico</pt>
+   <ro>manager grafic de pachete</ro>
+   <ru>графический менеджер пакетов</ru>
+   <sk>grafický správca balíkov</sk>
+   <sl>grafični upravitelj paketov</sl>
+   <sq>menaxher grafik i paketave</sq>
+   <sr>графички менаџер пакета</sr>
+   <sv>grafisk pakethanterare</sv>
+   <tr>grafik paket yöneticisi</tr>
+   <uk>графічний менеджер пакетів</uk>
+   <vi>trình quản lý gói đồ họa</vi>
+   <zh_CN>圖形包管理器</zh_CN>
+   <zh_TW>图形包管理器</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/15300/simage/large-3b6f5869dede0492cebad905125b7550.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+synaptic
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+synaptic
+</uninstall_package_names>
+</app>
