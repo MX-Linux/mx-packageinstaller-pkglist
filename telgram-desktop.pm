addcf162a5a12f3ba8bf5b16765621f85ed682a0
Dolphin Oracle <dolphinoracle@gmail.com>
2021-10-05 18:27:16 -0400

    add telegram

---
+++ telgram-desktop.pm
@@ -0,0 +1,90 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Messaging
+</category>
+
+<name>
+Telegram Desktop Client
+</name>
+
+<description>
+   <am>Telegram Desktop Client</am>
+   <ar>Telegram Desktop Client</ar>
+   <bg>Telegram Desktop Client</bg>
+   <bn>Telegram Desktop Client</bn>
+   <ca>Telegram Desktop Client</ca>
+   <cs>Telegram Desktop Client</cs>
+   <da>Telegram Desktop Client</da>
+   <de>Telegram Desktop Client</de>
+   <el>Telegram Desktop Client</el>
+   <en>Telegram Desktop Client</en>
+   <es>Telegram Desktop Client</es>
+   <et>Telegram Desktop Client</et>
+   <eu>Telegram Desktop Client</eu>
+   <fa>Telegram Desktop Client</fa>
+   <fil_PH>Telegram Desktop Client</fil_PH>
+   <fi>Telegram Desktop Client</fi>
+   <fr>Telegram Desktop Client</fr>
+   <he_IL>Telegram Desktop Client</he_IL>
+   <hi>Telegram Desktop Client</hi>
+   <hr>Telegram Desktop Client</hr>
+   <hu>Telegram Desktop Client</hu>
+   <id>Telegram Desktop Client</id>
+   <is>Telegram Desktop Client</is>
+   <it>Telegram Desktop Client</it>
+   <ja_JP>Telegram Desktop Client</ja_JP>
+   <ja>Telegram Desktop Client</ja>
+   <kk>Telegram Desktop Client</kk>
+   <ko>Telegram Desktop Client</ko>
+   <lt>Telegram Desktop Client</lt>
+   <mk>Telegram Desktop Client</mk>
+   <mr>Telegram Desktop Client</mr>
+   <nb>Telegram Desktop Client</nb>
+   <nl>Telegram Desktop Client</nl>
+   <pl>Telegram Desktop Client</pl>
+   <pt_BR>Telegram Desktop Client</pt_BR>
+   <pt>Telegram Desktop Client</pt>
+   <ro>Telegram Desktop Client</ro>
+   <ru>Telegram Desktop Client</ru>
+   <sk>Telegram Desktop Client</sk>
+   <sl>Telegram Desktop Client</sl>
+   <sq>Telegram Desktop Client</sq>
+   <sr>Telegram Desktop Client</sr>
+   <sv>Telegram Desktop Client</sv>
+   <tr>Telegram Desktop Client</tr>
+   <uk>Telegram Desktop Client</uk>
+   <vi>Telegram Desktop Client</vi>
+   <zh_CN>Telegram Desktop Client</zh_CN>
+   <zh_TW>Telegram Desktop Client</zh_TW>
+</description>
+
+<installable>
+64
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/16680/simage/large-0fd665fe199a1e24fcbe3390976edca6.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+telegram-desktop
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+telegram-desktop
+</uninstall_package_names>
+
+<postuninstall>
+
+</postuninstall>
+</app>
