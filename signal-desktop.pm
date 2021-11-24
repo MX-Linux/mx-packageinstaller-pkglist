008ef334f5eeda7337cdbfd21ea7df5713e4a323
Dolphin Oracle <dolphinoracle@gmail.com>
2021-11-23 22:46:10 -0500

    update signal

--- signal-desktop.pm
+++ signal-desktop.pm
@@ -68,7 +68,7 @@ Signal Desktop Client
 
 <preinstall>
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | apt-key add -
-echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" |tee -a /etc/apt/sources.list.d/signal-xenial-added-by-mxpi.list
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main">/etc/apt/sources.list.d/signal-xenial-added-by-mxpi.list
 apt-get update
 </preinstall>
 
