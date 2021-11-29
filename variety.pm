456702fbecbbcdf9d68b9f52ccfb2da4af5fdd77
Dolphin Oracle <dolphinoracle@gmail.com>
2021-11-28 23:23:43 -0500

    add variety

---
+++ variety.pm
@@ -0,0 +1,87 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Utility
+</category>
+
+<name>
+Variety
+</name>
+
+<description>
+   <am>wallpaper changer and manager</am>
+   <ar>wallpaper changer and manager</ar>
+   <bg>wallpaper changer and manager</bg>
+   <bn>wallpaper changer and manager</bn>
+   <ca>wallpaper changer and manager</ca>
+   <cs>wallpaper changer and manager</cs>
+   <da>wallpaper changer and manager</da>
+   <de>wallpaper changer and manager</de>
+   <el>wallpaper changer and manager</el>
+   <en>wallpaper changer and manager</en>
+   <es>wallpaper changer and manager</es>
+   <et>wallpaper changer and manager</et>
+   <eu>wallpaper changer and manager</eu>
+   <fa>wallpaper changer and manager</fa>
+   <fil_PH>wallpaper changer and manager</fil_PH>
+   <fi>wallpaper changer and manager</fi>
+   <fr>wallpaper changer and manager</fr>
+   <he_IL>wallpaper changer and manager</he_IL>
+   <hi>wallpaper changer and manager</hi>
+   <hr>wallpaper changer and manager</hr>
+   <hu>wallpaper changer and manager</hu>
+   <id>wallpaper changer and manager</id>
+   <is>wallpaper changer and manager</is>
+   <it>wallpaper changer and manager</it>
+   <ja_JP>wallpaper changer and manager</ja_JP>
+   <ja>wallpaper changer and manager</ja>
+   <kk>wallpaper changer and manager</kk>
+   <ko>wallpaper changer and manager</ko>
+   <lt>wallpaper changer and manager</lt>
+   <mk>wallpaper changer and manager</mk>
+   <mr>wallpaper changer and manager</mr>
+   <nb>wallpaper changer and manager</nb>
+   <nl>wallpaper changer and manager</nl>
+   <pl>wallpaper changer and manager</pl>
+   <pt_BR>wallpaper changer and manager</pt_BR>
+   <pt>wallpaper changer and manager</pt>
+   <ro>wallpaper changer and manager</ro>
+   <ru>wallpaper changer and manager</ru>
+   <sk>wallpaper changer and manager</sk>
+   <sl>wallpaper changer and manager</sl>
+   <sq>wallpaper changer and manager</sq>
+   <sr>wallpaper changer and manager</sr>
+   <sv>wallpaper changer and manager</sv>
+   <tr>wallpaper changer and manager</tr>
+   <uk>wallpaper changer and manager</uk>
+   <vi>wallpaper changer and manager</vi>
+   <zh_CN>wallpaper changer and manager</zh_CN>
+   <zh_TW>wallpaper changer and manager</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/15095/simage/large-df7ab13a8aa66dc8b807949ead8b1d98.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+variety
+python3-httplib2
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+variety
+</uninstall_package_names>
+</app>
