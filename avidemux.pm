a50aff3113a612b857650f865a0de0e912058db9
Dolphin Oracle <dolphinoracle@gmail.com>
2022-01-18 18:15:52 -0500

    add avidemux

---
+++ avidemux.pm
@@ -0,0 +1,86 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Video
+</category>
+
+<name>
+Avidemux
+</name>
+
+<description>
+   <am>the avidemux video editor</am>
+   <ar>the avidemux video editor</ar>
+   <bg>the avidemux video editor</bg>
+   <bn>the avidemux video editor</bn>
+   <ca>Editor de vídeo de avidemux</ca>
+   <cs>the avidemux video editor</cs>
+   <da>avidemux-videoredigeringen</da>
+   <de>Der avidemux-Video-Editor</de>
+   <el>τον επεξεργαστή βίντεο του avidemux</el>
+   <en>the avidemux video editor</en>
+   <es>Editor de video de avidemux</es>
+   <et>the avidemux video editor</et>
+   <eu>the avidemux video editor</eu>
+   <fa>the avidemux video editor</fa>
+   <fil_PH>the avidemux video editor</fil_PH>
+   <fi>the avidemux video editor</fi>
+   <fr>L'éditeur vidéo de avidemux</fr>
+   <he_IL>the avidemux video editor</he_IL>
+   <hi>the avidemux video editor</hi>
+   <hr>the avidemux video editor</hr>
+   <hu>the avidemux video editor</hu>
+   <id>the avidemux video editor</id>
+   <is>the avidemux video editor</is>
+   <it>editor video dell'ambiente avidemux</it>
+   <ja_JP>the avidemux video editor</ja_JP>
+   <ja>the avidemux video editor</ja>
+   <kk>the avidemux video editor</kk>
+   <ko>the avidemux video editor</ko>
+   <lt>the avidemux video editor</lt>
+   <mk>the avidemux video editor</mk>
+   <mr>the avidemux video editor</mr>
+   <nb>the avidemux video editor</nb>
+   <nl>de avidemux video editor</nl>
+   <pl>edytor wideo avidemux</pl>
+   <pt_BR>O editor de vídeo do Ambiente de Trabalho avidemux</pt_BR>
+   <pt>O editor de vídeo do Ambiente de Trabalho avidemux</pt>
+   <ro>the avidemux video editor</ro>
+   <ru>Полупрофессиональный нелинейный видео редактор</ru>
+   <sk>the avidemux video editor</sk>
+   <sl>avidemux video urejevalnik</sl>
+   <sq>the avidemux video editor</sq>
+   <sr>the avidemux video editor</sr>
+   <sv>avidemux video redigerare</sv>
+   <tr>avidemux vidyo düzenleyici</tr>
+   <uk>КДЕ редактор відео</uk>
+   <vi>the avidemux video editor</vi>
+   <zh_CN>the avidemux video editor</zh_CN>
+   <zh_TW>the avidemux video editor</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/9191/simage/large-d636f93beb6b52c308742e71412cddda.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+avidemux
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+avidemux
+</uninstall_package_names>
+</app>
