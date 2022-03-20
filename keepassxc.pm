7c5d7bb063853dc0ca0e83d7245c12b2a6fc78db
fehlix <feh.ler@t-online.de>
2022-03-20 20:32:37 +0100

    added KeePassXC; removed KeePassX

---
+++ keepassxc.pm
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Misc
+</category>
+
+<name>
+KeePassXC
+</name>
+
+<description>
+   <am>KeePassXC is a free/open-source password manager/safe</am>
+   <ar>KeePassXC is a free/open-source password manager/safe</ar>
+   <bg>KeePassXC is a free/open-source password manager/safe</bg>
+   <bn>KeePassXC is a free/open-source password manager/safe</bn>
+   <ca>KeePassXC és un gestor de contrasenyes lliure/codi obert</ca>
+   <cs>KeePassXC is a free/open-source password manager/safe</cs>
+   <da>KeePassXC er en fri/open source adgangskodehåndtering/-skab</da>
+   <de>KeePassXC ist ein kostenloser/open-source Passwortmanager/-safe</de>
+   <el>Το KeePassXC είναι ένας διαχειριστής κωδικών πρόσβασης ελεύθερου/ανοιχτού κώδικα</el>
+   <en>KeePassXC is a free/open-source password manager/safe</en>
+   <es>Administrador de contraseñas de código abierto</es>
+   <et>KeePassXC is a free/open-source password manager/safe</et>
+   <eu>KeePassXC is a free/open-source password manager/safe</eu>
+   <fa>KeePassXC is a free/open-source password manager/safe</fa>
+   <fil_PH>KeePassXC is a free/open-source password manager/safe</fil_PH>
+   <fi>KeePassXC is a free/open-source password manager/safe</fi>
+   <fr>KeePassXC est un gestionnaire sécurisé open-source de mots de passe</fr>
+   <he_IL>KeePassXC is a free/open-source password manager/safe</he_IL>
+   <hi>KeePassXC is a free/open-source password manager/safe</hi>
+   <hr>KeePassXC is a free/open-source password manager/safe</hr>
+   <hu>KeePassXC is a free/open-source password manager/safe</hu>
+   <id>KeePassXC is a free/open-source password manager/safe</id>
+   <is>KeePassXC is a free/open-source password manager/safe</is>
+   <it>KeePassXC è un gestore di password free/open-source</it>
+   <ja_JP>KeePassXC is a free/open-source password manager/safe</ja_JP>
+   <ja>KeePassXC is a free/open-source password manager/safe</ja>
+   <kk>KeePassXC is a free/open-source password manager/safe</kk>
+   <ko>KeePassXC is a free/open-source password manager/safe</ko>
+   <lt>KeePassXC is a free/open-source password manager/safe</lt>
+   <mk>KeePassXC is a free/open-source password manager/safe</mk>
+   <mr>KeePassXC is a free/open-source password manager/safe</mr>
+   <nb>KeePassXC is a free/open-source password manager/safe</nb>
+   <nl>KeePassXC is een gratis/open-source wachtwoord manager/kluis</nl>
+   <pl>KeePassXC to bezpłatny menedżer haseł/sejf o otwartym kodzie źródłowym</pl>
+   <pt_BR>Guarda e gestão de senhas (palavras-passe) em software livre e de fonte aberta</pt_BR>
+   <pt>Guarda e gestão de senhas (palavras-passe) em software livre e de fonte aberta</pt>
+   <ro>KeePassXC is a free/open-source password manager/safe</ro>
+   <ru>KeePassXC - менеджер паролей с открытым кодом (форк KeePass)</ru>
+   <sk>KeePassXC is a free/open-source password manager/safe</sk>
+   <sl>KeePassXje brezplačen odprtokodni urejevalnik/sef za gesla</sl>
+   <sq>KeePassXC is a free/open-source password manager/safe</sq>
+   <sr>KeePassXC is a free/open-source password manager/safe</sr>
+   <sv>KeePassXC är en fri/öppen källkod lösenordshanterare/förvarare</sv>
+   <tr>KeePassXC is a free/open-source password manager/safe</tr>
+   <uk>KeePassXC вільний та відкритий менеджер і сховище паролів</uk>
+   <vi>KeePassXC is a free/open-source password manager/safe</vi>
+   <zh_CN>KeePassXC is a free/open-source password manager/safe</zh_CN>
+   <zh_TW>KeePassXC is a free/open-source password manager/safe</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/simage/large-d9d44a0cfc544b43f4a5442af1992cc0.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+keepassxc
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+keepassxc
+</uninstall_package_names>
+</app>
