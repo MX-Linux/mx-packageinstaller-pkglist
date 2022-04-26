c23249607482dfd40f1e968c705e5eb6c9a768a6
Dolphin Oracle <dolphinoracle@gmail.com>
2022-04-25 21:36:20 -0400

    virtualization category, new vm apps

---
+++ virt-manager.pm
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+
+<category>
+Virtualization
+</category>
+
+<name>
+Virt-manager (libvirt)
+</name>
+
+<description>
+<am>ምናባዊ ማሽኖችን ለማስተዳደር የዴስክቶፕ መተግበሪያ (libvirt)</am>
+<ar>ﺔﻴﺿﺍﺮﺘﻓﻻ﻿ﺍ ﺓﺰﻬﺟﻷ﻿ﺍ ﺓﺭﺍﺩﻹ﻿ ﺐﺘﻜﻤﻟﺍ ﺢﻄﺳ ﻖﻴﺒﻄﺗ (libvirt)</ar>
+<bg>настолно приложение за управление на виртуални машини (libvirt)</bg>
+<bn>ভার্চুয়াল মেশিন পরিচালনার জন্য ডেস্কটপ অ্যাপ্লিকেশন (libvirt)</bn>
+<ca>aplicació d'escriptori per a la gestió de màquines virtuals (libvirt)</ca>
+<cs>desktopová aplikace pro správu virtuálních strojů (libvirt)</cs>
+<da>desktop-applikation til styring af virtuelle maskiner (libvirt)</da>
+<de>Desktop-Anwendung zum Verwalten virtueller Maschinen (libvirt)</de>
+<el>εφαρμογή επιτραπέζιου υπολογιστή για τη διαχείριση εικονικών μηχανών (libvirt)</el>
+<es>aplicación de escritorio para administrar máquinas virtuales (libvirt)</es>
+<et>töölauarakendus virtuaalmasinate haldamiseks (libvirt)</et>
+<eu>makina birtualak kudeatzeko mahaigaineko aplikazioa (libvirt)</eu>
+<en>desktop application for managing virtual machines (libvirt)</en>
+<fa>ﯼﺯﺎﺠﻣ ﯼﺎﻫ ﻦﯿﺷﺎﻣ ﺖﯾﺮﯾﺪﻣ ﯼﺍﺮﺑ ﭖﺎﺘﮑﺳﺩ ﺭﺍﺰﻓﺍ ﻡﺮﻧ (libvirt)</fa>
+<fil_PH>desktop application para sa pamamahala ng mga virtual machine (libvirt)</fil_PH>
+<fi>työpöytäsovellus virtuaalikoneiden hallintaan (libvirt)</fi>
+<fr>application de bureau pour la gestion des machines virtuelles (libvirt)</fr>
+<he_IL>תוילאוטריו תונוכמ לוהינל ינחלוש םושיי (libvirt)</he_IL>
+<hi>वर्चुअल मशीन के प्रबंधन के लिए डेस्कटॉप एप्लिकेशन (libvirt)</hi>
+<hr>desktop aplikacija za upravljanje virtualnim strojevima (libvirt)</hr>
+<hu>asztali alkalmazás virtuális gépek kezelésére (libvirt)</hu>
+<id>aplikasi desktop untuk mengelola mesin virtual (libvirt)</id>
+<is>skrifborðsforrit til að stjórna sýndarvélum (libvirt)</is>
+<it>applicazione desktop per la gestione di macchine virtuali (libvirt)</it>
+<ja>仮想マシンを管理するためのデスクトップアプリケーション (libvirt)</ja>
+<kk>виртуалды машиналарды басқаруға арналған жұмыс үстелі қолданбасы (libvirt)</kk>
+<ko>가상 머신 관리를 위한 데스크톱 애플리케이션 (libvirt)</ko>
+<lt>darbalaukio programa, skirta virtualioms mašinoms valdyti (libvirt)</lt>
+<mk>десктоп апликација за управување со виртуелни машини (libvirt)</mk>
+<mr>आभासी मशीन व्यवस्थापित करण्यासाठी डेस्कटॉप अनुप्रयोग (libvirt)</mr>
+<nb>skrivebordsapplikasjon for å administrere virtuelle maskiner (libvirt)</nb>
+<nl>desktop-applicatie voor het beheren van virtuele machines (libvirt)</nl>
+<pl>aplikacja desktopowa do zarządzania maszynami wirtualnym (libvirt)</pl>
+<pt_BR>aplicativo de desktop para gerenciar máquinas virtuais (libvirt)</pt_BR>
+<pt>aplicativo de desktop para gerenciar máquinas virtuais (libvirt)</pt>
+<ro>aplicație desktop pentru gestionarea mașinilor virtuale (libvirt)</ro>
+<ru>настольное приложение для управления виртуальными машинами (libvirt)</ru>
+<sk>desktopová aplikácia na správu virtuálnych strojov (libvirt)</sk>
+<sl>namizna aplikacija za upravljanje navideznih strojev (libvirt)</sl>
+<sq>aplikacion desktop për menaxhimin e makinave virtuale (libvirt)</sq>
+<sr>десктоп апликација за управљање виртуелним машинама (libvirt)</sr>
+<sv>skrivbordsapplikation för att hantera virtuella maskiner (libvirt)</sv>
+<tr>sanal makineleri yönetmek için masaüstü uygulaması (libvirt)</tr>
+<uk>настільний додаток для управління віртуальними машинами (libvirt)</uk>
+<vi>ứng dụng máy tính để bàn để quản lý máy ảo (libvirt)</vi>
+<zh_CN>用於管理虛擬機的桌面應用程序</zh_CN>
+<zh_TW>用于管理虚拟机的桌面应用程序</zh_TW>
+
+</description>
+
+<installable>
+64
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/18374/simage/large-e79ab57d62d37d5957d0fef012fdab3b.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+virt-manager
+libvirt-daemon-system-sysv 
+gir1.2-spiceclientglib-2.0 
+gir1.2-spiceclientgtk-3.0 
+qemu-utils 
+virt-viewer 
+qemu-system-x86
+libvirt-daemon-system
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+virt-manager
+virt-viewer
+</uninstall_package_names>
+
+</app>
