6701b13e47bf113efa1f6b6ad9c6890c731848e4
Dolphin Oracle <dolphinoracle@gmail.com>
2022-07-24 10:29:20 -0400

    add steam-devices

---
+++ steam-devices.pm
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Games
+</category>
+
+<name>
+Steam Device Support
+</name>
+
+<description>
+<am>የእንፋሎት ምናባዊ እውነታ እና የግቤት መሳሪያዎች ድጋፍ</am>
+<ar> ﻝﺎﺧﺩﻹ﻿ﺍ ﺓﺰﻬﺟﺃﻭ ﻱﺭﺎﺨﺒﻟﺍ ﻲﺿﺍﺮﺘﻓﻻ﻿ﺍ ﻊﻗﺍﻮﻠﻟ ﻢﻋﺩ</ar>
+<bg>поддръжка за виртуална реалност и устройства за въвеждане на steam</bg>
+<bn>বাষ্প ভার্চুয়াল বাস্তবতা এবং ইনপুট ডিভাইসের জন্য সমর্থন</bn>
+<ca>suport per a la realitat virtual i els dispositius d'entrada de Steam</ca>
+<cs>podpora pro virtuální realitu steam a vstupní zařízení</cs>
+<da>understøttelse af steam virtual reality og inputenheder</da>
+<de>Unterstützung für Steam Virtual Reality und Eingabegeräte</de>
+<el>υποστήριξη για συσκευές εικονικής πραγματικότητας και εισόδου steam</el>
+<es>soporte para dispositivos de entrada y realidad virtual de vapor</es>
+<et>Steam virtuaalreaalsuse ja sisendseadmete tugi</et>
+<eu>Lurrun errealitate birtuala eta sarrera gailuetarako laguntza</eu>
+<en>support for steam virtual reality and input devices</en>
+<fa>ﯼﺩﻭﺭﻭ ﯼﺎﻫ ﻩﺎﮕﺘﺳﺩ ﻭ ﻢﯿﺘﺳﺍ ﯼﺯﺎﺠﻣ ﺖﯿﻌﻗﺍﻭ ﺯﺍ ﯽﻧﺎﺒﯿﺘﺸﭘ</fa>
+<fil_PH>suporta para sa steam virtual reality at mga input device</fil_PH>
+<fi>tuki steam-virtuaalitodellisuudelle ja syöttölaitteille</fi>
+<fr>prise en charge de la réalité virtuelle Steam et des périphériques d'entrée</fr>
+<he_IL>טלק ינקתהו Steam לש המודמ תואיצמב הכימת</he_IL>
+<hi>भाप आभासी वास्तविकता और इनपुट उपकरणों के लिए समर्थन</hi>
+<hr>podrška za parnu virtualnu stvarnost i ulazne uređaje</hr>
+<hu>Steam virtuális valóság és beviteli eszközök támogatása</hu>
+<id>dukungan untuk realitas virtual uap dan perangkat input</id>
+<is>stuðningur við steam sýndarveruleika og inntakstæki</is>
+<it>supporto per la realtà virtuale di Steam e dispositivi di input</it>
+<ja>Steamバーチャルリアリティと入力デバイスのサポート</ja>
+<kk>steam виртуалды шындықты және енгізу құрылғыларын қолдау</kk>
+<ko>"Steam 가상 현실 및 입력 장치 지원"</ko>
+<lt>„Steam“ virtualios realybės ir įvesties įrenginių palaikymas</lt>
+<mk>поддршка за steam виртуелна реалност и уреди за влез</mk>
+<mr>स्टीम व्हर्च्युअल रिअॅलिटी आणि इनपुट उपकरणांसाठी समर्थन</mr>
+<nb>støtte for steam virtual reality og inngangsenheter</nb>
+<nl>ondersteuning voor steam virtual reality en invoerapparaten</nl>
+<pl>obsługa wirtualnej rzeczywistości Steam i urządzeń wejściowych</pl>
+<pt_BR>suporte para realidade virtual a vapor e dispositivos de entrada</pt_BR>
+<pt>suporte para realidade virtual a vapor e dispositivos de entrada</pt>
+<ro>suport pentru realitate virtuală Steam și dispozitive de intrare</ro>
+<ru>поддержка виртуальной реальности Steam и устройств ввода</ru>
+<sk>podpora pre steam virtuálnu realitu a vstupné zariadenia</sk>
+<sl>podpora za virtualno resničnost Steam in vhodne naprave</sl>
+<sq>mbështetje për realitetin virtual me avull dhe pajisjet hyrëse</sq>
+<sr>подршка за парну виртуелну стварност и уређаје за унос</sr>
+<sv>stöd för steam virtual reality och inmatningsenheter</sv>
+<tr>steam sanal gerçeklik ve giriş cihazları için destek</tr>
+<uk>підтримка віртуальної реальності Steam і пристроїв введення</uk>
+<vi>hỗ trợ thực tế ảo hơi nước và các thiết bị đầu vào</vi>
+</description>
+
+<installable>
+32,64
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/14991/simage/large-fdb68e27705c0554fa7ffc10e4ba1dc0.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+steam-devices
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+steam-devices
+</uninstall_package_names>
+</app>
