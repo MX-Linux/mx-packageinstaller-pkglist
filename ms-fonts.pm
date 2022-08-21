e76e3862df03e8b87628e68412c41a732602053d
Dolphin Oracle <dolphinoracle@gmail.com>
2022-08-21 15:08:27 -0400

    add ms-fonts, 5.18 kernel

---
+++ ms-fonts.pm
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Office
+</category>
+
+<name>
+Microsoft Fonts
+</name>
+
+<description>
+   <am>ክላሲክ የማይክሮሶፍት ቅርጸ-ቁምፊዎች</am>
+<ar>﻿ﻜﻟﺍ ﺖﻓﻮﺳﻭﺮﻜﻳﺎﻣ ﻁﻮﻄﺧ</ar>
+<bg>класически шрифтове на microsoft</bg>
+<bn>ক্লাসিক মাইক্রোসফ্ট ফন্ট</bn>
+<ca>tipus de lletra de Microsoft clàssics</ca>
+<cs>klasické microsoftí fonty</cs>
+<da>klassiske microsoft-skrifttyper</da>
+<de>klassische Microsoft-Schriftarten</de>
+<el>κλασικές γραμματοσειρές microsoft</el>
+<es>fuentes clásicas de microsoft</es>
+<et>klassikalised microsofti fondid</et>
+<eu>Microsoft-eko letra-tipo klasikoak</eu>
+<en>classic microsoft fonts</en>
+<fa>ﺖﻓﺎﺳﻭﺮﮑﯾﺎﻣ ﮏﯿﺳﻼ﻿ﮐ ﯼﺎﻫ ﺖﻧﻮﻓ</fa>
+<fil_PH>mga klasikong microsoft font</fil_PH>
+<fi>klassiset microsoft-fontit</fi>
+<fr>polices microsoft classiques</fr>
+<he_IL>טפוסורקימ לש םייסאלק םינפוג</he_IL>
+<hi>क्लासिक माइक्रोसॉफ्ट फोंट</hi>
+<hr>klasični microsoft fontovi</hr>
+<hu>klasszikus microsoft betűtípusok</hu>
+<id>font microsoft klasik</id>
+<is>klassískt microsoft leturgerðir</is>
+<it>caratteri Microsoft classici</it>
+<ja>クラシック マイクロソフト フォント</ja>
+<kk>Классикалық Microsoft қаріптері</kk>
+<ko>"고전적인 마이크로소프트 글꼴"</ko>
+<lt>klasikiniai „Microsoft“ šriftai</lt>
+<mk>класични микрософт фонтови</mk>
+<mr>क्लासिक मायक्रोसॉफ्ट फॉन्ट</mr>
+<nb>klassiske Microsoft-fonter</nb>
+<nl>klassieke Microsoft-lettertypen</nl>
+<pl>klasyczne czcionki Microsoft</pl>
+<pt_BR>fontes clássicas da microsoft</pt_BR>
+<pt>fontes clássicas da microsoft</pt>
+<ro>fonturi Microsoft clasice</ro>
+<ru>классические шрифты майкрософт</ru>
+<sk>klasické fonty microsoftu</sk>
+<sl>klasične Microsoftove pisave</sl>
+<sq>fontet klasike të microsoft</sq>
+<sr>класични Мицрософт фонтови</sr>
+<sv>klassiska Microsoft-teckensnitt</sv>
+<tr>klasik microsoft yazı tipleri</tr>
+<uk>класичні шрифти microsoft</uk>
+<vi>phông chữ microsoft cổ điển</vi>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot></screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+ttf-mscorefonts-installer
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+ttf-mscorefonts-installer
+</uninstall_package_names>
+</app>
