e9c3b1c2c59f4d8b21e07277b429f2785c8bcc46
Dolphin Oracle <dolphinoracle@gmail.com>
2022-08-23 20:47:37 -0400

    update 5.18 kernel

--- kernel_mx_ahs_5.18.pm
+++ kernel_mx_ahs_5.18.pm
@@ -72,20 +72,20 @@ apt-get update
 </preinstall>
 
 <install_package_names>
-linux-image-5.18.0-2mx-amd64-unsigned
-linux-headers-5.18.0-2mx-amd64
+linux-image-5.18.0-4mx-amd64-unsigned
+linux-headers-5.18.0-4mx-amd64
 </install_package_names>
 
 
 <postinstall>
 rm /etc/apt/sources.list.d/mxpitemp.list
 apt-get update
-rebuild_dkms_packages.sh linux-image-5.18.0-2mx-amd64-unsigned
+rebuild_dkms_packages.sh linux-image-5.18.0-4mx-amd64-unsigned
 </postinstall>
 
 
 <uninstall_package_names>
-linux-image-5.18.0-2mx-amd64-unsigned
-linux-headers-5.18.0-2mx-amd64
+linux-image-5.18.0-4mx-amd64-unsigned
+linux-headers-5.18.0-4mx-amd64
 </uninstall_package_names>
 </app>
