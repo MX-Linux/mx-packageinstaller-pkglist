50e8bf88eacf6b2a8b027f8d8d854489f513b44a
Dolphin Oracle <dolphinoracle@gmail.com>
2023-02-20 16:06:36 -0500

    update spotify keyring

--- spotify_64.pm
+++ spotify_64.pm
@@ -67,7 +67,7 @@ Spotify
 <screenshot>none</screenshot>
 
 <preinstall>
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/repository-spotify-com-keyring.gpg
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free">/etc/apt/sources.list.d/spotify.list
 apt-get update
 </preinstall>
