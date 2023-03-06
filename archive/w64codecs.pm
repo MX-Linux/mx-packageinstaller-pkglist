f9e8909ab221f0f0e9a9905de7789f9dd5c5a889
Dolphin Oracle <dolphinoracle@gmail.com>
2023-03-05 21:04:43 -0500

    add mx-codecs functions to mxpi

---
+++ archive/w64codecs.pm
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Video
+</category>
+
+<name>
+w32codecs
+</name>
+
+<description>
+library of older windows, quicktime, and realplayer codecs
+</description>
+
+<installable>
+64
+</installable>
+
+<screenshot></screenshot>
+
+<preinstall>
+ARCH=$(dpkg --print-architecture)
+SITE="https://www.deb-multimedia.org/"
+TARGET=$(curl -s  https://www.deb-multimedia.org/dists/stable/non-free/binary-${ARCH}/package/w64codecs |grep Filename |cut -d":" -f2 |cut -d" " -f2)
+TARGET="${TARGET%deb*}deb"
+wget $SITE${TARGET} -O /tmp/w64codecs.deb
+apt-get install /tmp/w64codecs.deb
+</preinstall>
+
+<install_package_names>
+
+</install_package_names>
+
+
+<postinstall>
+rm /tmp/w64codecs.deb
+</postinstall>
+
+
+<uninstall_package_names>
+w64codecs
+</uninstall_package_names>
+</app>
---
+++ libdvdcss-deb-multi.pm
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Video
+</category>
+
+<name>
+libdvdcss
+</name>
+
+<description>
+<am>በጋራ የቪዲዮ ማጫወቻዎች ውስጥ የንግድ ዲቪዲ መልሶ ማጫወትን ለመፍቀድ ቤተ-መጽሐፍት</am>
+<ar>ﺔﻌﺋﺎﺸﻟﺍ ﻮﻳﺪﻴﻔﻟﺍ ﺕﻼ﻿ﻐﺸﻣ ﻲﻓ ﺔﻳﺭﺎﺠﺘﻟﺍ DVD ﺹﺍﺮﻗﺃ ﻞﻴﻐﺸﺘﺑ ﺡﺎﻤﺴﻠﻟ ﺔﺒﺘﻜﻣ</ar>
+<bg>библиотека, за да позволи възпроизвеждане на комерсиални DVD в обикновени видео плейъри</bg>
+<bn>সাধারণ ভিডিও প্লেয়ারগুলিতে বাণিজ্যিক ডিভিডি প্লেব্যাকের অনুমতি দেওয়ার জন্য লাইব্রেরি</bn>
+<ca>biblioteca per permetre la reproducció de DVD comercials en reproductors de vídeo habituals</ca>
+<cs>knihovna umožňující komerční přehrávání DVD v běžných video přehrávačích</cs>
+<da>bibliotek for at tillade kommerciel dvd-afspilning i almindelige videoafspillere</da>
+<de>Bibliothek, um die kommerzielle DVD-Wiedergabe in gängigen Videoplayern zu ermöglichen</de>
+<el>βιβλιοθήκη για να επιτρέπει την αναπαραγωγή εμπορικών dvd σε κοινά προγράμματα αναπαραγωγής βίντεο</el>
+<es>biblioteca para permitir la reproducción de DVD comerciales en reproductores de video comunes</es>
+<et>raamatukogu, et võimaldada kommerts-dvd-de taasesitust tavalistes videopleierites</et>
+<eu>liburutegia dvd komertziala erreproduzitzeko bideo erreproduzitzaile arruntetan ahalbidetzeko</eu>
+<en>library to allow commercial dvd playback in common video players</en>
+<fa>ﺪﻨﮐ ﯽﻣ ﻢﻫﺍﺮﻓ ﺍﺭ ﺞﯾﺍﺭ ﯽﯾﻮﯾﺪﯾﻭ ﯼﺎﻫ ﻩﺪﻨﻨﮐ ﺶﺨﭘ ﺭﺩ ﯼﺭﺎﺠﺗ dvd ﺶﺨﭘ ﻥﺎﮑﻣﺍ ﻪﮐ ﯼﺍ ﻪﻧﺎﺨﺑﺎﺘﮐ</fa>
+<fil_PH>library upang payagan ang komersyal na pag-playback ng dvd sa mga karaniwang video player</fil_PH>
+<fi>kirjasto, joka mahdollistaa kaupallisen dvd-toiston yleisissä videosoittimissa</fi>
+<fr>bibliothèque pour permettre la lecture de DVD commerciaux dans des lecteurs vidéo courants</fr>
+<he_IL>םיצופנ ואדיו ינגנב תירחסמ DVD תעמשה רשפאל ידכ היירפס</he_IL>
+<hi>सामान्य वीडियो प्लेयर में वाणिज्यिक डीवीडी प्लेबैक की अनुमति देने के लिए पुस्तकालय</hi>
+<hr>knjižnica kako bi se omogućila komercijalna reprodukcija DVD-a u uobičajenim video playerima</hr>
+<hu>könyvtár, amely lehetővé teszi a kereskedelmi forgalomban lévő DVD-lejátszást általános videolejátszókban</hu>
+<id>perpustakaan untuk memungkinkan pemutaran dvd komersial di pemutar video umum</id>
+<is>bókasafn til að leyfa spilun á dvd í auglýsingum í algengum myndbandsspilurum</is>
+<it>libreria per consentire la riproduzione di dvd commerciali nei comuni lettori video</it>
+<ja>一般的なビデオ プレーヤーで商用 DVD を再生できるようにするライブラリ</ja>
+<kk>жалпы бейне ойнатқыштарда коммерциялық DVD ойнатуға мүмкіндік беретін кітапхана</kk>
+<ko>일반 비디오 플레이어에서 상업용 DVD 재생을 허용하는 라이브러리</ko>
+<lt>biblioteka, leidžianti leisti komercinius DVD diskus įprastuose vaizdo grotuvuose</lt>
+<mk>библиотека за да се овозможи комерцијална репродукција на ДВД во обичните видео плеери</mk>
+<mr>सामान्य व्हिडिओ प्लेयर्समध्ये व्यावसायिक डीव्हीडी प्लेबॅकला अनुमती देण्यासाठी लायब्ररी</mr>
+<nb>bibliotek for å tillate kommersiell dvd-avspilling i vanlige videospillere</nb>
+<nl>bibliotheek om commerciële dvd-weergave in gewone videospelers mogelijk te maken</nl>
+<pl>biblioteka umożliwiająca odtwarzanie komercyjnych płyt DVD w popularnych odtwarzaczach wideo</pl>
+<pt_BR>biblioteca para permitir a reprodução de DVD comercial em reprodutores de vídeo comuns</pt_BR>
+<pt>biblioteca para permitir a reprodução de DVD comercial em reprodutores de vídeo comuns</pt>
+<ro>bibliotecă pentru a permite redarea DVD-urilor comerciale în playerele video obișnuite</ro>
+<ru>библиотека, позволяющая воспроизводить коммерческие DVD в обычных видеопроигрывателях</ru>
+<sk>knižnica umožňujúca komerčné prehrávanie DVD v bežných videoprehrávačoch</sk>
+<sl>knjižnico, ki omogoča predvajanje komercialnih DVD-jev v običajnih video predvajalnikih</sl>
+<sq>bibliotekë për të lejuar riprodhimin komercial të DVD-ve në luajtësit e zakonshëm të videos</sq>
+<sr>библиотека која омогућава комерцијалну репродукцију ДВД-а у уобичајеним видео плејерима</sr>
+<sv>bibliotek för att tillåta kommersiell dvd-uppspelning i vanliga videospelare</sv>
+<tr>ortak video oynatıcılarda ticari dvd oynatmaya izin veren kitaplık</tr>
+<uk>бібліотека, що дозволяє відтворювати комерційні DVD-диски у звичайних відеопрогравачах</uk>
+<vi>thư viện để cho phép phát lại dvd thương mại trong các trình phát video phổ biến</vi>
+
+</description>
+
+<installable>
+32,64
+</installable>
+
+<screenshot></screenshot>
+
+<preinstall>
+ARCH=$(dpkg --print-architecture)
+SITE="https://www.deb-multimedia.org/"
+TARGET=$(curl -s  https://www.deb-multimedia.org/dists/stable/main/binary-${ARCH}/package/libdvdcss2 |grep Filename |cut -d":" -f2 |cut -d" " -f2)
+TARGET="${TARGET%deb*}deb"
+wget $SITE${TARGET} -O /tmp/libdvdcss2.deb
+apt-get install /tmp/libdvdcss2.deb
+</preinstall>
+
+<install_package_names>
+
+</install_package_names>
+
+
+<postinstall>
+rm /tmp/libdvdcss2.deb
+</postinstall>
+
+
+<uninstall_package_names>
+libdvdcss2
+</uninstall_package_names>
+</app>
