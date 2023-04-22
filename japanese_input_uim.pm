69dc0fe548054554bb42fcc9cf5d760c1e8817e1
Dolphin Oracle <dolphinoracle@gmail.com>
2023-04-22 13:29:44 -0400

    add japanese uim

---
+++ japanese_input_uim.pm
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Language
+</category>
+
+<name>
+Japanese_Input_uim
+</name>
+
+<description>
+   <am>Japanese uim</am>
+   <ar>Japanese uim</ar>
+   <bg>Japanese uim</bg>
+   <bn>Japanese uim</bn>
+   <ca>uim en japonès</ca>
+   <cs>Japanese uim</cs>
+   <da>Japansk uim</da>
+   <de>Japanischer uim</de>
+   <el>Ιαπωνικά uim</el>
+   <en>Japanese uim</en>
+   <es>uim Japonés</es>
+   <et>Japanese uim</et>
+   <eu>Japanese uim</eu>
+   <fa>Japanese uim</fa>
+   <fil_PH>Japanese uim</fil_PH>
+   <fi>Japanese uim</fi>
+   <fr>Japonais uim</fr>
+   <he_IL>Japanese uim</he_IL>
+   <hi>Japanese uim</hi>
+   <hr>Japanese uim</hr>
+   <hu>Japanese uim</hu>
+   <id>Japanese uim</id>
+   <is>Japanese uim</is>
+   <it>uim per la lingua giapponese</it>
+   <ja_JP>Japanese uim</ja_JP>
+   <ja>Japanese uim</ja>
+   <kk>Japanese uim</kk>
+   <ko>Japanese uim</ko>
+   <lt>Japonų uim</lt>
+   <mk>Japanese uim</mk>
+   <mr>Japanese uim</mr>
+   <nb>Japanese uim</nb>
+   <nl>Japanse uim</nl>
+   <pl>Japoński uim</pl>
+   <pt_BR>Japonês uim</pt_BR>
+   <pt>Japonês uim</pt>
+   <ro>Japanese uim</ro>
+   <ru>Japanese uim</ru>
+   <sk>Japanese uim</sk>
+   <sl>Japanese uim</sl>
+   <sq>Japanese uim</sq>
+   <sr>Japanese uim</sr>
+   <sv>Japansk uim</sv>
+   <tr>Japonca uim</tr>
+   <uk>Japanese uim</uk>
+   <vi>Japanese uim</vi>
+   <zh_CN>Japanese uim</zh_CN>
+   <zh_TW>Japanese uim</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+uim-anthy 
+uim-mozc 
+uim 
+im-config 
+zenity 
+kasumi 
+uim-gtk2.0 
+uim-gtk3 
+uim-qt5 
+uim-xim 
+uim-fep 
+fonts-vlgothic
+manpages-ja
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+uim-anthy 
+uim-mozc 
+uim 
+im-config 
+zenity 
+kasumi 
+uim-gtk2.0 
+uim-gtk3 
+uim-qt5 
+uim-xim 
+uim-fep 
+fonts-vlgothic
+manpages-ja
+</uninstall_package_names>
+</app>
