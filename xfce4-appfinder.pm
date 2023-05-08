6c3b326859cdc3e50cd4a0485d06a06181d983df
Dolphin Oracle <dolphinoracle@gmail.com>
2023-05-08 19:20:37 -0400

    appfinder description update

--- xfce4-appfinder.pm
+++ xfce4-appfinder.pm
@@ -10,51 +10,51 @@ Xfce4 Appfinder
 </name>
 
 <description>
-<am>የመተግበሪያ አግኚው ከ Xfce</am>
-<ar>Xfce ﻦﻣ ﻖﻴﺒﻄﺘﻟﺍ ﻒﺸﺘﻜﻣ</ar>
-<bg>Търсач на приложения от Xfce</bg>
-<bn>Xfce থেকে অ্যাপ্লিকেশন সন্ধানকারী</bn>
-<ca>Cercador d'aplicacions de Xfce</ca>
-<cs>Vyhledávač aplikací z Xfce</cs>
-<da>Applikationsfinder fra Xfce</da>
-<de>Anwendungsfinder von Xfce</de>
-<el>Εύρεση εφαρμογών από το Xfce</el>
-<es>Buscador de aplicaciones de la Xfce</es>
-<et>Rakenduste otsija Xfce-st</et>
-<eu>Xfce-ko aplikazio-bilatzailea</eu>
-<en>Application finder from the Xfce</en>
-<fa>Xfce ﺯﺍ ﺏﺎﯾ ﻪﻣﺎﻧﺮﺑ</fa>
-<fil_PH>Tagahanap ng application mula sa Xfce</fil_PH>
-<fi>Sovellushaku Xfce:stä</fi>
-<fr>Outil de recherche d'applications de Xfce</fr>
-<he_IL>Xfce-המ םימושיי רתאמ</he_IL>
-<hi>Xfce से आवेदन खोजक</hi>
-<hr>Tražilica aplikacija iz Xfce-a</hr>
-<hu>Alkalmazáskereső az Xfce-ből</hu>
-<id>Pencari aplikasi dari Xfce</id>
-<is>Forritaleitari frá Xfce</is>
-<it>Ricerca applicazioni da Xfce</it>
-<ja>Xfce からのアプリケーション ファインダー</ja>
-<kk>Xfce қолданбасын іздеуші</kk>
-<ko>Xfce의 응용 프로그램 찾기</ko>
-<lt>Programų ieškiklis iš Xfce</lt>
-<mk>Пронаоѓач на апликации од Xfce</mk>
-<mr>Xfce कडून अनुप्रयोग शोधक</mr>
-<nb>Applikasjonsfinner fra Xfce</nb>
-<nl>Applicatiezoeker van de Xfce</nl>
-<pl>Wyszukiwarka aplikacji z Xfce</pl>
-<pt_BR>Localizador de aplicativos do Xfce</pt_BR>
-<pt>Localizador de aplicativos do Xfce</pt>
-<ro>Găsitorul de aplicații din Xfce</ro>
-<ru>Поиск приложений от Xfce</ru>
-<sk>Vyhľadávač aplikácií z Xfce</sk>
-<sl>Iskalnik aplikacij iz Xfce</sl>
-<sq>Gjetësi i aplikacionit nga Xfce</sq>
-<sr>Проналазач апликација из Ксфце-а</sr>
-<sv>Applikationssökare från Xfce</sv>
-<tr>Xfce'den uygulama bulucu</tr>
-<uk>Пошук програм із Xfce</uk>
-<vi>Công cụ tìm ứng dụng từ Xfce</vi>
+<am>የመተግበሪያ አግኚው ከ Xfce ፕሮጀክት</am>
+<ar>Xfce ﻉﻭﺮﺸﻣ ﻦﻣ ﻖﻴﺒﻄﺘﻟﺍ ﻒﺸﺘﻜﻣ</ar>
+<bg>Търсач на приложения от проекта Xfce</bg>
+<bn>Xfce প্রকল্প থেকে অ্যাপ্লিকেশন সন্ধানকারী</bn>
+<ca>Cercador d'aplicacions del projecte Xfce</ca>
+<cs>Vyhledávač aplikací z projektu Xfce</cs>
+<da>Applikationsfinder fra Xfce-projektet</da>
+<de>Anwendungsfinder aus dem Xfce-Projekt</de>
+<el>Εύρεση εφαρμογών από το έργο Xfce</el>
+<es>Buscador de aplicaciones del proyecto Xfce</es>
+<et>Rakenduste otsija Xfce projektist</et>
+<eu>Xfce proiektuko aplikazio-bilatzailea</eu>
+<en>Application finder from the Xfce project</en>
+<fa>Xfce ﻩﮊﻭﺮﭘ ﺯﺍ ﺏﺎﯾ ﻪﻣﺎﻧﺮﺑ</fa>
+<fil_PH>Tagahanap ng application mula sa proyekto ng Xfce</fil_PH>
+<fi>Sovellushaku Xfce-projektista</fi>
+<fr>Outil de recherche d'applications du projet Xfce</fr>
+<he_IL>Xfce טקיורפמ םימושיי רתאמ</he_IL>
+<hi>Xfce परियोजना से आवेदन खोजक</hi>
+<hr>Tražilica aplikacija iz Xfce projekta</hr>
+<hu>Alkalmazáskereső az Xfce projektből</hu>
+<id>Pencari aplikasi dari proyek Xfce</id>
+<is>Forritaleitari frá Xfce verkefninu</is>
+<it>Ricercatore di applicazioni dal progetto Xfce</it>
+<ja>Xfce プロジェクトのアプリケーション ファインダー</ja>
+<kk>Xfce жобасынан қолданбаларды іздеуші</kk>
+<ko>Xfce 프로젝트의 응용 프로그램 찾기</ko>
+<lt>Programų ieškiklis iš Xfce projekto</lt>
+<mk>Пронаоѓач на апликации од проектот Xfce</mk>
+<mr>Xfce प्रकल्पातील अनुप्रयोग शोधक</mr>
+<nb>Applikasjonsfinner fra Xfce-prosjektet</nb>
+<nl>Applicatiezoeker van het Xfce-project</nl>
+<pl>Wyszukiwarka aplikacji z projektu Xfce</pl>
+<pt_BR>Localizador de aplicativos do projeto Xfce</pt_BR>
+<pt>Localizador de aplicativos do projeto Xfce</pt>
+<ro>Găsitor de aplicații din proiectul Xfce</ro>
+<ru>Поиск приложений из проекта Xfce</ru>
+<sk>Vyhľadávač aplikácií z projektu Xfce</sk>
+<sl>Iskalnik aplikacij iz projekta Xfce</sl>
+<sq>Gjetësi i aplikacionit nga projekti Xfce</sq>
+<sr>Проналазач апликација из Ксфце пројекта</sr>
+<sv>Applikationssökare från Xfce-projektet</sv>
+<tr>Xfce projesinden uygulama bulucu</tr>
+<uk>Пошук додатків із проекту Xfce</uk>
+<vi>Công cụ tìm ứng dụng từ dự án Xfce</vi>
 </description>
 
 <installable>
