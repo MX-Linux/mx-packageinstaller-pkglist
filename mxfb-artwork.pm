9c49ce43f16c6533d16e45ede090d10804ad90df
Dolphin Oracle <dolphinoracle@gmail.com>
2023-06-10 21:46:56 -0400

    fix debian labels, add mxfb-artwork

---
+++ mxfb-artwork.pm
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Wallpapers
+</category>
+
+<name>
+MX Fluxbox Wallpapers
+</name>
+
+<description>
+backgrounds originally supplied with MX 19 with a fluxbox theme
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+mxfb-art
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+mxfb-art
+</uninstall_package_names>
+</app>
