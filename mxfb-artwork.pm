171360c3239a9674a2191d7d9adb0b603c5a3be1
Dolphin Oracle <dolphinoracle@gmail.com>
2023-06-10 21:49:02 -0400

    update mxfb-artwork descriptions

--- mxfb-artwork.pm
+++ mxfb-artwork.pm
@@ -10,7 +10,52 @@ MX Fluxbox Wallpapers
 </name>
 
 <description>
-backgrounds originally supplied with MX 19 with a fluxbox theme
+<am>ዳራዎች በመጀመሪያ ከ MX 19 ከ fluxbox ጭብጥ ጋር ቀርበዋል</am>
+<ar>fluxbox ﺔﻤﺳ ﻊﻣ MX 19 ـﺑ ﻼً﻿ﺻﺃ ﺓﺩﻭﺰﻤﻟﺍ ﺕﺎﻴﻔﻠﺨﻟﺍ</ar>
+<bg>фонове, първоначално доставени с MX 19 с тема на fluxbox</bg>
+<bn>ব্যাকগ্রাউন্ডগুলি মূলত একটি ফ্লাক্সবক্স থিম সহ MX 19 এর সাথে সরবরাহ করা হয়েছে৷</bn>
+<ca>fons subministrats originalment amb MX 19 amb un tema de fluxbox</ca>
+<cs>pozadí původně dodávané s MX 19 s tématem fluxboxu</cs>
+<da>baggrunde, der oprindeligt blev leveret med MX 19 med et fluxbox-tema</da>
+<de>Hintergründe, die ursprünglich mit MX 19 mit einem Fluxbox-Design geliefert wurden</de>
+<el>φόντα που παρέχονται αρχικά με το MX 19 με θέμα fluxbox</el>
+<es>fondos suministrados originalmente con MX 19 con un tema fluxbox</es>
+<et>taustad, mis olid algselt kaasas fluxboxi teemaga MX 19-ga</et>
+<eu>jatorriz MX 19-rekin hornitutako atzeko planoak fluxbox gaiarekin</eu>
+<en>backgrounds originally supplied with MX 19 with a fluxbox theme</en>
+<fa>ﺪﻧﺩﻮﺑ ﻩﺪﺷ ﻪﺿﺮﻋ fluxbox ﻉﻮﺿﻮﻣ ﺎﺑ MX 19 ﺎﺑ ﻞﺻﺍ ﺭﺩ ﻪﮐ ﯽﯾﺎﻫu200cﻪﻨﯿﻣﺯu200cﺲﭘ</fa>
+<fil_PH>mga background na orihinal na ibinigay sa MX 19 na may tema ng fluxbox</fil_PH>
+<fi>taustat toimitettiin alun perin MX 19:n kanssa fluxbox-teemalla</fi>
+<fr>arrière-plans fournis à l'origine avec MX 19 avec un thème fluxbox</fr>
+<he_IL>Fluxbox אשונ תכרע םע MX 19 םע רוקמב וקפוסש םיעקר</he_IL>
+<hi>पृष्ठभूमि मूल रूप से फ्लक्सबॉक्स थीम के साथ एमएक्स 19 के साथ आपूर्ति की जाती है</hi>
+<hr>pozadine izvorno isporučene s MX 19 s temom fluxbox</hr>
+<hu>hátterek, amelyeket eredetileg az MX 19-hez szállítottak fluxbox témával</hu>
+<id>latar belakang awalnya disertakan dengan MX 19 dengan tema fluxbox</id>
+<is>bakgrunnur sem upphaflega fylgdi MX 19 með fluxbox þema</is>
+<it>sfondi originariamente forniti con MX 19 con un tema fluxbox</it>
+<ja>Fluxbox テーマを備えた MX 19 に元々付属していた背景</ja>
+<kk>фон бастапқыда fluxbox тақырыбымен MX 19-мен бірге жеткізіледі</kk>
+<ko>원래 플럭스박스 테마로 MX 19와 함께 제공된 배경</ko>
+<lt>fonai iš pradžių buvo pateikti kartu su MX 19 su fluxbox tema</lt>
+<mk>позадини првично испорачани со MX 19 со тема флуксбокс</mk>
+<mr>फ्लक्सबॉक्स थीमसह MX 19 सह पार्श्वभूमी मूळत: पुरविली जाते</mr>
+<nb>bakgrunner som opprinnelig ble levert med MX 19 med et fluxbox-tema</nb>
+<nl>achtergronden die oorspronkelijk bij MX 19 werden geleverd met een fluxbox-thema</nl>
+<pl>tła oryginalnie dostarczone z MX 19 z motywem fluxboksa</pl>
+<pt_BR>fundos originalmente fornecidos com o MX 19 com um tema fluxbox</pt_BR>
+<pt>fundos originalmente fornecidos com o MX 19 com um tema fluxbox</pt>
+<ro>fundaluri furnizate inițial cu MX 19 cu o temă fluxbox</ro>
+<ru>фоны, изначально поставляемые с MX 19, с темой fluxbox</ru>
+<sk>pozadia pôvodne dodávané s MX 19 s témou fluxboxu</sk>
+<sl>ozadja, prvotno priložena MX 19 s temo fluxbox</sl>
+<sq>sfondet e furnizuara fillimisht me MX 19 me një temë fluxbox</sq>
+<sr>позадине које су првобитно испоручене са МКС 19 са темом флукбок</sr>
+<sv>bakgrunder som ursprungligen levererades med MX 19 med ett fluxbox-tema</sv>
+<tr>orijinal olarak fluxbox temalı MX 19 ile birlikte verilen arka planlar</tr>
+<uk>фони, які спочатку постачалися разом із MX 19 із темою fluxbox</uk>
+<vi>hình nền ban đầu được cung cấp cùng với MX 19 với chủ đề fluxbox</vi>
+
 </description>
 
 <installable>
