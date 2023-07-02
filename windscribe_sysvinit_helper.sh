9f20e4298305e8c4238c7fbe862527bd7f0737ac
fehlix <feh.ler@t-online.de>
2023-07-02 18:36:34 +0200

    update windscribe

--- windscribe_sysvinit_helper.sh
+++ windscribe_sysvinit_helper.sh
@@ -2,43 +2,45 @@
 
 #  windscribe_systemctl_helper
 
+if [ -d /run/systemd/system ]; then
+    :
+else
+
 echodo() {
-	echo "$@"
-	"$@"
+    echo "$@"
+    "$@"
 }
 
 systemctl() {
-	local ret
-	if [ -d /run/systemd/system ]; then
-	   echodo /usr/bin/systemctl "$@"
-	   ret=$?
-	   return $ret
-	fi
-	local action="$1"
-	shift $(($# - 1))
-	local service="$1"
-	case "$action" in
-	   start|stop|status) 
-			echodo service $service $action
-			ret=$?
-		    ;;
-	   disable)
-			echodo update-rc.d $service remove
-			ret=$?
-			;;
-	   enable)
-			echodo update-rc.d $service remove
-			echodo update-rc.d $service defaults
-			ret=$?
-			;;
-	   is-active)
-			sysv-rc-conf  --list $service | grep -q ':on'
-			ret=$?
-			;;
-		*)
-			echodo /usr/bin/systemctl "$@"
-			ret=$?
-			;;
-	esac
+    local ret
+    local arg=( "$@" ) 
+    local action="$1"
+    shift $(($# - 1))
+    local service="$1"
+    case "$action" in
+       start|stop|status) 
+            echodo service $service $action || :
+            ret=$?
+            ;;
+       disable)
+            echodo update-rc.d $service remove || :
+            ret=$?
+            ;;
+       enable|preset)
+            echodo update-rc.d $service remove    || :
+            echodo update-rc.d $service defaults  || :
+            ret=$?
+            ;;
+       is-active)
+            sysv-rc-conf  --list $service | grep -q ':on'
+            ret=$?
+            ;;
+        *)
+            echodo /usr/bin/systemctl "${arg[@]}"
+            ret=$?
+            ;;
+    esac
     return $ret
 }
+
+fi
