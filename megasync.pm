0e998eea6cc5315b54c5847d669cbb3fed3f5c84
Dolphin Oracle <dolphinoracle@gmail.com>
2023-07-15 09:16:36 -0400

    update ahs kernel and megasync

--- megasync.pm
+++ megasync.pm
@@ -66,10 +66,8 @@ Megasync
 <screenshot></screenshot>
 
 <preinstall>
-echo "deb https://mega.nz/linux/MEGAsync/Debian_11/ ./">/etc/apt/sources.list.d/mxpitemp.list
-wget https://mega.nz/linux/MEGAsync/Debian_11/Release.key -O /tmp/megarelease.key
-apt-key add /tmp/megarelease.key
-rm /tmp/megarelease.key
+echo "deb https://mega.nz/linux/MEGAsync/Debian_testing/ ./">/etc/apt/sources.list.d/mxpitemp.list
+curl -sS https://mega.nz/linux/MEGAsync/Debian_testing/Release.key | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/megasync.gpg
 apt-get update
 </preinstall>
 
