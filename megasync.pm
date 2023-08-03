83bffdd2a010998490a88cddc618866717d499a7
Dolphin Oracle <dolphinoracle@gmail.com>
2023-08-03 12:14:26 -0400

    Update megasync.pm

--- megasync.pm
+++ megasync.pm
@@ -66,7 +66,7 @@ Megasync
 <screenshot></screenshot>
 
 <preinstall>
-echo "deb https://mega.nz/linux/repo/Debian_12/ ./ Packages">/etc/apt/sources.list.d/mxpitemp.list
+echo "deb https://mega.nz/linux/repo/Debian_12/ ./">/etc/apt/sources.list.d/mxpitemp.list
 curl -sS https://mega.nz/linux/repo/Debian_12/Release.key | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/megasync.gpg
 apt-get update
 </preinstall>
