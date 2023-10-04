ccd7800b8e1d73ddea34d213fa25facc55af3c2d
Dolphin Oracle <dolphinoracle@gmail.com>
2023-10-03 20:28:54 -0400

    kernel updates

--- kernel_antix-5.X_pae_32.pm
+++ kernel_antix-5.X_pae_32.pm
@@ -10,54 +10,54 @@ antiX 5.10 32 bit pae
 </name>
 
 <description>
-   <am>antiX 5.10.181 kernel pae 32 bit</am>
-   <ar>antiX 5.10.181 kernel pae 32 bit</ar>
-   <bg>antiX 5.10.181 kernel pae 32 bit</bg>
-   <bn>antiX 5.10.181 kernel pae 32 bit</bn>
-   <ca>Kernel antiX 5.10.181 PAE 32 bit</ca>
-   <cs>antiX 5.10.181 kernel pae 32 bit</cs>
-   <da>antiX 5.10.181 kernel pae 32 bit</da>
-   <de>antiX 5.10.181 Kernel pae 32 bit</de>
-   <el>antiX 5.10.181 πυρήνα pae 32 bit</el>
-   <en>antiX 5.10.181 kernel pae 32 bit</en>
-   <es>antiX 5.10.181 kernel pae 32 bit</es>
-   <et>antiX 5.10.181 kernel pae 32 bit</et>
-   <eu>antiX 5.10.181 kernel pae 32 bit</eu>
-   <fa>antiX 5.10.181 kernel pae 32 bit</fa>
-   <fil_PH>antiX 5.10.181 kernel pae 32 bit</fil_PH>
-   <fi>antiX 5.10.181 kernel pae 32 bit</fi>
-   <fr>antiX 5.10.181 kernel pae 32 bit</fr>
-   <he_IL>antiX 5.10.181 kernel pae 32 bit</he_IL>
-   <hi>antiX 5.10.181 kernel pae 32 bit</hi>
-   <hr>antiX 5.10.181 kernel pae 32 bit</hr>
-   <hu>antiX 5.10.181 kernel pae 32 bit</hu>
-   <id>antiX 5.10.181 kernel pae 32 bit</id>
-   <is>antiX 5.10.181 kernel pae 32 bit</is>
-   <it>Kernel antiX 5.10.181, 32 bit pae</it>
-   <ja_JP>antiX 5.10.181 kernel pae 32 bit</ja_JP>
-   <ja>antiX 5.10.181 kernel pae 32 bit</ja>
-   <kk>antiX 5.10.181 kernel pae 32 bit</kk>
-   <ko>antiX 5.10.181 kernel pae 32 bit</ko>
-   <lt>antiX 5.10.181 kernel pae 32 bit</lt>
-   <mk>antiX 5.10.181 kernel pae 32 bit</mk>
-   <mr>antiX 5.10.181 kernel pae 32 bit</mr>
-   <nb>antiX 5.10.181 kernel pae 32 bit</nb>
-   <nl>antiX 5.10.181 kernel pae 32 bit</nl>
-   <pl>antiX 5.10.181 kernel pae 32 bit</pl>
-   <pt_BR>antiX 5.10.181 kernel pae 32 bit</pt_BR>
-   <pt>Núcleo (kernel) antiX 5.10.181 pae 32 bit</pt>
-   <ro>antiX 5.10.181 kernel pae 32 bit</ro>
-   <ru>antiX 5.10.181 kernel pae 32 bit</ru>
-   <sk>antiX 5.10.181 kernel pae 32 bit</sk>
-   <sl>antiX 5.10.181 jedro pae 32 bitno</sl>
-   <sq>antiX 5.10.181 kernel pae 32 bit</sq>
-   <sr>antiX 5.10.181 kernel pae 32 bit</sr>
-   <sv>antiX 5.10.181 kärna pae 32 bit</sv>
-   <tr>antiX 5.10.181 kernel pae 32 bit</tr>
-   <uk>antiX 5.10.181 kernel pae 32 bit</uk>
-   <vi>antiX 5.10.181 kernel pae 32 bit</vi>
-   <zh_CN>antiX 5.10.181 kernel pae 32 bit</zh_CN>
-   <zh_TW>antiX 5.10.181 kernel pae 32 bit</zh_TW>
+   <am>antiX 5.10.197 kernel pae 32 bit</am>
+   <ar>antiX 5.10.197 kernel pae 32 bit</ar>
+   <bg>antiX 5.10.197 kernel pae 32 bit</bg>
+   <bn>antiX 5.10.197 kernel pae 32 bit</bn>
+   <ca>Kernel antiX 5.10.197 PAE 32 bit</ca>
+   <cs>antiX 5.10.197 kernel pae 32 bit</cs>
+   <da>antiX 5.10.197 kernel pae 32 bit</da>
+   <de>antiX 5.10.197 Kernel pae 32 bit</de>
+   <el>antiX 5.10.197 πυρήνα pae 32 bit</el>
+   <en>antiX 5.10.197 kernel pae 32 bit</en>
+   <es>antiX 5.10.197 kernel pae 32 bit</es>
+   <et>antiX 5.10.197 kernel pae 32 bit</et>
+   <eu>antiX 5.10.197 kernel pae 32 bit</eu>
+   <fa>antiX 5.10.197 kernel pae 32 bit</fa>
+   <fil_PH>antiX 5.10.197 kernel pae 32 bit</fil_PH>
+   <fi>antiX 5.10.197 kernel pae 32 bit</fi>
+   <fr>antiX 5.10.197 kernel pae 32 bit</fr>
+   <he_IL>antiX 5.10.197 kernel pae 32 bit</he_IL>
+   <hi>antiX 5.10.197 kernel pae 32 bit</hi>
+   <hr>antiX 5.10.197 kernel pae 32 bit</hr>
+   <hu>antiX 5.10.197 kernel pae 32 bit</hu>
+   <id>antiX 5.10.197 kernel pae 32 bit</id>
+   <is>antiX 5.10.197 kernel pae 32 bit</is>
+   <it>Kernel antiX 5.10.197, 32 bit pae</it>
+   <ja_JP>antiX 5.10.197 kernel pae 32 bit</ja_JP>
+   <ja>antiX 5.10.197 kernel pae 32 bit</ja>
+   <kk>antiX 5.10.197 kernel pae 32 bit</kk>
+   <ko>antiX 5.10.197 kernel pae 32 bit</ko>
+   <lt>antiX 5.10.197 kernel pae 32 bit</lt>
+   <mk>antiX 5.10.197 kernel pae 32 bit</mk>
+   <mr>antiX 5.10.197 kernel pae 32 bit</mr>
+   <nb>antiX 5.10.197 kernel pae 32 bit</nb>
+   <nl>antiX 5.10.197 kernel pae 32 bit</nl>
+   <pl>antiX 5.10.197 kernel pae 32 bit</pl>
+   <pt_BR>antiX 5.10.197 kernel pae 32 bit</pt_BR>
+   <pt>Núcleo (kernel) antiX 5.10.197 pae 32 bit</pt>
+   <ro>antiX 5.10.197 kernel pae 32 bit</ro>
+   <ru>antiX 5.10.197 kernel pae 32 bit</ru>
+   <sk>antiX 5.10.197 kernel pae 32 bit</sk>
+   <sl>antiX 5.10.197 jedro pae 32 bitno</sl>
+   <sq>antiX 5.10.197 kernel pae 32 bit</sq>
+   <sr>antiX 5.10.197 kernel pae 32 bit</sr>
+   <sv>antiX 5.10.197 kärna pae 32 bit</sv>
+   <tr>antiX 5.10.197 kernel pae 32 bit</tr>
+   <uk>antiX 5.10.197 kernel pae 32 bit</uk>
+   <vi>antiX 5.10.197 kernel pae 32 bit</vi>
+   <zh_CN>antiX 5.10.197 kernel pae 32 bit</zh_CN>
+   <zh_TW>antiX 5.10.197 kernel pae 32 bit</zh_TW>
 </description>
 
 <installable>
@@ -73,20 +73,20 @@ apt-get update
 </preinstall>
 
 <install_package_names>
-linux-image-5.10.181-antix.1-686-smp-pae
-linux-headers-5.10.181-antix.1-686-smp-pae
+linux-image-5.10.197-antix.1-686-smp-pae
+linux-headers-5.10.197-antix.1-686-smp-pae
 </install_package_names>
 
 
 <postinstall>
 rm /etc/apt/sources.list.d/mxpitemp.list
 apt-get update
-rebuild_dkms_packages.sh linux-image-5.10.181-antix.1-686-smp-pae
+rebuild_dkms_packages.sh linux-image-5.10.197-antix.1-686-smp-pae
 </postinstall>
 
 
 <uninstall_package_names>
-linux-image-5.10.181-antix.1-686-smp-pae
-linux-headers-5.10.181-antix.1-686-smp-pae
+linux-image-5.10.197-antix.1-686-smp-pae
+linux-headers-5.10.197-antix.1-686-smp-pae
 </uninstall_package_names>
 </app>
