43ab325467cc128237ca4f0e92980b8806092a08
Dolphin Oracle <dolphinoracle@gmail.com>
2023-11-26 19:50:54 -0500

    arm64 version of kodi for raspOS repos packing

---
+++ kodi-arm64.pm
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Media Center
+</category>
+
+<name>
+Kodi
+</name>
+
+<description>
+   <am>Kodi Media Center (formerly XBMC)</am>
+   <ar>Kodi Media Center (formerly XBMC)</ar>
+   <bg>Kodi Media Center (formerly XBMC)</bg>
+   <bn>Kodi Media Center (formerly XBMC)</bn>
+   <ca>Centre de Mèdia Kodi (anteriorment XBMC)</ca>
+   <cs>Kodi Media Center (formerly XBMC)</cs>
+   <da>Kodi Media Center (tidligere XBMC)</da>
+   <de>Kodi Media Center (ehemals XBMC)</de>
+   <el>Κέντρο πολυμέσων Kodi (πρώην XBMC)</el>
+   <en>Kodi Media Center (formerly XBMC)</en>
+   <es>Kodi Media Center (anteriormente conocido como XBMC)</es>
+   <et>Kodi Media Center (formerly XBMC)</et>
+   <eu>Kodi Media Center (formerly XBMC)</eu>
+   <fa>Kodi Media Center (formerly XBMC)</fa>
+   <fil_PH>Kodi Media Center (formerly XBMC)</fil_PH>
+   <fi>Kodi-mediakeskus (tunnettiin aikasemmin nimellä XBMC)</fi>
+   <fr>Kodi Media Center (anciennement XBMC)</fr>
+   <he_IL>Kodi Media Center (formerly XBMC)</he_IL>
+   <hi>Kodi मीडिया केंद्र (पूर्व नाम XBMC)</hi>
+   <hr>Kodi Media Center (formerly XBMC)</hr>
+   <hu>Kodi Media Center (formerly XBMC)</hu>
+   <id>Kodi Media Center (formerly XBMC)</id>
+   <is>Kodi Media Center (formerly XBMC)</is>
+   <it>Kodi Media Center (in precedenza XBMC)</it>
+   <ja>Kodi メディアセンタ（旧 XBMC）</ja>
+   <kk>Kodi Media Center (formerly XBMC)</kk>
+   <ko>Kodi Media Center (formerly XBMC)</ko>
+   <lt>Kodi medijos centras (buvęs XBMC)</lt>
+   <mk>Kodi Media Center (formerly XBMC)</mk>
+   <mr>Kodi Media Center (formerly XBMC)</mr>
+   <nb>Kodi Media Center (het tidligere XBMC)</nb>
+   <nl>Kodi Media Centrum (voorheen XBMC)</nl>
+   <pl>Kodi Media Center (poprzednio XBMC)</pl>
+   <pt_BR>Kodi - Centro de Multimídia (anteriormente chamado de XBMC)</pt_BR>
+   <pt>Centro Multimédia Kodi (anteriormente chamava-se XBMC)</pt>
+   <ro>Kodi Media Center (formerly XBMC)</ro>
+   <ru>Медиацентр Kodi (бывший XBMC)</ru>
+   <sk>Kodi Media Center (formerly XBMC)</sk>
+   <sl>Kodi medijsko središče (bivši XBMC)</sl>
+   <sq>Qendra e Medias Kodi (ish-XBMC)</sq>
+   <sr>Kodi Media Center (formerly XBMC)</sr>
+   <sv>Kodi Media Center (tidigare XBMC)</sv>
+   <tr>Kodi Ortam Merkezi (eskiden XBMC)</tr>
+   <uk>Kodi Media Center (колишній XBMC)</uk>
+   <vi>Kodi Media Center (formerly XBMC)</vi>
+   <zh_CN>Kodi Media Center (formerly XBMC)</zh_CN>
+   <zh_TW>Kodi Media Center (formerly XBMC)</zh_TW>
+</description>
+
+<installable>
+arm64
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/13655/simage/large-416335837027fc248977245b4bf5d9e5.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+kodi
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+kodi
+</uninstall_package_names>
+</app>
