185a8ba84091be1d7f94910a2d9b9832fb8f1dca
Dolphin Oracle <dolphinoracle@gmail.com>
2023-12-07 17:55:11 -0500

    get version location

--- get-veracrypt.sh
+++ get-veracrypt.sh
@@ -20,12 +20,15 @@ arch=$(dpkg --print-architecture)
 
 #process text file to get version number 
 version=$(grep Debian-12 /tmp/veracrypt.txt | grep -v console| grep launchpad |cut -d"=" -f1 |cut -d"<" -f1 |cut -d">" -f1 |grep "$arch"|awk '{print $1}')
+trunk=$(echo $version |cut -d"-" -f2)
+echo $version
+echo $trunk
 if [ -e "/tmp/veracrypt.txt" ]; then
 	rm /tmp/veracrypt.txt
 fi
 
 #get vercrypt deb
-wget https://launchpad.net/veracrypt/trunk/1.26.7/+download/$version -O /tmp/veracrypt.deb
+wget https://launchpad.net/veracrypt/trunk/$trunk/+download/$version -O /tmp/veracrypt.deb
 if [ -e /tmp/veracrypt.deb ]; then
 	apt-get install /tmp/veracrypt.deb
 	rm /tmp/veracrypt.deb
