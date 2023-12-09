a2b37863a23f35dce4213b9b3d2ad43e6b84288e
Dolphin Oracle <dolphinoracle@gmail.com>
2023-12-09 00:27:33 -0500

    update liqourix, add liquorix metapackage

---
+++ kernel_liqourix_metapackage.pm
@@ -0,0 +1,98 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Kernels
+</category>
+
+<name>
+Liquorix 64 bit (ahs updates package)
+</name>
+
+<description>
+<am>Liquorix (የአህስ ማሻሻያ ጥቅል፣ ለራስ-ሰር ዝማኔዎች እንዲነቃ ያስፈልጋል)</am>
+<ar>Liquorix (ﺕﺎﺜﻳﺪﺤﺗ ﺔﻣﺰﺣ ahs، ﻦﻴﻜﻤﺗ ﺐﻠﻄﺘﺗ ahs ﺔﻴﺋﺎﻘﻠﺘﻟﺍ ﺕﺎﺜﻳﺪﺤﺘﻠﻟ)</ar>
+<bg>Liquorix (ahs пакет за актуализации, изисква ahs да бъде активиран за автоматични актуализации)</bg>
+<bn>Liquorix (ahs আপডেট প্যাকেজ, স্বয়ংক্রিয় আপডেটের জন্য ahs সক্ষম করা প্রয়োজন)</bn>
+<ca>Liquorix (paquet d'actualitzacions d'ahs, requereix que ahs estigui habilitat per a actualitzacions automàtiques)</ca>
+<cs>Liquorix (balíček aktualizací ahs, vyžaduje povolení ahs pro automatické aktualizace)</cs>
+<da>Liquorix (ahs opdateringspakke, kræver at ahs er aktiveret for automatiske opdateringer)</da>
+<de>Liquorix (ahs-Updates-Paket, erfordert die Aktivierung von ahs für automatische Updates)</de>
+<el>Liquorix (πακέτο ενημερώσεων ahs, απαιτεί το ahs να είναι ενεργοποιημένο για αυτόματες ενημερώσεις)</el>
+<es>Liquorix (paquete de actualizaciones de ahs, requiere que ahs esté habilitado para actualizaciones automáticas)</es>
+<et>Liquorix (ahs-i värskenduste pakett, automaatsete värskenduste jaoks peab ah olema lubatud)</et>
+<eu>Liquorix (ahs eguneratze paketea, eguneratze automatikoetarako ahs gaituta egotea eskatzen du)</eu>
+<en>Liquorix (ahs updates package, requires ahs be enabled for automatic updates)</en>
+<fa>Liquorix (ﻪﺑ ﻪﺘﺴﺑu200cﯽﻧﺎﺳﺭﺯﻭﺭu200cﯼﺎﻫ ahs، ﻪﺑ ﯼﺍﺮﺑu200cﺪﯾﺎﺑ ﺭﺎﮐﺩﻮﺧ ﯽﻧﺎﺳﺭﺯﻭﺭ ahs ﺪﺷﺎﺑ ﻝﺎﻌﻓ)</fa>
+<fil_PH>Liquorix (ahs update package, nangangailangan ng ahs na paganahin para sa mga awtomatikong update)</fil_PH>
+<fi>Liquorix (ahs-päivityspaketti, vaatii ah:n olevan käytössä automaattisia päivityksiä varten)</fi>
+<fr>Liquorix (package de mises à jour ahs, nécessite qu'ahs soit activé pour les mises à jour automatiques)</fr>
+<he_IL>Liquorix (ינוכדע תליבח ahs, לש הלעפה תשרוד ahs םייטמוטוא םינוכדע רובע)</he_IL>
+<hi>लिकोरिक्स (एएचएस अपडेट पैकेज, स्वचालित अपडेट के लिए एएचएस सक्षम होना आवश्यक है)</hi>
+<hr>Liquorix (ahs paket ažuriranja, zahtijeva da ahs bude omogućen za automatska ažuriranja)</hr>
+<hu>Liquorix (ahs frissítési csomag, az automatikus frissítésekhez engedélyezni kell az ah-t)</hu>
+<id>Liquorix (paket pembaruan ahs, memerlukan ahs diaktifkan untuk pembaruan otomatis)</id>
+<is>Liquorix (ahs uppfærslupakki, krefst þess að ahs sé virkt fyrir sjálfvirkar uppfærslur)</is>
+<it>Liquorix (pacchetto aggiornamenti ahs, richiede che ahs sia abilitato per gli aggiornamenti automatici)</it>
+<ja>Liquorix (ahs 更新パッケージ。自動更新には ahs を有効にする必要があります)</ja>
+<kk>Liquorix (ahs жаңартулар пакеті, автоматты жаңартулар үшін ahs қосулы болуы қажет)</kk>
+<ko>Liquorix(ahs 업데이트 패키지, 자동 업데이트를 위해 ahs를 활성화해야 함)</ko>
+<lt>Liquorix (ahs naujinimų paketas, norint automatiškai atnaujinti reikia įjungti ahs)</lt>
+<mk>Liquorix (пакет за ажурирања на ahs, бара овозможено ahs за автоматско ажурирање)</mk>
+<mr>Liquorix (ahs अद्यतन पॅकेज, स्वयंचलित अद्यतनांसाठी ahs सक्षम करणे आवश्यक आहे)</mr>
+<nb>Liquorix (ahs oppdateringspakke, krever at ahs er aktivert for automatiske oppdateringer)</nb>
+<nl>Liquorix (ahs-updatepakket, vereist dat ahs is ingeschakeld voor automatische updates)</nl>
+<pl>Liquorix (pakiet aktualizacji ahs, wymaga włączenia ahs do automatycznych aktualizacji)</pl>
+<pt_BR>Liquorix (pacote de atualizações ahs, requer que o ahs esteja habilitado para atualizações automáticas)</pt_BR>
+<pt>Liquorix (pacote de atualizações ahs, requer que o ahs esteja habilitado para atualizações automáticas)</pt>
+<ro>Liquorix (pachet de actualizări ahs, necesită activarea ahs pentru actualizări automate)</ro>
+<ru>Liquorix (пакет обновлений ahs, для автоматического обновления требуется включить ahs)</ru>
+<sk>Liquorix (balíček aktualizácií ahs, vyžaduje, aby bol ahs povolený pre automatické aktualizácie)</sk>
+<sl>Liquorix (paket posodobitev ahs, zahteva, da je ahs omogočen za samodejne posodobitve)</sl>
+<sq>Liquorix (paketa e përditësimeve të ahs, kërkon që ahs të aktivizohet për përditësime automatike)</sq>
+<sr>Ликуорик (ахс пакет ажурирања, захтева да ахс буде омогућен за аутоматска ажурирања)</sr>
+<sv>Liquorix (ahs uppdateringspaket, kräver att ahs är aktiverat för automatiska uppdateringar)</sv>
+<tr>Liquorix (ahs güncelleme paketi, otomatik güncellemeler için ahs'nin etkinleştirilmesini gerektirir)</tr>
+<uk>Liquorix (пакет оновлень ahs, для автоматичного оновлення потрібно ввімкнути ahs)</uk>
+<vi>Liquorix (gói cập nhật ahs, yêu cầu kích hoạt ahs để cập nhật tự động)</vi>
+</description>
+
+<installable>
+64
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+if [ $(apt-get update --print-uris | grep -c -m1 -E "/mx/repo/dists/bookworm/ahs/") = 0 ]; then
+MXREPO=$(apt-get update --print-uris | grep -oE "https?://.*/mx/repo/dists/bookworm/main" | tail -1 | sed "s:^:deb :; s:/repo/dists/:/repo/ :; s:/main: ahs:")
+: ${MXREPO:=deb http://mxrepo.com/mx/repo/ bookworm ahs}
+echo "$MXREPO" > /etc/apt/sources.list.d/mxpitemp.list
+apt-get update 
+fi
+DKMS_PKGS=($(dpkg-query -f '${db:Status-Abbrev}\t${Package}\n' -W  -- '*-dkms' | grep ^i | grep -- '-dkms$' | cut -d$'\t' -f2))
+apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64 ${DKMS_PKGS[*]}
+</preinstall>
+
+<install_package_names>
+
+</install_package_names>
+
+
+<postinstall>
+file=$(apt-cache show linux-image-liquorix-amd64 |grep -m1 Depends) 
+file=${file#"Depends:"}
+rebuild_dkms_packages.sh $file
+if [ -f /etc/apt/sources.list.d/mxpitemp.list ]; then
+rm /etc/apt/sources.list.d/mxpitemp.list
+apt-get update
+fi
+echo "...$(gettext -d apt -s ' Done')!"
+</postinstall>
+
+
+<uninstall_package_names>
+linux-image-liquorix-amd64
+linux-headers-liquorix-amd64
+</uninstall_package_names>
+</app>
