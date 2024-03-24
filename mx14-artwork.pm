ad5d4e562018cb95bf0b8fc41d92e954cbffba65
Dolphin Oracle <dolphinoracle@gmail.com>
2024-03-24 10:46:04 -0400

    add mx14-artwork

---
+++ mx14-artwork.pm
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Wallpapers
+</category>
+
+<name>
+MX 14 Wallpapers
+</name>
+
+<description>
+   <am>backgrounds originally supplied with MX 14</am>
+   <ar>backgrounds originally supplied with MX 14</ar>
+   <bg>backgrounds originally supplied with MX 14</bg>
+   <bn>backgrounds originally supplied with MX 14</bn>
+   <ca>fons de pantalla subministrats amb MX 14</ca>
+   <cs>backgrounds originally supplied with MX 14</cs>
+   <da>baggrunde fra MX 14</da>
+   <de>Bildschirmhintergründe, die ursprünglich mit MX 14 geliefert wurden</de>
+   <el>Ταπετσαρίες που παρέχονται αρχικά με το MX 14</el>
+   <en>backgrounds originally supplied with MX 14</en>
+   <es>Fondos de escritorio originalmente suministrados con MX 14</es>
+   <et>backgrounds originally supplied with MX 14</et>
+   <eu>backgrounds originally supplied with MX 14</eu>
+   <fa>backgrounds originally supplied with MX 14</fa>
+   <fil_PH>backgrounds originally supplied with MX 14</fil_PH>
+   <fi>taustakuvat jotka toimitettiin alunperin MX 14:sta mukana</fi>
+   <fr>Fonds d’écran initialement fourni avec MX 14</fr>
+   <he_IL>backgrounds originally supplied with MX 14</he_IL>
+   <hi>एमएक्स 14 के साथ प्रदान किए गए वॉलपेपर</hi>
+   <hr>backgrounds originally supplied with MX 14</hr>
+   <hu>backgrounds originally supplied with MX 14</hu>
+   <id>backgrounds originally supplied with MX 14</id>
+   <is>backgrounds originally supplied with MX 14</is>
+   <it>sfondi originariamente forniti con MX 14</it>
+   <ja>MX 14 に同梱されているオリジナルな背景</ja>
+   <kk>backgrounds originally supplied with MX 14</kk>
+   <ko>backgrounds originally supplied with MX 14</ko>
+   <lt>backgrounds originally supplied with MX 14</lt>
+   <mk>backgrounds originally supplied with MX 14</mk>
+   <mr>backgrounds originally supplied with MX 14</mr>
+   <nb>bakgrunnsbilder som kom med MX 14</nb>
+   <nl>achtergronden oorspronkelijk geleverd met MX 14</nl>
+   <pl>tła oryginalnie dostarczone z MX 14</pl>
+   <pt_BR>Imagens de plano de fundo originalmente fornecidas no MX 14</pt_BR>
+   <pt>Imagens de fundo originalmente incluídas no MX 14</pt>
+   <ro>backgrounds originally supplied with MX 14</ro>
+   <ru>обои, первоначально выходившие с MX 14</ru>
+   <sk>backgrounds originally supplied with MX 14</sk>
+   <sl>Izvirna ozadja za MX 14 </sl>
+   <sq>sfonde dhënë fillimisht me MX 14</sq>
+   <sr>backgrounds originally supplied with MX 14</sr>
+   <sv>wallpapers ursprungligen medföljande MX 14</sv>
+   <tr>MX 14 ile sağlanan özgün arkaplanlar</tr>
+   <uk>оригінальні шпалери для MX 14</uk>
+   <vi>backgrounds originally supplied with MX 14</vi>
+   <zh_CN>backgrounds originally supplied with MX 14</zh_CN>
+   <zh_TW>backgrounds originally supplied with MX 14</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+mx14-artwork
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+mx14-artwork
+</uninstall_package_names>
+</app>
