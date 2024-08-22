e0e839064cb450bed73c668bd7b649cba9eeb7f0
Dolphin Oracle <dolphinoracle@gmail.com>
2024-08-22 08:57:19 -0400

    add arch specific tag to brave repo definition

--- brave.pm
+++ brave.pm
@@ -67,7 +67,7 @@ Brave
 
 <preinstall>
 curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
 apt-get update
 
 </preinstall>
