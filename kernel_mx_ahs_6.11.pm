e48060d5bb21664a320fda6af5020afd728c828b
Dolphin Oracle <dolphinoracle@gmail.com>
2024-10-25 18:20:33 -0400

    update debian 6.11 kernel

--- kernel_mx_ahs_6.11.pm
+++ kernel_mx_ahs_6.11.pm
@@ -6,55 +6,55 @@ Kernels
 </category>
 
 <name>
-Debian 6.11.2 64 bit (AHS)
+Debian 6.11.4 64 bit (AHS)
 </name>
 
 <description>
-<am>ዴቢያን 6.11.2፣ 64 ቢት የቅርብ ጊዜ ከMX repo</am>
-<ar>Debian 6.11.2 ، ﻦﻣ ﺙﺪﺣﻷ﻿ﺍ ﺖﺑ 64 ﺭﺍﺪﺻﻹ﻿ﺍ MX repo</ar>
-<bg>Debian 6.11.2, 64-битов най-нов от MX repo</bg>
-<bn>ডেবিয়ান 6.11.2, MX রেপো থেকে 64 বিট সর্বশেষ</bn>
-<ca>Debian 6.11.2, 64 bits més recent de MX repo</ca>
-<cs>Debian 6.11.2, 64 bit nejnovější z MX repo</cs>
-<da>Debian 6.11.2, 64 bit seneste fra MX repo</da>
-<de>Debian 6.11.2, 64 Bit, neueste Version aus dem MX-Repo</de>
-<el>Debian 6.11.2, 64 bit πιο πρόσφατο από το repo MX</el>
-<es>Debian 6.11.2, 64 bit más reciente del repositorio MX</es>
-<et>Debian 6.11.2, 64-bitine uusim versioon MX repost</et>
-<eu>Debian 6.11.2, 64 biteko azkena MX repo-tik</eu>
-<en>Debian 6.11.2, 64 bit latest from MX repo</en>
-<fa>Debian 6.11.2، 64 ﺯﺍ ﻦﯾﺮﺗﺪﯾﺪﺟ ﺖﯿﺑ MX repo</fa>
-<fil_PH>Debian 6.11.2, 64 bit pinakabago mula sa MX repo</fil_PH>
-<fi>Debian 6.11.2, 64-bittinen uusin MX reposta</fi>
-<fr>Debian 6.11.2, dernière version 64 bits du référentiel MX</fr>
-<he_IL>MX repo -מ ןורחאה תויביס 64 ,6.11.2 ןאיבד</he_IL>
-<hi>डेबियन 6.11.2, एमएक्स रेपो से 64 बिट नवीनतम</hi>
-<hr>Debian 6.11.2, 64-bitni najnoviji iz MX repoa</hr>
-<hu>Debian 6.11.2, 64 bit legfrissebb az MX repóból</hu>
-<id>Debian 6.11.2, 64 bit terbaru dari repo MX</id>
-<is>Debian 6.11.2, 64 bita nýjasta frá MX repo</is>
-<it>Debian 6.11.2, 64 bit più recente dal repository MX</it>
-<ja>Debian 6.11.2、MX リポジトリからの最新の 64 ビット</ja>
-<kk>Debian 6.11.2, MX репосынан ең соңғы 64 бит</kk>
-<ko>Debian 6.11.2, MX 저장소의 최신 64비트</ko>
-<lt>Debian 6.11.2, 64 bitų naujausia versija iš MX repo</lt>
-<mk>Debian 6.11.2, најново 64 битно од MX repo</mk>
-<mr>डेबियन 6.11.2, MX रेपो वरून 64 बिट नवीनतम</mr>
-<nb>Debian 6.11.2, 64 bit nyeste fra MX repo</nb>
-<nl>Debian 6.11.2, 64 bit laatste van MX repo</nl>
-<pl>Debian 6.11.2, 64-bitowy najnowszy z repozytorium MX</pl>
-<pt_BR>Debian 6.11.2, 64 bits mais recente do repositório MX</pt_BR>
-<pt>Debian 6.11.2, 64 bits mais recente do repositório MX</pt>
-<ro>Debian 6.11.2, cel mai recent 64 de biți de la MX repo</ro>
-<ru>Debian 6.11.2, последняя 64-разрядная версия из репозитория MX</ru>
-<sk>Debian 6.11.2, 64-bitová najnovšia verzia MX repo</sk>
-<sl>Debian 6.11.2, 64-bitni najnovejši iz repoja MX</sl>
-<sq>Debian 6.11.2, 64 bit më i fundit nga repo MX</sq>
-<sr>Дебиан 6.11.2, 64-битни најновији из МКС репо</sr>
-<sv>Debian 6.11.2, 64-bitars senaste från MX-repo</sv>
-<tr>Debian 6.11.2, MX deposundan en son 64 bit</tr>
-<uk>Debian 6.11.2, остання 64-розрядна версія з репозиторію MX</uk>
-<vi>Debian 6.11.2, 64 bit mới nhất từ MX repo</vi>
+<am>ዴቢያን 6.11.4፣ 64 ቢት የቅርብ ጊዜ ከMX repo</am>
+<ar>Debian 6.11.4 ، ﻦﻣ ﺙﺪﺣﻷ﻿ﺍ ﺖﺑ 64 ﺭﺍﺪﺻﻹ﻿ﺍ MX repo</ar>
+<bg>Debian 6.11.4, 64-битов най-нов от MX repo</bg>
+<bn>ডেবিয়ান 6.11.4, MX রেপো থেকে 64 বিট সর্বশেষ</bn>
+<ca>Debian 6.11.4, 64 bits més recent de MX repo</ca>
+<cs>Debian 6.11.4, 64 bit nejnovější z MX repo</cs>
+<da>Debian 6.11.4, 64 bit seneste fra MX repo</da>
+<de>Debian 6.11.4, 64 Bit, neueste Version aus dem MX-Repo</de>
+<el>Debian 6.11.4, 64 bit πιο πρόσφατο από το repo MX</el>
+<es>Debian 6.11.4, 64 bit más reciente del repositorio MX</es>
+<et>Debian 6.11.4, 64-bitine uusim versioon MX repost</et>
+<eu>Debian 6.11.4, 64 biteko azkena MX repo-tik</eu>
+<en>Debian 6.11.4, 64 bit latest from MX repo</en>
+<fa>Debian 6.11.4، 64 ﺯﺍ ﻦﯾﺮﺗﺪﯾﺪﺟ ﺖﯿﺑ MX repo</fa>
+<fil_PH>Debian 6.11.4, 64 bit pinakabago mula sa MX repo</fil_PH>
+<fi>Debian 6.11.4, 64-bittinen uusin MX reposta</fi>
+<fr>Debian 6.11.4, dernière version 64 bits du référentiel MX</fr>
+<he_IL>MX repo -מ ןורחאה תויביס 64 ,6.11.4 ןאיבד</he_IL>
+<hi>डेबियन 6.11.4, एमएक्स रेपो से 64 बिट नवीनतम</hi>
+<hr>Debian 6.11.4, 64-bitni najnoviji iz MX repoa</hr>
+<hu>Debian 6.11.4, 64 bit legfrissebb az MX repóból</hu>
+<id>Debian 6.11.4, 64 bit terbaru dari repo MX</id>
+<is>Debian 6.11.4, 64 bita nýjasta frá MX repo</is>
+<it>Debian 6.11.4, 64 bit più recente dal repository MX</it>
+<ja>Debian 6.11.4、MX リポジトリからの最新の 64 ビット</ja>
+<kk>Debian 6.11.4, MX репосынан ең соңғы 64 бит</kk>
+<ko>Debian 6.11.4, MX 저장소의 최신 64비트</ko>
+<lt>Debian 6.11.4, 64 bitų naujausia versija iš MX repo</lt>
+<mk>Debian 6.11.4, најново 64 битно од MX repo</mk>
+<mr>डेबियन 6.11.4, MX रेपो वरून 64 बिट नवीनतम</mr>
+<nb>Debian 6.11.4, 64 bit nyeste fra MX repo</nb>
+<nl>Debian 6.11.4, 64 bit laatste van MX repo</nl>
+<pl>Debian 6.11.4, 64-bitowy najnowszy z repozytorium MX</pl>
+<pt_BR>Debian 6.11.4, 64 bits mais recente do repositório MX</pt_BR>
+<pt>Debian 6.11.4, 64 bits mais recente do repositório MX</pt>
+<ro>Debian 6.11.4, cel mai recent 64 de biți de la MX repo</ro>
+<ru>Debian 6.11.4, последняя 64-разрядная версия из репозитория MX</ru>
+<sk>Debian 6.11.4, 64-bitová najnovšia verzia MX repo</sk>
+<sl>Debian 6.11.4, 64-bitni najnovejši iz repoja MX</sl>
+<sq>Debian 6.11.4, 64 bit më i fundit nga repo MX</sq>
+<sr>Дебиан 6.11.4, 64-битни најновији из МКС репо</sr>
+<sv>Debian 6.11.4, 64-bitars senaste från MX-repo</sv>
+<tr>Debian 6.11.4, MX deposundan en son 64 bit</tr>
+<uk>Debian 6.11.4, остання 64-розрядна версія з репозиторію MX</uk>
+<vi>Debian 6.11.4, 64 bit mới nhất từ MX repo</vi>
 </description>
 
 <installable>
@@ -71,7 +71,7 @@ echo "$MXREPO" > /etc/apt/sources.list.d/mxpitemp.list
 apt-get update
 fi
 DKMS_PKGS=($(dpkg-query -f '${db:Status-Abbrev}\t${Package}\n' -W  -- '*-dkms' | grep ^i | grep -- '-dkms$' | cut -d$'\t' -f2))
-apt-get install linux-image-6.11.2-amd64-unsigned linux-headers-6.11.2-amd64 ${DKMS_PKGS[*]}
+apt-get install linux-image-6.11.4-amd64-unsigned linux-headers-6.11.4-amd64 ${DKMS_PKGS[*]}
 </preinstall>
 
 <install_package_names>
@@ -80,7 +80,7 @@ apt-get install linux-image-6.11.2-amd64-unsigned linux-headers-6.11.2-amd64 ${D
 
 
 <postinstall>
-rebuild_dkms_packages.sh linux-image-6.11.2-amd64-unsigned
+rebuild_dkms_packages.sh linux-image-6.11.4-amd64-unsigned
 if [ -f /etc/apt/sources.list.d/mxpitemp.list ]; then
 rm /etc/apt/sources.list.d/mxpitemp.list
 apt-get update
@@ -90,7 +90,7 @@ echo "...$(gettext -d apt -s ' Done')!"
 
 
 <uninstall_package_names>
-linux-image-6.11.2-amd64-unsigned
-linux-headers-6.11.2-amd64
+linux-image-6.11.4-amd64-unsigned
+linux-headers-6.11.4-amd64
 </uninstall_package_names>
 </app>
