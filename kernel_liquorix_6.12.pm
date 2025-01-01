f2b8e12aa96a0b6c1cb1a839739ff9870b7b9ba1
Dolphin Oracle <dolphinoracle@gmail.com>
2025-01-01 11:28:12 -0500

    add liquorix 6.12.7

--- kernel_liquorix_6.12.pm
+++ kernel_liquorix_6.12.pm
@@ -6,11 +6,11 @@ Kernels
 </category>
 
 <name>
-Liquorix 6.12.6-1 64 bit
+Liquorix 6.12.7-2 64 bit
 </name>
 
 <description>
-   Liquorix 6.12.6-1
+   Liquorix 6.12.7-2
 </description>
 
 <installable>
@@ -27,7 +27,7 @@ echo "$MXREPO" > /etc/apt/sources.list.d/mxpitemp.list
 apt-get update 
 fi
 DKMS_PKGS=($(dpkg-query -f '${db:Status-Abbrev}\t${Package}\n' -W  -- '*-dkms' | grep ^i | grep -- '-dkms$' | cut -d$'\t' -f2))
-apt-get install linux-image-6.12.6-1-liquorix-amd64 linux-headers-6.12.6-1-liquorix-amd64 ${DKMS_PKGS[*]}
+apt-get install linux-image-6.12.7-2-liquorix-amd64 linux-headers-6.12.7-2-liquorix-amd64 ${DKMS_PKGS[*]}
 </preinstall>
 
 <install_package_names>
@@ -36,7 +36,7 @@ apt-get install linux-image-6.12.6-1-liquorix-amd64 linux-headers-6.12.6-1-liquo
 
 
 <postinstall>
-rebuild_dkms_packages.sh linux-image-6.12.6-1-liquorix-amd64
+rebuild_dkms_packages.sh linux-image-6.12.7-2-liquorix-amd64
 if [ -f /etc/apt/sources.list.d/mxpitemp.list ]; then
 rm /etc/apt/sources.list.d/mxpitemp.list
 apt-get update
@@ -46,7 +46,7 @@ echo "...$(gettext -d apt -s ' Done')!"
 
 
 <uninstall_package_names>
-linux-image-6.12.6-1-liquorix-amd64
-linux-headers-6.12.6-1-liquorix-amd64
+linux-image-6.12.7-2-liquorix-amd64
+linux-headers-6.12.7-2-liquorix-amd64
 </uninstall_package_names>
 </app>
