19a83f0239820f1a360749ffb3e1f5104fdb889e
Dolphin Oracle <dolphinoracle@gmail.com>
2025-05-28 21:02:48 -0400

    descriptions for slimjet

--- slimjet.pm
+++ slimjet.pm
@@ -10,7 +10,51 @@ SlimJet
 </name>
 
 <description>
-   SlimJet
+<am>Chromium ላይ የተመሠረተ የማስታወቂያ አሳሽ አሳሽ</am>
+<ar>ﻡﻭﺮﻜﻟﺍ ﻰﻠﻋ ﻢﺋﺎﻘﻟﺍ ﺕﺎﻧﻼ﻿ﻋﻹ﻿ﺍ ﺮﻈﺣ ﺢﻔﺼﺘﻣ</ar>
+<bg>Браузън на базата на хром на базата на хром</bg>
+<bn>ক্রোমিয়াম ভিত্তিক বিজ্ঞাপন ব্লকিং ব্রাউজার</bn>
+<ca>navegador de bloqueig d'anuncis basat en crom</ca>
+<cs>prohlížeč blokující reklamu na bázi chromu</cs>
+<da>Krombaseret annonceblokeringsbrowser</da>
+<de>AD -Blockierbrowser auf Chrombasis</de>
+<el>Πρόγραμμα περιήγησης με βάση το χρωμίου με βάση το χρωμίου</el>
+<es>navegador de bloqueo de anuncios a base de cromo</es>
+<et>kroomipõhine AD blokeeriv brauser</et>
+<eu>Chromium oinarritutako iragarkia blokeatzeko arakatzailea</eu>
+<en>chromium based ad blocking browser</en>
+<fa>ﻡﻭﺮﮐ ﺮﺑ ﯽﻨﺘﺒﻣ ﺕﺎﻐﯿﻠﺒﺗ ﻩﺪﻨﻨﮐ ﺩﻭﺪﺴﻣ ﺮﮔﺭﻭﺮﻣ</fa>
+<fil_PH>Chromium based ad blocking browser</fil_PH>
+<fi>Kromipohjainen AD -esto -selain</fi>
+<fr>navigateur de blocage publicitaire à base de chrome</fr>
+<he_IL>םורכ ססובמ תועדומ תמיסח ןפדפד</he_IL>
+<hi>क्रोमियम आधारित विज्ञापन अवरोधक ब्राउज़र</hi>
+<hr>Preglednik koji blokira oglas na bazi kroma</hr>
+<hu>króm alapú hirdetésblokkoló böngésző</hu>
+<id>browser pemblokiran iklan berbasis kromium</id>
+<is>króm byggð auglýsingastífluvafri</is>
+<it>browser bloccante a base di cromo</it>
+<ja>クロムベースの広告ブロッキングブラウザー</ja>
+<kk>Хром негізіндегі жарнамалық шолғыш</kk>
+<ko>크롬 기반 AD 차단 브라우저</ko>
+<lt>Chromo pagrindu pagaminta AD blokavimo naršyklė</lt>
+<mk>Прелистувач за блокирање на реклами засновани на хром</mk>
+<mr>क्रोमियम आधारित एडी ब्लॉकिंग ब्राउझर</mr>
+<nb>Krombasert annonseblokkerende nettleser</nb>
+<nl>Op chroom gebaseerde advertentiebrowser</nl>
+<pl>przeglądarka blokująca reklamy na bazie chromu</pl>
+<pt_BR>navegador de bloqueio de anúncios à base de cromo</pt_BR>
+<pt>navegador de bloqueio de anúncios à base de cromo</pt>
+<ro>browser de blocare a anunțurilor pe bază de crom</ro>
+<ru>Благословение рекламы на основе хрома</ru>
+<sk>prehliadač blokovania reklám na báze chrómu</sk>
+<sl>Brskalnik za blokiranje oglasov na osnovi kroma</sl>
+<sq>Shfletuesi i bllokimit të reklamave të bazuara në krom</sq>
+<sr>Прегледник за блокирање на бази хромима</sr>
+<sv>Krombaserad annonsblockerings webbläsare</sv>
+<tr>krom tabanlı reklam engelleme tarayıcısı</tr>
+<uk>браузер, що блокує рекламу на основі хрому</uk>
+<vi>Trình duyệt chặn quảng cáo dựa trên crom</vi>
 </description>
 
 <installable>
