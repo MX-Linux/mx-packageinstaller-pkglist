0b0ebc365d914763d65d1d23e2a474c63318aa42
Dolphin Oracle <dolphinoracle@gmail.com>
2025-06-10 20:59:31 -0400

    add arch qualifier to vscodium

--- vscodium.pm
+++ vscodium.pm
@@ -67,7 +67,7 @@ vscodium
 
 <preinstall>
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg |gpg --dearmor -o /usr/share/keyrings/vscodium-archive-keyring.gpg
-echo "deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main">/etc/apt/sources.list.d/vscodium.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main">/etc/apt/sources.list.d/vscodium.list
 apt-get update
 </preinstall>
 
