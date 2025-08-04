f04571a639fffd4df13cde8314c3cd40028e91b3
Dolphin Oracle <dolphinoracle@gmail.com>
2025-08-03 22:23:32 -0400

    update java entry

--- java.pm
+++ java.pm
@@ -10,53 +10,53 @@ Java
 </name>
 
 <description>
-   <am>Java 17- installs openjdk-17</am>
-   <ar>Java 17- installs openjdk-17</ar>
-   <bg>Java 17- installs openjdk-17</bg>
-   <bn>Java 17- installs openjdk-17</bn>
-   <ca>Java 17- instal·la openjdk-17</ca>
-   <cs>Java 17- installs openjdk-17</cs>
-   <da>Java 17- installs openjdk-17</da>
-   <de>Java 17- installiert openjdk-17</de>
-   <el>Java 17- εγκατάσταση του openjdk-17</el>
-   <en>Java 17- installs openjdk-17</en>
-   <es>Java 17; instala openjdk-17</es>
-   <et>Java 17- installs openjdk-17</et>
-   <eu>Java 17- installs openjdk-17</eu>
-   <fa>Java 17- installs openjdk-17</fa>
-   <fil_PH>Java 17- installs openjdk-17</fil_PH>
-   <fi>Java 17- installs openjdk-17</fi>
-   <fr>Java 17- installe openjdk-17</fr>
-   <he_IL>Java 17- installs openjdk-17</he_IL>
-   <hi>Java 17- installs openjdk-17</hi>
-   <hr>Java 17- installs openjdk-17</hr>
-   <hu>Java 17- installs openjdk-17</hu>
-   <id>Java 17- installs openjdk-17</id>
-   <is>Java 17- installs openjdk-17</is>
-   <it>Java 17- installa openjdk-17</it>
-   <ja>Java 17- installs openjdk-17</ja>
-   <kk>Java 17- installs openjdk-17</kk>
-   <ko>Java 17- installs openjdk-17</ko>
-   <lt>Java 17- installs openjdk-17</lt>
-   <mk>Java 17- installs openjdk-17</mk>
-   <mr>Java 17- installs openjdk-17</mr>
-   <nb>Java 17- installs openjdk-17</nb>
-   <nl>Java 17- installeert openjdk-17</nl>
-   <pl>Java 17 - instaluje openjdk-17</pl>
-   <pt_BR>Java 17 - instala o openjdk-17</pt_BR>
-   <pt>Java 17 - instala o openjdk-17</pt>
-   <ro>Java 17- installs openjdk-17</ro>
-   <ru>Java 17 - устанавливает OpenJDK-17</ru>
-   <sk>Java 17- installs openjdk-17</sk>
-   <sl>Java 17- namesti openjdk-17</sl>
-   <sq>Java 17- installs openjdk-17</sq>
-   <sr>Java 17- installs openjdk-17</sr>
-   <sv>Java 17- installerar openjdk-17</sv>
-   <tr>Java 17- installs openjdk-17</tr>
-   <uk>Java 17- installs openjdk-17</uk>
-   <vi>Java 17- installs openjdk-17</vi>
-   <zh_CN>Java 17- installs openjdk-17</zh_CN>
-   <zh_TW>Java 17- installs openjdk-17</zh_TW>
+   <am>Java 21- installs openjdk-21</am>
+   <ar>Java 21- installs openjdk-21</ar>
+   <bg>Java 21- installs openjdk-21</bg>
+   <bn>Java 21- installs openjdk-21</bn>
+   <ca>Java 21- instal·la openjdk-21</ca>
+   <cs>Java 21- installs openjdk-21</cs>
+   <da>Java 21- installs openjdk-21</da>
+   <de>Java 21- installiert openjdk-21</de>
+   <el>Java 21- εγκατάσταση του openjdk-21</el>
+   <en>Java 21- installs openjdk-21</en>
+   <es>Java 21; instala openjdk-21</es>
+   <et>Java 21- installs openjdk-21</et>
+   <eu>Java 21- installs openjdk-21</eu>
+   <fa>Java 21- installs openjdk-21</fa>
+   <fil_PH>Java 21- installs openjdk-21</fil_PH>
+   <fi>Java 21- installs openjdk-21</fi>
+   <fr>Java 21- installe openjdk-21</fr>
+   <he_IL>Java 21- installs openjdk-21</he_IL>
+   <hi>Java 21- installs openjdk-21</hi>
+   <hr>Java 21- installs openjdk-21</hr>
+   <hu>Java 21- installs openjdk-21</hu>
+   <id>Java 21- installs openjdk-21</id>
+   <is>Java 21- installs openjdk-21</is>
+   <it>Java 21- installa openjdk-21</it>
+   <ja>Java 21- installs openjdk-21</ja>
+   <kk>Java 21- installs openjdk-21</kk>
+   <ko>Java 21- installs openjdk-21</ko>
+   <lt>Java 21- installs openjdk-21</lt>
+   <mk>Java 21- installs openjdk-21</mk>
+   <mr>Java 21- installs openjdk-21</mr>
+   <nb>Java 21- installs openjdk-21</nb>
+   <nl>Java 21- installeert openjdk-21</nl>
+   <pl>Java 21 - instaluje openjdk-21</pl>
+   <pt_BR>Java 21 - instala o openjdk-21</pt_BR>
+   <pt>Java 21 - instala o openjdk-21</pt>
+   <ro>Java 21- installs openjdk-21</ro>
+   <ru>Java 21 - устанавливает OpenJDK-21</ru>
+   <sk>Java 21- installs openjdk-21</sk>
+   <sl>Java 21- namesti openjdk-21</sl>
+   <sq>Java 21- installs openjdk-21</sq>
+   <sr>Java 21- installs openjdk-21</sr>
+   <sv>Java 21- installerar openjdk-21</sv>
+   <tr>Java 21- installs openjdk-21</tr>
+   <uk>Java 21- installs openjdk-21</uk>
+   <vi>Java 21- installs openjdk-21</vi>
+   <zh_CN>Java 21- installs openjdk-21</zh_CN>
+   <zh_TW>Java 21- installs openjdk-21</zh_TW>
 </description>
 
 <installable>
@@ -83,7 +83,7 @@ default-jre-headless
 <uninstall_package_names>
 default-jre
 default-jre-headless
-openjdk-17-jre
-openjdk-17-jre-headless
+openjdk-21-jre
+openjdk-21-jre-headless
 </uninstall_package_names>
 </app>
