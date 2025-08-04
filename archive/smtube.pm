54cec4dc4ccdeb205d2dca76667c87924522447b
Dolphin Oracle <dolphinoracle@gmail.com>
2025-08-03 22:27:50 -0400

    archive smtube

---
+++ archive/smtube.pm
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<app>
+
+<category>
+Video
+</category>
+
+<name>
+SMtube
+</name>
+
+<description>
+   <am>search and download Youtube videos</am>
+   <ar>search and download Youtube videos</ar>
+   <bg>search and download Youtube videos</bg>
+   <bn>search and download Youtube videos</bn>
+   <ca>Cerca i descarrega vídeos de Youtube</ca>
+   <cs>search and download Youtube videos</cs>
+   <da>søg efter og download Youtube-videoer</da>
+   <de>YouTube-Videos finden und herunterladen</de>
+   <el>Αναζήτηση και λήψη βίντεο από το Youtube</el>
+   <en>search and download Youtube videos</en>
+   <es>Busque y descargue videos de Youtube</es>
+   <et>search and download Youtube videos</et>
+   <eu>search and download Youtube videos</eu>
+   <fa>search and download Youtube videos</fa>
+   <fil_PH>search and download Youtube videos</fil_PH>
+   <fi>etsi ja lataa Youtube-videoita</fi>
+   <fr>Rechercher et télécharger des vidéos Youtube</fr>
+   <he_IL>search and download Youtube videos</he_IL>
+   <hi>यूट्यूब वीडियो खोज व डाउनलोड</hi>
+   <hr>search and download Youtube videos</hr>
+   <hu>search and download Youtube videos</hu>
+   <id>search and download Youtube videos</id>
+   <is>search and download Youtube videos</is>
+   <it>cerca e scarica video da Youtube</it>
+   <ja>YouTube 動画の検索とダウンロード</ja>
+   <kk>search and download Youtube videos</kk>
+   <ko>search and download Youtube videos</ko>
+   <lt>search and download Youtube videos</lt>
+   <mk>search and download Youtube videos</mk>
+   <mr>search and download Youtube videos</mr>
+   <nb>søk og last ned Youtube-videoer</nb>
+   <nl>zoek en download Youtube video's</nl>
+   <pl>wyszukuj i pobieraj filmy z YouTube</pl>
+   <pt_BR>Pesquisar e baixar vídeos do Youtube</pt_BR>
+   <pt>Pesquisar e descarregar vídeos do Youtube</pt>
+   <ro>search and download Youtube videos</ro>
+   <ru>Поиск и скачивание видео в Youtube</ru>
+   <sk>search and download Youtube videos</sk>
+   <sl>Išči in prenesivideo posnetke iz Youtube</sl>
+   <sq>kërkoni dhe shkarkoni video Youtube</sq>
+   <sr>search and download Youtube videos</sr>
+   <sv>sök och ladda ner Youtube videos</sv>
+   <tr>Youtube videolarını arama ve indirme</tr>
+   <uk>пошук та завантаження відео з Youtube</uk>
+   <vi>search and download Youtube videos</vi>
+   <zh_CN>search and download Youtube videos</zh_CN>
+   <zh_TW>search and download Youtube videos</zh_TW>
+</description>
+
+<installable>
+all
+</installable>
+
+<screenshot>https://screenshots.debian.net/shrine/screenshot/16566/simage/large-a31e1d6ee82b6e3be8387b934b9b16b0.png</screenshot>
+
+<preinstall>
+
+</preinstall>
+
+<install_package_names>
+smtube
+</install_package_names>
+
+
+<postinstall>
+
+</postinstall>
+
+
+<uninstall_package_names>
+smtube
+</uninstall_package_names>
+</app>
