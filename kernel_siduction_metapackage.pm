0237454dbaff12d93da09f35e71d2e415867ec20
Dolphin Oracle <dolphinoracle@gmail.com>
2026-07-26 08:44:44 -0400

    add siduction meta package

---
+++ kernel_siduction_metapackage.pm
@@ -0,0 +1,102 @@
+<?xml version="1.0"?>
+<app>
+
+<category>
+Kernels
+</category>
+
+<name>
+Siduction 64 bit (ahs updates package)
+</name>
+
+<description>
+<am>የ Siduction ahs ማሻሻያ ጥቅል፣ ለራስ-ሰር ዝማኔዎች አህስ መንቃት ያስፈልገዋል</am>
+<ar>ﺔﻴﺋﺎﻘﻠﺘﻟﺍ ﺕﺎﺜﻳﺪﺤﺘﻠﻟ ahs ﻦﻴﻜﻤﺗ Siduction ahs ﺕﺎﺜﻳﺪﺤﺗ ﺔﻣﺰﺣ ﺐﻠﻄﺘﺗ</ar>
+<bg>Пакет за актуализации на Siduction ahs, изисква ahs да бъде активиран за автоматични актуализации</bg>
+<bn>Siduction ahs আপডেট প্যাকেজ, স্বয়ংক্রিয় আপডেটের জন্য ahs সক্ষম করা প্রয়োজন</bn>
+<ca>Paquet d'actualitzacions de Siduction ahs, requereix que ahs estigui habilitat per a actualitzacions automàtiques</ca>
+<cs>Balíček aktualizací Siduction ahs vyžaduje, aby byl ahs povolen pro automatické aktualizace</cs>
+<da>Siduction ahs opdateringspakke, kræver at ahs er aktiveret for automatiske opdateringer</da>
+<de>Siduction ahs-Aktualisierungspaket erfordert die Aktivierung von ahs für automatische Updates</de>
+<el>Το πακέτο ενημερώσεων Siduction ahs, απαιτεί το ahs να είναι ενεργοποιημένο για αυτόματες ενημερώσεις</el>
+<es>Paquete de actualizaciones de Siduction ahs, requiere que ahs esté habilitado para actualizaciones automáticas</es>
+<et>Siductioni ahsi värskenduspakett, automaatsete värskenduste jaoks peab ahs olema lubatud</et>
+<eu>Siduction ahs eguneratze paketea, eguneratze automatikoetarako ahs gaituta egotea eskatzen du</eu>
+<en>Siduction ahs updates package, requires ahs be enabled for automatic updates</en>
+<fa>ﺩﺭﺍﺩ ﺭﺎﮐﺩﻮﺧ ﯽﻧﺎﺳﺭ ﺯﻭﺭ ﻪﺑ ﯼﺍﺮﺑ ahs ﻥﺩﺮﮐ ﻝﺎﻌﻓ ﻪﺑ ﺯﺎﯿﻧ ،Siduction ahs ﯽﻧﺎﺳﺭ ﺯﻭﺭ ﻪﺑ ﻪﺘﺴﺑ</fa>
+<fil_PH>Ang pakete ng mga update ng Siduction ahs, ay nangangailangan ng mga ahs na paganahin para sa mga awtomatikong pag-update</fil_PH>
+<fi>Siduction ahs -päivityspaketti, vaatii ah:n olevan käytössä automaattisia päivityksiä varten</fi>
+<fr>Package de mises à jour Siduction ahs, nécessite que ahs soit activé pour les mises à jour automatiques</fr>
+<he_IL>םייטמוטוא םינוכדע רובע ahs לש הלעפה תשרוד ,Siduction ahs ינוכדע תליבח</he_IL>
+<hi>लिकॉरिक्स एएचएस अपडेट पैकेज, स्वचालित अपडेट के लिए एएचएस को सक्षम करना आवश्यक है</hi>
+<hr>Siduction ahs paket ažuriranja, zahtijeva da ahs bude omogućen za automatska ažuriranja</hr>
+<hu>A Siduction ahs frissítőcsomagja, az automatikus frissítésekhez engedélyezni kell az ahs-t</hu>
+<id>Paket pembaruan Siduction ahs, mengharuskan ahs diaktifkan untuk pembaruan otomatis</id>
+<is>Siduction ahs uppfærslupakki, krefst þess að ahs sé virkt fyrir sjálfvirkar uppfærslur</is>
+<it>Pacchetto aggiornamenti Siduction ahs, richiede che ahs sia abilitato per gli aggiornamenti automatici</it>
+<ja>Siduction ahs アップデート パッケージ。自動アップデートするには ahs を有効にする必要があります</ja>
+<kk>Siduction ahs жаңарту бумасы, автоматты жаңартулар үшін ahs қосулы болуын талап етеді</kk>
+<ko>Siduction ahs 업데이트 패키지, 자동 업데이트를 위해 ahs 활성화 필요</ko>
+<lt>„Siduction ahs“ naujinimų paketas, norint automatiškai atnaujinti, reikia įjungti ahs</lt>
+<mk>Пакетот за ажурирања на Siduction ahs, бара овозможено ahs за автоматско ажурирање</mk>
+<mr>Siduction ahs पॅकेज अपडेट करते, स्वयंचलित अद्यतनांसाठी ahs सक्षम करणे आवश्यक आहे</mr>
+<nb>Siduction ahs oppdateringspakke, krever at ahs er aktivert for automatiske oppdateringer</nb>
+<nl>Siduction ahs-updatepakket, vereist dat ahs is ingeschakeld voor automatische updates</nl>
+<pl>Pakiet aktualizacji Siduction ahs, wymaga włączenia ahs do automatycznych aktualizacji</pl>
+<pt_BR>Pacote de atualizações do Siduction ahs, requer que o ahs esteja habilitado para atualizações automáticas</pt_BR>
+<pt>Pacote de atualizações do Siduction ahs, requer que o ahs esteja habilitado para atualizações automáticas</pt>
+<ro>Pachetul de actualizări Siduction ahs, necesită activarea ahs pentru actualizări automate</ro>
+<ru>Пакет обновлений Siduction ahs, для автоматического обновления требуется включить ahs.</ru>
+<sk>Aktualizačný balík Siduction ahs vyžaduje, aby bol ahs povolený pre automatické aktualizácie</sk>
+<sl>Paket posodobitev Siduction ahs, zahteva, da je ahs omogočen za samodejne posodobitve</sl>
+<sq>Paketa e përditësimeve Siduction ahs, kërkon që ahs të aktivizohet për përditësime automatike</sq>
+<sr>Ликуорик ахс пакет ажурирања, захтева да ахс буде омогућен за аутоматска ажурирања</sr>
+<sv>Siduction ahs uppdateringspaket, kräver att ahs är aktiverat för automatiska uppdateringar</sv>
+<tr>Siduction ahs güncelleme paketi, otomatik güncellemeler için ahs'nin etkinleştirilmesini gerektirir</tr>
+<uk>Пакет оновлень Siduction ahs, вимагає ввімкнення ahs для автоматичних оновлень</uk>
+<vi>Gói cập nhật Siduction ahs, yêu cầu kích hoạt ahs để cập nhật tự động</vi>
+</description>
+
+<installable>
+64
+</installable>
+
+<screenshot>none</screenshot>
+
+<preinstall>
+if [ $(apt-get update --print-uris | grep -c -m1 -E "/mx/repo/dists/trixie/ahs/") = 0 ]; then
+MXREPO=$(apt-get update --print-uris | grep -oE "https?://.*/mx/repo/dists/ahs/main" | tail -1 | sed "s:^:deb :; s:/repo/dists/:/repo/ :; s:/main: ahs:")
+: ${MXREPO:=deb http://mxrepo.com/mx/repo/ trixie ahs}
+echo "$MXREPO" > /etc/apt/sources.list.d/mxpitemp.list
+apt-get update 
+fi
+RTL_PKGS=($(dpkg-query -f '${db:Status-Abbrev}\t${Package}\n' -W  -- 'rtl*-dkms' | grep ^i | grep -- '-dkms$' | cut -d$'\t' -f2))
+echo "realtek rtl package to remove are " ${RTL_PKGS[*]}
+apt-get purge ${RTL_PKGS[*]}
+DKMS_PKGS=($(dpkg-query -f '${db:Status-Abbrev}\t${Package}\n' -W  -- '*-dkms' | grep ^i | grep -- '-dkms$' | cut -d$'\t' -f2))
+apt-get purge rtl*-dkms
+apt-get install linux-image-siduction-amd64 linux-headers-siduction-amd64 ${DKMS_PKGS[*]}
+</preinstall>
+
+<install_package_names>
+
+</install_package_names>
+
+
+<postinstall>
+file=$(apt-cache show linux-image-siduction-amd64 |grep -m1 Depends) 
+file=${file#"Depends:"}
+rebuild_dkms_packages.sh $file
+if [ -f /etc/apt/sources.list.d/mxpitemp.list ]; then
+rm /etc/apt/sources.list.d/mxpitemp.list
+apt-get update
+fi
+echo "...$(gettext -d apt -s ' Done')!"
+</postinstall>
+
+
+<uninstall_package_names>
+linux-image-siduction-amd64
+linux-headers-siduction-amd64
+</uninstall_package_names>
+</app>
